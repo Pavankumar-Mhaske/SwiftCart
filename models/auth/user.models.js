392a0559863d344454c2d825c393e09f549a5525
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-12 17:24:57 +0530

    feat: more fields added to the user schemas

--- models/auth/user.models.js
+++ models/auth/user.models.js
@@ -1,37 +1,88 @@
 import mongoose, { Schema, model } from "mongoose";
 // import bcrypt from "bcryptjs";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {
+  AvailableUserRoles,
+  USER_TEMPORARY_TOKEN_EXPIRY,
+  UserRolesEnum,
+} from "../../constants.js";
+import crypto from "crypto";
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: `https://via.placeholder.com/200x200.png`,
+        localPath: "",
+      },
+    },
+    firstname: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: AvailableUserRoles,
+      default: UserRolesEnum.USER,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+      maxlength: [20, "Contact number can not be more than 20 digits"],
+      trim: true,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
 
-const userSchema = new Schema({
-  firstname: {
-    type: String,
-    required: true,
-    index: true,
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
   },
-  lastname: {
-    type: String,
-    required: true,
-    index: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-  mobile: {
-    type: String,
-    required: true,
-    unique: true,
-    maxlength: [20, "Contact number can not be more than 20 digits"],
-    trim: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
@@ -40,6 +91,48 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      role: this.role,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+
+/**
+ * @description Method responsible for generating tokens for email verification, password reset etc.
+ */
+userSchema.methods.generateTemporaryToken = function () {
+  // This token should be client facing
+  // for example: for email verification unHashedToken should go into the user's mail
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+
+  // This should stay in the DB to compare at the time of verification
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+  // This is the expiry time for the token (20 minutes)
+  const tokenExpiry = Date.now() + USER_TEMPORARY_TOKEN_EXPIRY;
+
+  return { unHashedToken, hashedToken, tokenExpiry };
 };
 
 //  Registration of the User Model(userShema) with name 'User'
