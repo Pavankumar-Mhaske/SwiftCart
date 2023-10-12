876468ab1526a80772627c5f77882f39ac50f5a5
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-12 17:25:39 +0530

    feat: done with the tokens kind of the things

--- controller/auth/user.controllers.js
+++ controller/auth/user.controllers.js
@@ -2,10 +2,31 @@ import { User } from "../../models/auth/user.models.js";
 import { ApiError } from "../../utils/ApiError.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
+import { UserRolesEnum } from "../../constants.js";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    // attach refresh token to the user document to avoid refreshing the access token with multiple refresh tokens
+    user.refreshToken = refreshToken;
+
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating the access token"
+    );
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   try {
-    const { firstname, lastname, email, password, mobile } = req.body;
+    const { firstname, lastname, email, password, mobile, role } = req.body;
 
     const existedUser = await User.findOne({
       $or: [{ mobile }, { email }],
@@ -21,13 +42,35 @@ const registerUser = asyncHandler(async (req, res) => {
       email,
       password,
       mobile,
+      role: role || UserRolesEnum.USER,
     });
 
-    // return res.status(201).json({
-    //   success: true,
-    //   message: "User registered successfully",
-    //   user: user,
-    // });
+    /**
+     * unHashedToken: unHashed token is something we will send to the user's mail
+     * hashedToken: we will keep record of hashedToken to validate the unHashedToken in verify email controller
+     * tokenExpiry: Expiry to be checked before validating the incoming token
+     */
+    const { unHashedToken, hashedToken, tokenExpiry } =
+      user.generateTemporaryToken();
+
+    /**
+     * assign hashedToken and tokenExpiry in DB till user clicks on email verification link
+     * The email verification is handled by {@link verifyEmail}
+     */
+    user.emailVerificationToken = hashedToken;
+    user.emailVerificationExpiry = tokenExpiry;
+    await user.save({ validateBeforeSave: false });
+
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
+    );
+
+    if (!createdUser) {
+      throw new ApiError(
+        500,
+        "Something went wrong while registering the user"
+      );
+    }
 
     return res
       .status(201)
@@ -50,36 +93,66 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  const { mobile, email, password } = req.body;
+  try {
+    const { mobile, email, password } = req.body;
 
-  if (!email || !mobile) {
-    throw new ApiError(400, "Please provide email or mobile");
+    if (!email || !mobile) {
+      throw new ApiError(400, "Please provide email or mobile");
+    }
+    if (!password) {
+      throw new ApiError(400, "Please provide password");
+    }
+
+    const user = await User.findOne({
+      $or: [{ mobile }, { email }],
+    });
+
+    if (!user) {
+      throw new ApiError(404, "User does not exist");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+      throw new ApiError(401, "Invalid user credentials");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+      user._id
+    );
+
+    // const loggedInUser = await User.findById(user._id).select(
+    //   "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
+    // );
+
+    // TODO: Add more options to make cookie more secure and reliable
+    const options = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    };
+    console.log("accessToken: ", accessToken);
+    console.log("refreshToken: ", refreshToken);
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options) // set the access token in the cookie
+      .cookie("refreshToken", refreshToken, options) // set the refresh token in the cookie
+      .json(
+        new ApiResponse(
+          200,
+          { user: user }, // send access and refresh token in response if client decides to save them by themselves
+          "User logged in successfully"
+        )
+      );
+  } catch (error) {
+    console.log("Error in login user controller");
+    console.log("ERROR: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error,
+    });
   }
-  if (!password) {
-    throw new ApiError(400, "Please provide password");
-  }
-
-  const user = await User.findOne({
-    $or: [{ mobile }, { email }],
-  });
-
-  if (!user) {
-    throw new ApiError(404, "User does not exist");
-  }
-
-  const isPasswordValid = await user.isPasswordCorrect(password);
-
-  if (!isPasswordValid) {
-    throw new ApiError(401, "Invalid user credentials");
-  }
-
-  return res.status(200).json(
-    new ApiResponse(
-      200,
-      { user: loggedInUser, accessToken, refreshToken }, // send access and refresh token in response if client decides to save them by themselves
-      "User logged in successfully"
-    )
-  );
 });
 
 export { registerUser, loginUser };
