b8c0c72d69027ea1e64a14a0b3602cd7438fced1
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-12 14:56:11 +0530

    feat: added middlewares

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express();
 
 import userRouter from "./routes/auth/user.routes.js";
 import cors from "cors";
+// api routes
+import { notFound, errorHandler } from "./middlewares/error.middlewares.js";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/v1/users", userRouter);
+
+// common error handling middleware
+app.use(notFound);
+app.use(errorHandler);
 
 // export default app;
 export { app };
