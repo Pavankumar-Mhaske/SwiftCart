b6e2484a63e59ddb5ba90555c6d13a7ae0f4056c
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-12 14:12:49 +0530

    chore: added the try-catch block

--- controller/auth/user.controllers.js
+++ controller/auth/user.controllers.js
@@ -1,28 +1,39 @@
 import { User } from "../../models/auth/user.models.js";
 
 const registerUser = async (req, res) => {
-  const { firstname, lastname, email, password, mobile } = req.body;
+  try {
+    const { firstname, lastname, email, password, mobile } = req.body;
 
-  const existedUser = await User.findOne({
-    $or: [{ mobile }, { email }],
-  });
+    const existedUser = await User.findOne({
+      $or: [{ mobile }, { email }],
+    });
 
-  if (existedUser) {
-    throw new Error(409, "User with email or mobile already exists", []);
+    if (existedUser) {
+      throw new Error(409, "User with email or mobile already exists", []);
+    }
+
+    const user = await User.create({
+      firstname,
+      lastname,
+      email,
+      password,
+      mobile,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: user,
+    });
+  } catch (error) {
+    console.log("Error in create user controller");
+    console.log("ERROR: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error,
+    });
   }
-  const user = await User.create({
-    firstname,
-    lastname,
-    email,
-    password,
-    mobile,
-  });
-
-  return res.status(201).json({
-    success: true,
-    message: "User registered successfully",
-    user: user,
-  });
 };
 
 export { registerUser };
