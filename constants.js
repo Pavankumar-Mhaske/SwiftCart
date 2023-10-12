f89aff5dc7d54aeedf6c9c0de625e6aebf8d0cce
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-12 17:24:30 +0530

    constants: added more constants

--- constants.js
+++ constants.js
@@ -1,1 +1,12 @@
+/**
+ * @type {{ ADMIN: "ADMIN"; USER: "USER"} as const}
+ */
+export const UserRolesEnum = {
+  ADMIN: "ADMIN",
+  USER: "USER",
+};
+export const AvailableUserRoles = Object.values(UserRolesEnum);
+
 export const DB_NAME = "ecommerce";
+
+export const USER_TEMPORARY_TOKEN_EXPIRY = 20 * 60 * 1000; // 20 minutes
