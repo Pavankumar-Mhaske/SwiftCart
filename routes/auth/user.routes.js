d8e7a5c8d65240ccb5f0901110f8defaba284c3e
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-12 11:36:38 +0530

    feat: added route for registerUser

--- routes/auth/user.routes.js
+++ routes/auth/user.routes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { Router } from "express";
+import { registerUser } from "../../controller/auth/user.controllers.js";
+
 const router = Router();
 
 router.route("/").get((req, res) => {
   res.send("Welcome to the E-commerce App!");
 });
 
-router.route("/register").post((req, res) => {
-  res.send("register");
-});
+router.route("/register").post(registerUser);
 
 export default router;
