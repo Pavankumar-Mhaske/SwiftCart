7e9ac14df921f13c0696d28b252a81ab8704047c
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-13 13:22:03 +0530

    constants: added constants

--- constants.js
+++ constants.js
@@ -7,6 +7,26 @@ export const UserRolesEnum = {
 };
 export const AvailableUserRoles = Object.values(UserRolesEnum);
 
+export const ProductColorsEnum = {
+  RED: "RED",
+  GREEN: "GREEN",
+  BLUE: "BLUE",
+  BLACK: "BLACK",
+  WHITE: "WHITE",
+};
+export const AvailableProductColors = Object.values(ProductColorsEnum);
+
+export const ProductBrandsEnum = {
+  APPLE: "APPLE",
+  LENOVO: "LENOVO",
+  SAMSUNG: "SAMSUNG",
+  MI: "MI",
+  TESLA: "TESLA",
+};
+export const AvailableProductBrands = Object.values(ProductBrandsEnum);
+
 export const DB_NAME = "ecommerce";
 
 export const USER_TEMPORARY_TOKEN_EXPIRY = 20 * 60 * 1000; // 20 minutes
+
+export const MAXIMUM_SUB_IMAGE_COUNT = 10;
