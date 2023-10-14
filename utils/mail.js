1312473d1adfd6d929a153bd4c55a3e85f4ad1bc
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-14 14:44:46 +0530

    feat: added mail utilites

--- utils/mail.js
+++ utils/mail.js
@@ -1,57 +1,57 @@
-// import Mailgen from "mailgen";
-// import nodemailer from "nodemailer";
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
 // import { Product } from "../models/apps/ecommerce/product.models.js";
 
 /**
  *
  * @param {{email: string; subject: string; mailgenContent: Mailgen.Content; }} options
  */
-// const sendEmail = async (options) => {
-//   // Initialize mailgen instance with default theme and brand configuration
-//   const mailGenerator = new Mailgen({
-//     theme: "default",
-//     product: {
-//       name: "FreeAPI",
-//       link: "https://freeapi.app",
-//     },
-//   });
+const sendEmail = async (options) => {
+  // Initialize mailgen instance with default theme and brand configuration
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "FreeAPI",
+      link: "https://freeapi.app",
+    },
+  });
 
-//   // For more info on how mailgen content work visit https://github.com/eladnava/mailgen#readme
-//   // Generate the plaintext version of the e-mail (for clients that do not support HTML)
-//   const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
+  // For more info on how mailgen content work visit https://github.com/eladnava/mailgen#readme
+  // Generate the plaintext version of the e-mail (for clients that do not support HTML)
+  const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
 
-//   // Generate an HTML email with the provided contents
-//   const emailHtml = mailGenerator.generate(options.mailgenContent);
+  // Generate an HTML email with the provided contents
+  const emailHtml = mailGenerator.generate(options.mailgenContent);
 
-//   // Create a nodemailer transporter instance which is responsible to send a mail
-//   const transporter = nodemailer.createTransport({
-//     host: process.env.MAILTRAP_SMTP_HOST,
-//     port: process.env.MAILTRAP_SMTP_PORT,
-//     auth: {
-//       user: process.env.MAILTRAP_SMTP_USER,
-//       pass: process.env.MAILTRAP_SMTP_PASS,
-//     },
-//   });
+  // Create a nodemailer transporter instance which is responsible to send a mail
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASS,
+    },
+  });
 
-//   const mail = {
-//     from: "api.service@gmail.com", // We can name this anything. The mail will go to your Mailtrap inbox
-//     to: options.email, // receiver's mail
-//     subject: options.subject, // mail subject
-//     text: emailTextual, // mailgen content textual variant
-//     html: emailHtml, // mailgen content html variant
-//   };
+  const mail = {
+    from: "api.service@gmail.com", // We can name this anything. The mail will go to your Mailtrap inbox
+    to: options.email, // receiver's mail
+    subject: options.subject, // mail subject
+    text: emailTextual, // mailgen content textual variant
+    html: emailHtml, // mailgen content html variant
+  };
 
-//   try {
-//     await transporter.sendMail(mail);
-//   } catch (error) {
-//     // As sending email is not strongly coupled to the business logic it is not worth to raise an error when email sending fails
-//     // So it's better to fail silently rather than breaking the app
-//     console.log(
-//       "Email service failed silently. Make sure you have provided your MAILTRAP credentials in the .env file"
-//     );
-//     console.log("Error: ", error);
-//   }
-// };
+  try {
+    await transporter.sendMail(mail);
+  } catch (error) {
+    // As sending email is not strongly coupled to the business logic it is not worth to raise an error when email sending fails
+    // So it's better to fail silently rather than breaking the app
+    console.log(
+      "Email service failed silently. Make sure you have provided your MAILTRAP credentials in the .env file"
+    );
+    console.log("Error: ", error);
+  }
+};
 
 /**
  *
@@ -153,8 +153,9 @@ const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
 //   };
 // };
 
-export // sendEmail,
-// emailVerificationMailgenContent,
-// forgotPasswordMailgenContent,
-// orderConfirmationMailgenContent,
- {};
+export {
+  sendEmail,
+  // emailVerificationMailgenContent,
+  forgotPasswordMailgenContent,
+  // orderConfirmationMailgenContent,
+};
