d6f4830db25bb61eb87d9edeb957a800e9918dfa
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-14 19:35:56 +0530

    feat: added blog model

---
+++ models/ecommerce/blog.models.js
@@ -0,0 +1,61 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      // index: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    numberOfViews: {
+      type: Number,
+      default: 0,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisliked: {
+      type: Boolean,
+      default: false,
+    },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    dislikes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    image: {
+      type: String,
+      default:
+        "https://www.shutterstock.com/shutterstock/photos/1029506242/display_1500/stock-photo-blogging-blog-concepts-ideas-with-white-worktable-1029506242.jpg",
+    },
+    author: {
+      type: String,
+      default: "ADMIN",
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: true,
+  }
+);
+
+export const Blog = model("Blog", blogSchema);
