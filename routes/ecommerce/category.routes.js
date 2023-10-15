7f50b820645412a9d80820c30599aae9c09aacb8
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-15 11:29:10 +0530

    feat: added category validators

---
+++ routes/ecommerce/category.routes.js
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import {
+  createCategory,
+  getAllCategories,
+  deleteCategory,
+  updateCategory,
+  getCategoryById,
+} from "../../controller/ecommerce/category.controllers.js";
+import { categoryRequestBodyValidator } from "../../validators/ecommerce/category.validators.js";
+import { validate } from "../../validators/validate.js";
+import {
+  verifyPermission,
+  verifyJWT,
+} from "../../middlewares/auth.middlewares.js";
+import { UserRolesEnum } from "../../constants.js";
+import { mongoIdPathVariableValidator } from "../../validators/common/mongodb.validators.js";
+
+const router = Router();
+
+router
+  .route("/")
+  .post(
+    verifyJWT,
+    verifyPermission([UserRolesEnum.ADMIN]),
+    categoryRequestBodyValidator(),
+    validate,
+    createCategory
+  )
+  .get(getAllCategories);
+
+router
+  .route("/:categoryId")
+  .get(mongoIdPathVariableValidator("categoryId"), validate, getCategoryById)
+  .delete(
+    verifyJWT,
+    verifyPermission([UserRolesEnum.ADMIN]),
+    mongoIdPathVariableValidator("categoryId"),
+    validate,
+    deleteCategory
+  )
+  .patch(
+    verifyJWT,
+    verifyPermission([UserRolesEnum.ADMIN]),
+    categoryRequestBodyValidator(),
+    mongoIdPathVariableValidator("categoryId"),
+    validate,
+    updateCategory
+  );
+
+export default router;
