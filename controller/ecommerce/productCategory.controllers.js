f5c7b39498691f4d2f50250b8d56f7b94e01213b
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-15 13:38:31 +0530

    rename: file renamed ( category -> productCategory ) as we have another category of blogs as well

--- controller/ecommerce/productCategory.controllers.js
+++ controller/ecommerce/productCategory.controllers.js
@@ -1,4 +1,4 @@
-import { Category } from "../../models/ecommerce/category.models.js";
+import { ProductCategory } from "../../models/ecommerce/productCategory.models.js";
 import { ApiError } from "../../utils/ApiError.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
@@ -7,13 +7,18 @@ import { getMongoosePaginationOptions } from "../../utils/helpers.js";
 const createCategory = asyncHandler(async (req, res) => {
   try {
     const { name } = req.body;
-    const category = await Category.create({ name, owner: req.user._id });
+    const category = await ProductCategory.create({
+      name,
+      owner: req.user._id,
+    });
     if (!category) {
-      throw new ApiError(400, "Category could not be created");
+      throw new ApiError(400, "ProductCategory could not be created");
     }
     return res
       .status(201)
-      .json(new ApiResponse(200, category, "Category created successfully"));
+      .json(
+        new ApiResponse(200, category, "ProductCategory created successfully")
+      );
   } catch (error) {
     return res
       .status(500)
@@ -23,10 +28,10 @@ const createCategory = asyncHandler(async (req, res) => {
 
 const getAllCategories = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
-  // $match operation is using an empty object {} as the condition, which means that it will match all documents in the Category collection.
-  const categoryAggregate = Category.aggregate([{ $match: {} }]);
+  // $match operation is using an empty object {} as the condition, which means that it will match all documents in the ProductCategory collection.
+  const categoryAggregate = ProductCategory.aggregate([{ $match: {} }]);
 
-  const categories = await Category.aggregatePaginate(
+  const categories = await ProductCategory.aggregatePaginate(
     categoryAggregate,
     getMongoosePaginationOptions({
       page,
@@ -44,19 +49,21 @@ const getAllCategories = asyncHandler(async (req, res) => {
 
 const getCategoryById = asyncHandler(async (req, res) => {
   const { categoryId } = req.params;
-  const category = await Category.findById(categoryId);
+  const category = await ProductCategory.findById(categoryId);
   if (!category) {
-    throw new ApiError(404, "Category does not exist");
+    throw new ApiError(404, "ProductCategory does not exist");
   }
   return res
     .status(200)
-    .json(new ApiResponse(200, category, "Category fetched successfully"));
+    .json(
+      new ApiResponse(200, category, "ProductCategory fetched successfully")
+    );
 });
 
 const updateCategory = asyncHandler(async (req, res) => {
   const { categoryId } = req.params;
   const { name } = req.body;
-  const category = await Category.findByIdAndUpdate(
+  const category = await ProductCategory.findByIdAndUpdate(
     categoryId,
     {
       $set: {
@@ -66,20 +73,22 @@ const updateCategory = asyncHandler(async (req, res) => {
     { new: true }
   );
   if (!category) {
-    throw new ApiError(404, "Category does not exist");
+    throw new ApiError(404, "ProductCategory does not exist");
   }
 
   return res
     .status(200)
-    .json(new ApiResponse(200, category, "Category updated successfully"));
+    .json(
+      new ApiResponse(200, category, "ProductCategory updated successfully")
+    );
 });
 
 const deleteCategory = asyncHandler(async (req, res) => {
   const { categoryId } = req.params;
-  const category = await Category.findByIdAndDelete(categoryId);
+  const category = await ProductCategory.findByIdAndDelete(categoryId);
 
   if (!category) {
-    throw new ApiError(404, "Category does not exist");
+    throw new ApiError(404, "ProductCategory does not exist");
   }
 
   return res
@@ -88,7 +97,7 @@ const deleteCategory = asyncHandler(async (req, res) => {
       new ApiResponse(
         200,
         { deletedCategory: category },
-        "Category deleted successfully"
+        "ProductCategory deleted successfully"
       )
     );
 });
