58c5ad35f8c552a979eb7738b2558e384ec0307a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-15 16:01:18 +0530

    comment: categoryRequestBodyValidator can be used for both productCategory and blogCategory request body validation

--- validators/ecommerce/category.validators.js
+++ validators/ecommerce/category.validators.js
@@ -1,5 +1,8 @@
 import { body, param } from "express-validator";
 
+// categoryRequestBodyValidator is validator for
+// - productCategory request body and
+// - blogCategory request body
 const categoryRequestBodyValidator = () => {
   return [
     body("name").trim().notEmpty().withMessage("Category name is required"),
