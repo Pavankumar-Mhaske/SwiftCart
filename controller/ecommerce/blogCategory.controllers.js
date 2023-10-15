14db510b012712f7bca04cf8d24136f7a469fa87
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-15 16:03:06 +0530

    feat: added the blogCategory controller

--- controller/ecommerce/blogCategory.controllers.js
+++ controller/ecommerce/blogCategory.controllers.js
@@ -1,4 +1,4 @@
-import { ProductCategory } from "../../models/ecommerce/productCategory.models.js";
+import { BlogCategory } from "../../models/ecommerce/blogCategory.models.js";
 import { ApiError } from "../../utils/ApiError.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
@@ -7,17 +7,17 @@ import { getMongoosePaginationOptions } from "../../utils/helpers.js";
 const createCategory = asyncHandler(async (req, res) => {
   try {
     const { name } = req.body;
-    const category = await ProductCategory.create({
+    const category = await BlogCategory.create({
       name,
       owner: req.user._id,
     });
     if (!category) {
-      throw new ApiError(400, "ProductCategory could not be created");
+      throw new ApiError(400, "BlogCategory could not be created");
     }
     return res
       .status(201)
       .json(
-        new ApiResponse(200, category, "ProductCategory created successfully")
+        new ApiResponse(200, category, "BlogCategory created successfully")
       );
   } catch (error) {
     return res
@@ -28,10 +28,10 @@ const createCategory = asyncHandler(async (req, res) => {
 
 const getAllCategories = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
-  // $match operation is using an empty object {} as the condition, which means that it will match all documents in the ProductCategory collection.
-  const categoryAggregate = ProductCategory.aggregate([{ $match: {} }]);
+  // $match operation is using an empty object {} as the condition, which means that it will match all documents in the BlogCategory collection.
+  const categoryAggregate = BlogCategory.aggregate([{ $match: {} }]);
 
-  const categories = await ProductCategory.aggregatePaginate(
+  const categories = await BlogCategory.aggregatePaginate(
     categoryAggregate,
     getMongoosePaginationOptions({
       page,
@@ -49,22 +49,19 @@ const getAllCategories = asyncHandler(async (req, res) => {
 
 const getCategoryById = asyncHandler(async (req, res) => {
   const { categoryId } = req.params;
-  const category = await ProductCategory.findById(categoryId);
-  // const category = await ProductCategory.findById(categoryId).populate("owner");
+  const category = await BlogCategory.findById(categoryId);
   if (!category) {
-    throw new ApiError(404, "ProductCategory does not exist");
+    throw new ApiError(404, "BlogCategory does not exist");
   }
   return res
     .status(200)
-    .json(
-      new ApiResponse(200, category, "ProductCategory fetched successfully")
-    );
+    .json(new ApiResponse(200, category, "BlogCategory fetched successfully"));
 });
 
 const updateCategory = asyncHandler(async (req, res) => {
   const { categoryId } = req.params;
   const { name } = req.body;
-  const category = await ProductCategory.findByIdAndUpdate(
+  const category = await BlogCategory.findByIdAndUpdate(
     categoryId,
     {
       $set: {
@@ -74,19 +71,17 @@ const updateCategory = asyncHandler(async (req, res) => {
     { new: true }
   );
   if (!category) {
-    throw new ApiError(404, "ProductCategory does not exist");
+    throw new ApiError(404, "BlogCategory does not exist");
   }
 
   return res
     .status(200)
-    .json(
-      new ApiResponse(200, category, "ProductCategory updated successfully")
-    );
+    .json(new ApiResponse(200, category, "BlogCategory updated successfully"));
 });
 
 const deleteCategory = asyncHandler(async (req, res) => {
   const { categoryId } = req.params;
-  const category = await ProductCategory.findByIdAndDelete(categoryId);
+  const category = await BlogCategory.findByIdAndDelete(categoryId);
 
   if (!category) {
     throw new ApiError(404, "ProductCategory does not exist");
