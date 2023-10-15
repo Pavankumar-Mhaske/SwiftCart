18ad7a17bf363ca6114e806c301a06dd02d3c50b
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-15 13:40:06 +0530

    rename: imported file renamed ( category -> productCategory ) as we have another category of blogs as well

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 import userRouter from "./routes/auth/user.routes.js";
 import productRouter from "./routes/ecommerce/product.routes.js";
 import blogRouter from "./routes/ecommerce/blog.routes.js";
-import categoryRouter from "./routes/ecommerce/category.routes.js";
+import productCategoryRouter from "./routes/ecommerce/productCategory.routes.js";
 import cors from "cors";
 // api routes
 import { notFound, errorHandler } from "./middlewares/error.middlewares.js";
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/blogs", blogRouter);
-app.use("/api/v1/categories", categoryRouter);
+app.use("/api/v1/product-categories", productCategoryRouter);
 // common error handling middleware
 app.use(notFound);
 app.use(errorHandler);
