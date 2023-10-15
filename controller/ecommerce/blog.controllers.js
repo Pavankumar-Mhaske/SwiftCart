0a3bc61ba70038e15c654f0750b07feb5d8642c4
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-15 09:31:09 +0530

    feat: added getBlogById controller 👻 newest things - update numberOfViews on every call of getBlogById...

--- controller/ecommerce/blog.controllers.js
+++ controller/ecommerce/blog.controllers.js
@@ -48,4 +48,50 @@ const updateBlog = asyncHandler(async (req, res) => {
   }
 });
 
-export { createBlog, updateBlog };
+//  getABlog
+const getBlogById = asyncHandler(async (req, res) => {
+  try {
+    const { blogId } = req.params;
+
+    /**
+    *
+      const fetchedBlog = await Blog.findById(blogId);
+      if (!fetchedBlog) {
+        throw new ApiError(404, "Blog not found");
+      }
+    // Increment the numberOfViews property
+      fetchedBlog.numberOfViews += 1;
+
+    // Save the updated document
+      await fetchedBlog.save();
+
+      or
+      (because this approch makes two calls to the database which is not efficient,
+        same work can be done in single call by the help of findByIdAndUpdate so we use the below approch)
+     */
+    const updatedViewsBlog = await Blog.findByIdAndUpdate(
+      blogId,
+      { $inc: { numberOfViews: 1 } }, // 👻$inc: Increment the numberOfViews property by 1
+      { new: true }
+    );
+
+    if (!updatedViewsBlog) {
+      throw new ApiError(404, "Error in updating the views");
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          updatedViewsBlog,
+          "Blog fetched and views updated successfully!"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(400, error.message);
+  }
+});
+
+
+
+export { createBlog, updateBlog, getBlogById, getAllBlogs };
