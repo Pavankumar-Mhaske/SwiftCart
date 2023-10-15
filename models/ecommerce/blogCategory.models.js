dae9fa0bee2491c5fee00eec595dcd14450c10d1
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-15 16:02:24 +0530

    feat: added blogCategory schemas

---
+++ models/ecommerce/blogCategory.models.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+import { User } from "../auth/user.models.js";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+categorySchema.plugin(mongooseAggregatePaginate);
+
+export const BlogCategory = mongoose.model(
+  "BlogCategory",
+  categorySchema
+);
