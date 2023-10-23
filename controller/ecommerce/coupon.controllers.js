15d1555bd182ba26672ee5b93e09f8d82ed299de
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-23 14:37:52 +0530

    feat: added coupon controller

--- controller/ecommerce/coupon.controllers.js
+++ controller/ecommerce/coupon.controllers.js
@@ -56,6 +56,129 @@ const createCoupon = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, coupon, "Coupon created successfully"));
 });
 
+const getAllCoupons = asyncHandler(async (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
+  const couponAggregate = Coupon.aggregate([{ $match: {} }]);
+
+  const coupons = await Coupon.aggregatePaginate(
+    couponAggregate,
+    getMongoosePaginationOptions({
+      page,
+      limit,
+      customLabels: {
+        totalDocs: "totalCoupons",
+        docs: "coupons",
+      },
+    })
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, coupons, "Coupons fetched successfully"));
+});
+
+const getCouponById = asyncHandler(async (req, res) => {
+  const { couponId } = req.params;
+
+  const coupon = await Coupon.findById(couponId);
+  if (!coupon) {
+    throw new ApiError(404, "Coupon does not exist");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, coupon, "Coupon deleted successfully"));
+});
+
+const updateCoupon = asyncHandler(async (req, res) => {
+  const { couponId } = req.params;
+  const {
+    name,
+    couponCode,
+    type = CouponTypeEnum.FLAT,
+    discountValue,
+    minimumCartValue,
+    startDate,
+    expiryDate,
+  } = req.body;
+
+  const couponToBeUpdated = await Coupon.findById(couponId);
+
+  if (!couponToBeUpdated) {
+    throw new ApiError(404, "Coupon does not exist");
+  }
+
+  const duplicateCoupon = await Coupon.aggregate([
+    {
+      $match: {
+        // See if there is a similar coupon code exist
+        couponCode: couponCode?.trim().toUpperCase(),
+        // Also ignore the current coupon code object which we are updating
+        _id: {
+          $ne: new mongoose.Types.ObjectId(couponToBeUpdated._id),
+        },
+      },
+    },
+  ]);
+
+  if (duplicateCoupon[0]) {
+    throw new ApiError(
+      409,
+      "Coupon with code " + duplicateCoupon[0].couponCode + " already exists"
+    );
+  }
+
+  // Variable to check if min cart value is greater than discount value
+  const _minimumCartValue =
+    minimumCartValue || couponToBeUpdated.minimumCartValue;
+  const _discountValue = discountValue || couponToBeUpdated.discountValue;
+  /**
+   * using _ before the vairable or property is a convention that is widely used in many JavaScript libraries and frameworks to indicate that a variable or property is intended to be private or internal to a module or class.
+   */
+  if (_minimumCartValue && +_minimumCartValue < +_discountValue) {
+    throw new ApiError(
+      400,
+      "Minimum cart value must be greater than or equal to the discount value"
+    );
+  }
+
+  const coupon = await Coupon.findByIdAndUpdate(
+    couponId,
+    {
+      /**
+       * So, in summary, the $set operator is used to set the values of one or more fields in a document in the MongoDB collection.
+       */
+      $set: {
+        name,
+        couponCode,
+        type,
+        discountValue: _discountValue,
+        minimumCartValue: _minimumCartValue,
+        startDate,
+        expiryDate,
+      },
+    },
+    { new: true }
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, coupon, "Coupon updated successfully"));
+});
+
+const deleteCoupon = asyncHandler(async (req, res) => {
+  const { couponId } = req.params;
+
+  const deletedCoupon = await Coupon.findByIdAndDelete(couponId);
+  if (!deletedCoupon) {
+    throw new ApiError(404, "Coupon does not exist");
+  }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, { deletedCoupon }, "Coupon deleted successfully")
+    );
+});
+
 const applyCoupon = asyncHandler(async (req, res) => {
   const { couponCode } = req.body;
 
@@ -170,27 +293,6 @@ const updateCouponActiveStatus = asyncHandler(async (req, res) => {
     );
 });
 
-const getAllCoupons = asyncHandler(async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
-  const couponAggregate = Coupon.aggregate([{ $match: {} }]);
-
-  const coupons = await Coupon.aggregatePaginate(
-    couponAggregate,
-    getMongoosePaginationOptions({
-      page,
-      limit,
-      customLabels: {
-        totalDocs: "totalCoupons",
-        docs: "coupons",
-      },
-    })
-  );
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, coupons, "Coupons fetched successfully"));
-});
-
 const getValidCouponsForCustomer = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
 
@@ -234,108 +336,6 @@ const getValidCouponsForCustomer = asyncHandler(async (req, res) => {
     .status(200)
     .json(
       new ApiResponse(200, coupons, "Customer coupons fetched successfully")
-    );
-});
-
-const getCouponById = asyncHandler(async (req, res) => {
-  const { couponId } = req.params;
-
-  const coupon = await Coupon.findById(couponId);
-  if (!coupon) {
-    throw new ApiError(404, "Coupon does not exist");
-  }
-  return res
-    .status(200)
-    .json(new ApiResponse(200, coupon, "Coupon deleted successfully"));
-});
-
-const updateCoupon = asyncHandler(async (req, res) => {
-  const { couponId } = req.params;
-  const {
-    name,
-    couponCode,
-    type = CouponTypeEnum.FLAT,
-    discountValue,
-    minimumCartValue,
-    startDate,
-    expiryDate,
-  } = req.body;
-
-  const couponToBeUpdated = await Coupon.findById(couponId);
-
-  if (!couponToBeUpdated) {
-    throw new ApiError(404, "Coupon does not exist");
-  }
-
-  const duplicateCoupon = await Coupon.aggregate([
-    {
-      $match: {
-        // See if there is a similar coupon code exist
-        couponCode: couponCode?.trim().toUpperCase(),
-        // Also ignore the current coupon code object which we are updating
-        _id: {
-          $ne: new mongoose.Types.ObjectId(couponToBeUpdated._id),
-        },
-      },
-    },
-  ]);
-
-  if (duplicateCoupon[0]) {
-    throw new ApiError(
-      409,
-      "Coupon with code " + duplicateCoupon[0].couponCode + " already exists"
-    );
-  }
-
-  // Variable to check if min cart value is greater than discount value
-  const _minimumCartValue =
-    minimumCartValue || couponToBeUpdated.minimumCartValue;
-  const _discountValue = discountValue || couponToBeUpdated.discountValue;
-  /**
-   * using _ before the vairable or property is a convention that is widely used in many JavaScript libraries and frameworks to indicate that a variable or property is intended to be private or internal to a module or class.
-   */
-  if (_minimumCartValue && +_minimumCartValue < +_discountValue) {
-    throw new ApiError(
-      400,
-      "Minimum cart value must be greater than or equal to the discount value"
-    );
-  }
-
-  const coupon = await Coupon.findByIdAndUpdate(
-    couponId,
-    {
-      /**
-       * So, in summary, the $set operator is used to set the values of one or more fields in a document in the MongoDB collection.
-       */
-      $set: {
-        name,
-        couponCode,
-        type,
-        discountValue: _discountValue,
-        minimumCartValue: _minimumCartValue,
-        startDate,
-        expiryDate,
-      },
-    },
-    { new: true }
-  );
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, coupon, "Coupon updated successfully"));
-});
-
-const deleteCoupon = asyncHandler(async (req, res) => {
-  const { couponId } = req.params;
-
-  const deletedCoupon = await Coupon.findByIdAndDelete(couponId);
-  if (!deletedCoupon) {
-    throw new ApiError(404, "Coupon does not exist");
-  }
-  return res
-    .status(200)
-    .json(
-      new ApiResponse(200, { deletedCoupon }, "Coupon deleted successfully")
     );
 });
 
