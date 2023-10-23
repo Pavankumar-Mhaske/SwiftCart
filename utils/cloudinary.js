a9681efdb8ec7079bbb0f909069307a237f1c786
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-23 23:21:21 +0530

    feat: added cloudinary configuration

---
+++ utils/cloudinary.js
@@ -0,0 +1,52 @@
+import cloudinary from "cloudinary";
+// import {v2 as cloudinary} from 'cloudinary';
+// import { config, uploader } from "cloudinary";
+
+import dotenv from "dotenv";
+import { resolveContent } from "nodemailer/lib/shared";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const cloudinaryUploadImg = async (filetoUpload) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(filetoUpload, (error, result) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(
+        {
+          url: result.secure_url,
+        },
+        {
+          resource_type: "auto",
+        }
+      );
+    });
+  });
+};
+
+// const cloudinaryUpload = async (fileToUploads) => {
+//   try {
+//     const uploadedFiles = await Promise.all(
+//       fileToUploads.map((file) =>
+//         cloudinary.uploader.upload(file,
+//             {
+//           folder: "ecommerce",
+//         }
+
+//         )
+//       )
+//     );
+//     return uploadedFiles;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+export { cloudinaryUploadImg };
