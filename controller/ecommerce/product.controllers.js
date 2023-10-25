698fe1d62b310709d42897e67ac570b31b5c665d
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-25 22:55:14 +0530

    feat: added response message to the controller

--- controller/ecommerce/product.controllers.js
+++ controller/ecommerce/product.controllers.js
@@ -507,7 +507,8 @@ const reviewsAndRating = asyncHandler(async (req, res) => {
 });
 
 const uploadImages = asyncHandler(async (req, res) => {
-  console.log(req.files);
+  // console.log(req.files);
+  res.status(200).json({ message: "Images uploaded successfully" });
 });
 
 export {
