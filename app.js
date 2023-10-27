0140f9a73fdbf0c357259600928e798fb8c0afb9
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-27 18:25:24 +0530

    feat: added api routes for the cart

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ import blogCategoryRouter from "./routes/ecommerce/blogCategory.routes.js";
 import brandRouter from "./routes/ecommerce/brand.routes.js";
 import couponRouter from "./routes/ecommerce/coupon.routes.js";
 import addressRouter from "./routes/ecommerce/address.routes.js";
+import cartRouter from "./routes/ecommerce/cart.routes.js";
+
 import cors from "cors";
 // api routes
 import { notFound, errorHandler } from "./middlewares/error.middlewares.js";
@@ -36,6 +38,7 @@ app.use("/api/v1/blog-categories", blogCategoryRouter);
 app.use("/api/v1/brands", brandRouter);
 app.use("/api/v1/coupons", couponRouter);
 app.use("/api/v1/addresses", addressRouter);
+app.use("/api/v1/cart", cartRouter);
 
 // common error handling middleware
 app.use(notFound);
