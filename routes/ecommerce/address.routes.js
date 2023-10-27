33853713126a09e641d255f5a617e289b4d989d6
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-27 11:54:38 +0530

    feat: added routes for Address controllers - POST - createAddress, - GET -  getAllAddresses, getAddressById, - PATCH - updateAddress - DELETE -  deleteAddress

---
+++ routes/ecommerce/address.routes.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import {
+  createAddress,
+  deleteAddress,
+  getAddressById,
+  getAllAddresses,
+  updateAddress,
+} from "../../controller/ecommerce/address.controllers.js";
+import { verifyJWT } from "../../middlewares/auth.middlewares.js";
+import {
+  createAddressValidator,
+  updateAddressValidator,
+} from "../../validators/ecommerce/address.validators.js";
+import { validate } from "../../validators/validate.js";
+import { mongoIdPathVariableValidator } from "../../validators/common/mongodb.validators.js";
+
+const router = Router();
+
+/**
+ * All routes require authentication
+ * - The verifyJWT middleware is called for all routes in this router regardless of the HTTP method (e.g., GET, POST, PATCH) or the specific route path.
+        to authenticate incoming requests using JSON Web Tokens (JWT).
+ */
+router.use(verifyJWT);
+
+router
+  .route("/")
+  .post(createAddressValidator(), validate, createAddress)
+  .get(getAllAddresses);
+
+router
+  .route("/:addressId")
+  .get(mongoIdPathVariableValidator("addressId"), validate, getAddressById)
+  .delete(mongoIdPathVariableValidator("addressId"), validate, deleteAddress)
+  .patch(
+    updateAddressValidator(),
+    mongoIdPathVariableValidator("addressId"),
+    validate,
+    updateAddress
+  );
+
+export default router;
