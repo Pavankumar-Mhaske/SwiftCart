69b38816f3f04a48e0ecaa267e0a2785ea2d589a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-28 16:10:27 +0530

    feat: added color model

---
+++ models/ecommerce/color.models.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+import { User } from "../auth/user.models.js";
+import { AvailableProductColors, ProductColorsEnum } from "../../constants.js";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const colorSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      enum: AvailableProductColors,
+      default: ProductColorsEnum.WHITE,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+colorSchema.plugin(mongooseAggregatePaginate);
+
+export const Color = mongoose.model("Color", colorSchema);
