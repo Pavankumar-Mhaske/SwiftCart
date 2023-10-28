1ee7efc6c8d2dec5804a9b53798f04e75b9c81a1
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-29 00:32:29 +0530

    feat: added enquiry validators

---
+++ validators/ecommerce/enquiry.validators.js
@@ -0,0 +1,38 @@
+import { body, param } from "express-validator";
+
+// colorRequestBodyValidator is validator for
+// - productCategory request body and
+// - blogCategory request body
+
+const enquiryRequestBodyValidator = () => {
+  return [
+    body("name")
+      .trim()
+      .notEmpty()
+      .withMessage("name is required")
+      .isLowercase()
+      .withMessage("name must be lowercase")
+      .isLength({ min: 3 })
+      .withMessage("name must be at lease 3 characters long"),
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("mobile")
+      .trim()
+      .notEmpty()
+      .withMessage("mobile is required")
+      .isLength({ min: 10 })
+      .withMessage("mobile must be at lease 10 characters long"),
+    body("comment")
+      .trim()
+      .notEmpty()
+      .withMessage("comment is required")
+      .isLength({ min: 2 })
+      .withMessage("comment must be at lease 2 characters long"),
+  ];
+};
+
+export { enquiryRequestBodyValidator };
