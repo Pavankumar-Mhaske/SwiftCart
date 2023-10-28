60f91d181422a8c3d4afdbd482b4f3e96bd9394d
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-28 10:50:55 +0530

    fix: fixing the path issue

--- validators/ecommerce/order.validators.js
+++ validators/ecommerce/order.validators.js
@@ -1,5 +1,5 @@
 import { body, param } from "express-validator";
-import { AvailableOrderStatuses } from "../../../constants.js";
+import { AvailableOrderStatuses } from "../../constants.js";
 
 const verifyRazorpayPaymentValidator = () => {
   return [
