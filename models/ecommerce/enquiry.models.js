960d3ee276c261868d8073b829fe79a88d61c532
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-29 00:00:25 +0530

    feat: added enquiry model

---
+++ models/ecommerce/enquiry.models.js
@@ -0,0 +1,30 @@
+// enquiry model
+
+import mongoose, { Schema, model } from "mongoose";
+
+const enquirySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Enquiry = model("Enquiry", enquirySchema);
