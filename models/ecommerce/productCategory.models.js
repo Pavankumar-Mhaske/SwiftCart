b984112ada949fd52ea40088c57315f4f13dede6
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-28 18:16:46 +0530

    chore: restricted the name of ProductCategory in constants

--- models/ecommerce/productCategory.models.js
+++ models/ecommerce/productCategory.models.js
@@ -1,14 +1,20 @@
 import mongoose, { Schema } from "mongoose";
 import { User } from "../auth/user.models.js";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import {
+  AvailableProductCategories,
+  ProductCategoryEnum,
+} from "../../constants.js";
 
 const categorySchema = new Schema(
   {
     name: {
       type: String,
-      required: true,
+      // required: true,
       unique: true,
       index: true,
+      enum: AvailableProductCategories,
+      default: ProductCategoryEnum.GENERAL,
     },
     owner: {
       type: Schema.Types.ObjectId,
@@ -20,4 +26,7 @@ const categorySchema = new Schema(
 
 categorySchema.plugin(mongooseAggregatePaginate);
 
-export const ProductCategory = mongoose.model("ProductCategory", categorySchema);
+export const ProductCategory = mongoose.model(
+  "ProductCategory",
+  categorySchema
+);
