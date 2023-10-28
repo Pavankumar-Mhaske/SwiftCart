3149842c327030cc457928d96810a53cf0921c9f
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-28 16:10:49 +0530

    fix: changed categorySchema to brandSchema

--- models/ecommerce/brand.models.js
+++ models/ecommerce/brand.models.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 import { User } from "../auth/user.models.js";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const categorySchema = new Schema(
+const brandSchema = new Schema(
   {
     name: {
       type: String,
@@ -18,9 +18,6 @@ const categorySchema = new Schema(
   { timestamps: true }
 );
 
-categorySchema.plugin(mongooseAggregatePaginate);
+brandSchema.plugin(mongooseAggregatePaginate);
 
-export const Brand = mongoose.model(
-  "Brand",
-  categorySchema
-);
+export const Brand = mongoose.model("Brand", brandSchema);
