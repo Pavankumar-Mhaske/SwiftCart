05914c0d33ababb3629960a347a0305234348302
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-29 23:42:25 +0530

    chore: adding current page reference to the a tag

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -54,16 +54,16 @@ const Footer = () => {
                   mhaskepavankumar@gmail.com
                 </a>
                 <div className="social_icons d-flex align-items-center gap-30 mt-4">
-                  <a className="text-white" href="">
-                    <BsLinkedin className="fs-4" />{" "}
+                  <a className="text-white" href="#">
+                    <BsLinkedin className="fs-4" />
                   </a>
-                  <a className="text-white" href="">
+                  <a className="text-white" href="#">
                     <BsInstagram className="fs-4" />
                   </a>
-                  <a className="text-white" href="">
+                  <a className="text-white" href="#">
                     <BsGithub className="fs-4" />
                   </a>
-                  <a className="text-white" href="">
+                  <a className="text-white" href="#">
                     <BsYoutube className="fs-4" />
                   </a>
                 </div>
