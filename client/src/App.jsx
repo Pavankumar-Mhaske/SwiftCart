5eedd7ece71a34c39edd970f6e73ba990bbcb135
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-29 14:09:52 +0530

    feat: created the changed in app.jsx and added components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1>Hello from Ecommerce Business App</h1>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
