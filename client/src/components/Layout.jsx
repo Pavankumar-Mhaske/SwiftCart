a36b0f9a7848e82d0d72426063457df123cf58cc
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-29 14:51:37 +0530

    feat: created the jsx components

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
