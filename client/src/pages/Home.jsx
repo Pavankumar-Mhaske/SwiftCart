8d471bdda33d76c7b7d2c97e2d68d0398cf0a799
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-30 15:12:01 +0530

    feat: imported and Used the ProductCard Element in the Home.js file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
 import BlogCard from "../components/BlogCard";
+import ProductCard from "../components/ProductCard";
 const Home = () => {
   return (
     <>
@@ -265,13 +266,27 @@ const Home = () => {
           </div>
         </div>
       </section>
+      {/* Featured Collections */}
+      <section className="blog-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">Featured Collections</h3>
+            </div>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </div>
+      </section>
       {/* Blog Wrapper */}
       <section className="blog-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
-          <div className="col-12">
-            <h3 className="section-heading">Our Latest Blogs</h3>
-          </div>
+            <div className="col-12">
+              <h3 className="section-heading">Our Latest Blogs</h3>
+            </div>
             <BlogCard />
             <BlogCard />
             <BlogCard />
