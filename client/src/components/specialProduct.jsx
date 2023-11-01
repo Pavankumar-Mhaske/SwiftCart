7732afebd9517a475b869feaafe4ba61ef7fba79
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-01 22:26:13 +0530

    feat: created the special product section

---
+++ client/src/components/specialProduct.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SpecialProduct = () => {
+  return (
+    <div className="col-4">
+      <div className="special-product-card"></div>
+    </div>
+  );
+};
+
+export default SpecialProduct;
