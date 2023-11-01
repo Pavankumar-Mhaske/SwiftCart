6b48eedf9577c3f998deb0dde59b33076747b073
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-01 23:30:38 +0530

    feat: created the special Product

--- client/src/components/specialProduct.jsx
+++ client/src/components/specialProduct.jsx
@@ -1,9 +1,57 @@
 import React from "react";
-
+import ReactStars from "react-rating-stars-component";
+import { NavLink, Link } from "react-router-dom";
 const SpecialProduct = () => {
   return (
-    <div className="col-4">
-      <div className="special-product-card"></div>
+    <div className="col-6 mb-3">
+      <div className="special-product-card">
+        <div className="d-flex justify-content-between ">
+          <div>
+            <img className="img-fluid" src="images/watch.jpg" alt="watch" />
+          </div>
+          <div className="special-product-content">
+            <h5 className="brand">Havels</h5>
+            <h6 className="title">Samsung Galaxy Mobile Phone, Sim...</h6>
+            <ReactStars
+              count={5}
+              size={24}
+              value={4}
+              isHalf={true}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className="price">
+              <span className="red-p">
+                &nbsp; $100 <strike>$200</strike>
+              </span>
+            </p>
+            <div className="discount-till d-flex align-items-center gap-10">
+              <p className="mb-0">
+                <b>5</b>days
+              </p>
+              <div className="d-flex gap-10 align-items-center">
+                <span className="badge rounded-circle p-3 bg-danger">1</span>:
+                <span className="badge rounded-circle p-3 bg-danger">1</span>:
+                <span className="badge rounded-circle p-3 bg-danger">1</span>
+              </div>
+            </div>
+            <div className="prod-count my-3">
+              <p>Products: 5</p>
+              <div className="progress">
+                <div
+                  className="progress-bar"
+                  role="progressbar"
+                  style={{ width: "25%" }}
+                  aria-valuenow="25"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                ></div>
+              </div>
+            </div>
+            <Link className="button"> Add to Cart</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
