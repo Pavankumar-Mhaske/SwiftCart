0f37efd7afbf8ab2482dccfa20ae5b615389cee4
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-02 14:00:36 +0530

    feat: added the ourStore link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -134,7 +134,7 @@ const Header = () => {
                 <div className="menu-links">
                   <div className="d-flex align-items-center gap-15">
                     <NavLink to="/">Home</NavLink>
-                    <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="/store">Our Store</NavLink>
                     <NavLink to="/">Blogs</NavLink>
                     <NavLink to="/">contact</NavLink>
                   </div>
