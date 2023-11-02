698e092845e97ea740ca48e664ee0f345dbca9ef
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-02 14:00:01 +0530

    feat: created the OurStore element

---
+++ client/src/pages/OurStore.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+
+const OurStore = () => {
+  return (
+    <>
+      <BreadCrumb title="Our Store" />
+    </>
+  );
+};
+
+export default OurStore;
