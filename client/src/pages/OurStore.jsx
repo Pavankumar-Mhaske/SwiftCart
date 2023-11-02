a5985a264bb0364aff15d810a0f13de25a37bb3d
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-02 14:12:52 +0530

    feat: imported the meta and used

--- client/src/pages/OurStore.jsx
+++ client/src/pages/OurStore.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
 
 const OurStore = () => {
   return (
     <>
+      <Meta title={"Our Store"} />
       <BreadCrumb title="Our Store" />
     </>
   );
