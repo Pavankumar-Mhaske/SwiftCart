700fc70ad39ef54719eee76b76a2d43a09f26b54
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-05 00:52:38 +0530

    feat: added the link to the compare-products

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -49,7 +49,10 @@ const Header = () => {
             <div className="col-5">
               <div className="header-upper-links d-flex align-items-center justify-content-between">
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link
+                    to="/compare-product"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
                     {/* files in the public directory are served at the root path.
                         Instead of /public/images/compare.svg, use /images/compare.svg. */}
                     <img src="/images/compare.svg" alt="compare" />
@@ -59,7 +62,10 @@ const Header = () => {
                   </Link>
                 </div>
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link
+                    to="/wishlist"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
                     <img src="/images/wishlist.svg" alt="wishlist" />
                     <p className="mb-0">
                       Favourite <br /> Wishlist
@@ -67,7 +73,10 @@ const Header = () => {
                   </Link>
                 </div>
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link
+                    to="/login"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
                     <img src="/images/user.svg" alt="user" />
                     <p className="mb-0">
                       Login <br /> My Account
@@ -75,7 +84,10 @@ const Header = () => {
                   </Link>
                 </div>
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link
+                    to="/cart"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
                     <img src="/images/cart.svg" alt="cart" />
                     <div className="d-flex flex-column gap-10">
                       <span className="badge bg-white text-dark">0</span>
