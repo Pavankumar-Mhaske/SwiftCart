c5af1bb3f3e0fe4df46d25d51e4eb3e884d1e37e
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-04 18:36:08 +0530

    feat: added Blogs element

---
+++ client/src/pages/Blog.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+const Blog = () => {
+  return (
+    <>
+      <Meta title={"Blogs"} />
+      <BreadCrumb title="Blogs" />
+    </>
+  );
+};
+
+export default Blog;
