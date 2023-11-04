119f3b7b73a0015ec0cd938695efcfe4abe5fb8f
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-04 19:00:33 +0530

    feat: removed col-3 outer element from the definition of the component, and used the col-3 / col-9 whenever the blogCard components are used

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -2,22 +2,20 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 function BlogCard() {
   return (
-    <div className="col-3">
-      <div className="blog-card">
-        <div className="card-image">
-          <img className="img-fluid" src="/images/blog-1.jpg" alt="Blog" />
-        </div>
-        <div className="blog-content">
-          <p className="date">1 DEC 2023</p>
-          <h5 className="title">A Beautiful Sunday Morning Renaissance</h5>
-          <p className="description">
-            You're Only As Good As Your Last Collection. Which is An Enomous
-            Pressure. I Think there is Something About...
-          </p>
-          <Link className="button" to="/">
-            READ MORE
-          </Link>
-        </div>
+    <div className="blog-card">
+      <div className="card-image">
+        <img className="img-fluid  w-100" src="/images/blog-1.jpg" alt="Blog" />
+      </div>
+      <div className="blog-content">
+        <p className="date">1 DEC 2023</p>
+        <h5 className="title">A Beautiful Sunday Morning Renaissance</h5>
+        <p className="description">
+          You're Only As Good As Your Last Collection. Which is An Enomous
+          Pressure. I Think there is Something About...
+        </p>
+        <Link className="button" to="/">
+          READ MORE
+        </Link>
       </div>
     </div>
   );
