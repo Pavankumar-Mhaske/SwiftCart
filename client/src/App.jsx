ea54f74d3b50d1ed3a69f028c5b47952c50ed4f8
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-04 18:35:36 +0530

    feat: added the route element for the blogs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,8 +6,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import OurStore from "./pages/OurStore";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Blog from "./pages/Blog";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 function App() {
   const [count, setCount] = useState(0);
 
@@ -18,9 +18,10 @@ function App() {
           <Route path="/" element={<Layout />}>
             {/* Whatever we pass inside this Router it will be available in the Layout component as a prop called children that is Outlet */}
             <Route index element={<Home />} />
+            <Route path="store" element={<OurStore />} />
+            <Route path="blogs" element={<Blog />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
-            <Route path="store" element={ <OurStore/> } />
           </Route>
         </Routes>
       </Router>
