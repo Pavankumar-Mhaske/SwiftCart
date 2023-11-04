19d48559b0b350a72e56053329d42f5babd3f2f1
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-04 19:01:41 +0530

    feat: used the elements 4 times in the home.js file blogs section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -383,10 +383,20 @@ const Home = () => {
             <div className="col-12">
               <h3 className="section-heading">Our Latest Blogs</h3>
             </div>
-            <BlogCard />
-            <BlogCard />
-            <BlogCard />
-            <BlogCard />
+          </div>
+          <div className="row">
+            <div className="col-3">
+              <BlogCard />
+            </div>
+            <div className="col-3">
+              <BlogCard />
+            </div>
+            <div className="col-3">
+              <BlogCard />
+            </div>
+            <div className="col-3">
+              <BlogCard />
+            </div>
           </div>
         </div>
       </section>
