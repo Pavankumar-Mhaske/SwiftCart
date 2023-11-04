03f0f089d0a32a2acceaaa3ba5a48de4dafb7465
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-04 14:34:10 +0530

    feat: changed the class name from col -> gr

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -11,9 +11,7 @@ const ProductCard = (props) => {
   return (
     <>
       <div
-        className={`${
-          location.pathname === "/store" ? `col-${grid}` : "col-3"
-        }`}
+        className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}
       >
         <Link className="product-card position-relative">
           <div className="wishlist-icon position-absolute">
