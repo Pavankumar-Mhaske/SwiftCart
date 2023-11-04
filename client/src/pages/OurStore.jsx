230a351556585d1d994840c90a0a7d11bbd3d106
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-05 00:52:57 +0530

    feat: imported and used the Color

--- client/src/pages/OurStore.jsx
+++ client/src/pages/OurStore.jsx
@@ -3,6 +3,7 @@ import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import ReactStars from "react-rating-stars-component";
 import ProductCard from "../components/ProductCard";
+import Color from "../components/Color";
 
 const OurStore = () => {
   const [grid, setGrid] = useState(4);
@@ -92,20 +93,7 @@ const OurStore = () => {
                   {/* Colors 🔴🟥 🟡🟨 🟠🟧 ⚪⬜ 🔵🟦 🟢🟩  */}
                   <h5 className="sub-title ">Colors</h5>
                   <div>
-                    <ul className="colors ps-0">
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                    </ul>
+                    <Color />
                   </div>
 
                   {/* Size 🐁 -> 🐘 */}
