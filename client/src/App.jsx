0499f90736c403c2a11ac80d18a42715d3d31c1a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-08 19:26:32 +0530

    feat: added api to the checkout page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Contact from "./pages/Contact";
 import OurStore from "./pages/OurStore";
 import Cart from "./pages/Cart";
 import Blog from "./pages/Blog";
+import Checkout from "./pages/Checkout";
 import CompareProduct from "./pages/CompareProduct";
 import Wishlist from "./pages/Wishlist";
 import Login from "./pages/Login";
@@ -20,6 +21,7 @@ import RefundPolicy from "./pages/RefundPolicy";
 import ShipingPolicy from "./pages/ShipingPolicy";
 import TermsAndConditions from "./pages/TermsAndConditions";
 import SingleProduct from "./pages/SingleProduct";
+
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 function App() {
   const [count, setCount] = useState(0);
@@ -37,6 +39,7 @@ function App() {
             <Route path="product/:id" element={<SingleProduct />} />
             <Route path="blogs" element={<Blog />} />
             <Route path="cart" element={<Cart />} />
+            <Route path="checkouts" element={<Checkout />} />
             <Route path="blog/:id" element={<SingleBlog />} />
             <Route path="compare-product" element={<CompareProduct />} />
             <Route path="wishlist" element={<Wishlist />} />
