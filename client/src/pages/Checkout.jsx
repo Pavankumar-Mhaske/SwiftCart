36336d5c7ca1de8a9de3fd2105dcb152cea58c63
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-09 00:21:30 +0530

    chore: add done with the Checkout page

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -6,49 +6,79 @@ import watch from "../../public/images/watch.jpg";
 const Checkout = () => {
   return (
     <>
-      <div className="checkout-wrapper py-5 home-wrapper-2">
+      <div className="checkout-wrapper home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
             {/* Customer Information */}
-            <div className="col-7">
+            <div className="col-7 py-5">
               <div className="checkout-left-data">
                 <h3 className="website-name">Dev Corner</h3>
-                <nav
-                  style={{ "--bs-breadcrumb-divider": ">" }}
-                  aria-label="breadcrumb"
-                >
-                  <ol className="breadcrumb">
-                    <li className="breadcrumb-item active">
-                      <Link className="cart-link" to="/cart">
-                        Cart
-                      </Link>
-                    </li>
-                    <div className="mx-2 d-flex justify-content-center align-items-center">
-                      <IoIosArrowForward />
-                    </div>
-                    <li
-                      className="breadcrumb-item text-dark "
-                      aria-current="page"
-                    >
-                      {/* <li className="breadcrumb-item active" aria-current="page"> */}
-                      Information
-                    </li>
-                    <div className="mx-2 d-flex justify-content-center align-items-center">
-                      <IoIosArrowForward />
-                    </div>
-                    <li className="breadcrumb-item active">Shipping</li>
-                    <div className="mx-2 d-flex justify-content-center align-items-center">
-                      <IoIosArrowForward />
-                    </div>
-                    <li className="breadcrumb-item active" aria-current="page">
-                      Payment
-                    </li>
-                  </ol>
-                </nav>
-                <h4 className="title">Contact Information</h4>
-                <p className="user-details">
-                  Pavankumar Mhaske (mhaskepavankumar@gmail.com)
-                </p>
+                {/* Progress Breadcrumb */}
+                <div className="my-4">
+                  <nav
+                    style={{ "--bs-breadcrumb-divider": ">" }}
+                    aria-label="breadcrumb"
+                  >
+                    <ol className="breadcrumb">
+                      <li className="breadcrumb-item active">
+                        <Link className="cart-link" to="/cart">
+                          Cart
+                        </Link>
+                      </li>
+                      <div className="mx-2 d-flex justify-content-center align-items-center">
+                        <IoIosArrowForward />
+                      </div>
+                      <li
+                        className="breadcrumb-item text-dark "
+                        aria-current="page"
+                      >
+                        {/* <li className="breadcrumb-item active" aria-current="page"> */}
+                        Information
+                      </li>
+                      <div className="mx-2 d-flex justify-content-center align-items-center">
+                        <IoIosArrowForward />
+                      </div>
+                      <li className="breadcrumb-item active">Shipping</li>
+                      <div className="mx-2 d-flex justify-content-center align-items-center">
+                        <IoIosArrowForward />
+                      </div>
+                      <li
+                        className="breadcrumb-item active"
+                        aria-current="page"
+                      >
+                        Payment
+                      </li>
+                    </ol>
+                  </nav>
+                </div>
+                {/* Contact Information */}
+                <div className="">
+                  <h4 className="title">Contact Information</h4>
+                  <div className="d-flex justify-content-between">
+                    <p className="user-details">
+                      Pavankumar Mhaske (mhaskepavankumar@gmail.com)
+                    </p>
+                    <p className="user-details">Mobile 8530470684 </p>
+                  </div>
+                </div>
+
+                {/* Email me with news and offers */}
+                <div className="d-flex justify-content-between align-items-center mb-4">
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="flexCheckDefault"
+                    />
+                    <label className="form-check-label" for="flexCheckDefault">
+                      Email me with news and offers
+                    </label>
+                  </div>
+                </div>
+
+                {/* Shipping Address */}
+                <h4 className="mb-2 address-title">Shipping address</h4>
                 {/* 📃📃📃📃 Info. Form  📃📃📃📃 */}
                 <form
                   action=""
@@ -120,7 +150,7 @@ const Checkout = () => {
                     />
                   </div>
                   {/* Return to Cart / Continue To Shipping */}
-                  <div className="w-100">
+                  <div className="w-100  my-4">
                     <div className="d-flex justify-content-between align-items-center">
                       <Link to="/cart" className="text-dark">
                         <BiArrowBack className="fs-5 me-2" /> Return to Cart
@@ -134,46 +164,50 @@ const Checkout = () => {
               </div>
             </div>
             {/* Order Information */}
-            <div className="col-5">
+            <div className="col-5 order-info py-5">
               {/* Cart Section */}
               <div className="border-bottom py-4">
-                <div className="d-flex gap-10 mb-2 align-items-center">
+                <div className="d-flex gap-10 mb-2 justify-content-between align-items-center">
                   <div className="w-75 d-flex gap-10">
                     <div className="w-25 position-relative">
                       <span
-                        style={{ top: "-10px", right: "2px" }}
+                        style={{ top: "-10px", right: "-5px" }}
                         className="badge bg-secondary text-white rounded-circle position-absolute"
                       >
                         1
                       </span>
                       <img className="img-fluid" src={watch} alt="Products" />
                     </div>
-                    <div className="w-75 position-relative border border-2">
-                      <h5 className="title">
+                    <div className="w-75  d-flex flex-column justify-content-evenly position-relative ">
+                      <h5 className="cart-title">
                         Honor T1 7.0 1GB RAM 8GB ROM 7 inch with Wi-Fi+3G Tablet
                       </h5>
-                      <p>S / #B5CEDE</p>
+                      <p className="cart-sub-title">S / #B5CEDE</p>
                     </div>
                   </div>
-                  <div className="w-25 flex-grow-1 border border-2">
-                    <h5>$100</h5>
+                  <div className="w-25 flex-grow-1 d-flex justify-content-end ">
+                    <div className="">
+                      <p className="final-products-price">$ 410.00</p>
+                    </div>
                   </div>
                 </div>
               </div>
               {/* Pricing Section */}
               <div className="border-bottom py-4">
                 <div className="d-flex justify-content-between align-items-center">
-                  <p>SubTotal</p>
-                  <p>$ 410.00</p>
+                  <p className="subtotal">SubTotal</p>
+                  <p className="shipping-total">$ 410.00</p>
                 </div>
                 <div className="d-flex justify-content-between align-items-center">
-                  <p className="mb-0">Shipping</p>
-                  <p className="mb-0">$ 19.00</p>
+                  <p className="mb-0 shipping">Shipping</p>
+                  <p className="mb-0 shipping-price">$ 19.00</p>
                 </div>
               </div>
               <div className="border-bottom py-4 d-flex justify-content-between align-items-center">
                 <h4 className="total">Total</h4>
-                <h5 className="total-price">$ 429.00</h5>
+                <h5 className="total-price">
+                  <span>USD</span> $ 429.00
+                </h5>
               </div>
             </div>
           </div>
