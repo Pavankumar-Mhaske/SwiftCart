e0e4a555cf86d75d36ae1b29f718eff19776ec4f
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-09 11:28:19 +0530

    feat: created the Data list file

---
+++ client/src/utils/Data.jsx
@@ -0,0 +1,27 @@
+export const services = [
+  {
+    title: "Free Shipping",
+    tagline: "From all orders over $100",
+    image: "/images/service.png",
+  },
+  {
+    title: "Dialy Surprise Offers",
+    tagline: "Save up to 25% off",
+    image: "/images/service-02.png",
+  },
+  {
+    title: "Support 24/7",
+    tagline: "Shop with an expert",
+    image: "/images/service-03.png",
+  },
+  {
+    title: "Affordable Prices",
+    tagline: "Get Factory Default price",
+    image: "/images/service-04.png",
+  },
+  {
+    title: "Secure Payments",
+    tagline: "100% Protected Payments",
+    image: "/images/service-05.png",
+  },
+];
