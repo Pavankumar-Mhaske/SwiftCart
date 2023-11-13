9315944848714311e796bc7bbbd0b4cce19ef0d2
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-13 15:47:40 +0530

    feat: created the forgotPassword element

--- admin/src/pages/ForgotPassword.jsx
+++ admin/src/pages/ForgotPassword.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import CustomInput from "../components/CustomInput";
 
 const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
-  )
-}
+    <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
+        <h3 className="text-center">Forgot Password</h3>
+        <p className="text-center">
+          please enter your register email to get reset password mail.{" "}
+        </p>
+        <form action="">
+          <CustomInput type="password" label="password" id="password" />
+          <button
+            className="border-0 px-3 py-2 text-white fw-bold w-100"
+            style={{ background: "#ffd333" }}
+            type="submit"
+          >
+            Send Link
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
