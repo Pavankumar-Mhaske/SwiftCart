929d478f6629b542708c577e327721883ef159a4
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-20 23:36:46 +0530

    feat: imported and added route for the Enquiries

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,6 +5,7 @@ import ResetPassword from "./pages/ResetPassword";
 import ForgotPassword from "./pages/ForgotPassword";
 import Dashboard from "./pages/Dashboard";
 import MainLayout from "./components/MainLayout";
+import Enquiries from "./pages/Enquiries";
 function App() {
   return (
     <Router>
@@ -14,7 +15,10 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
+          <Route path="enquiries" element={<Enquiries />} />
         </Route>
+
+        {/* <Route path="*" element={<h1>Not Found</h1>} /> */}
       </Routes>
     </Router>
   );
