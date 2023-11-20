70533600580fcb8e0e3f14a6b6257c0f5a670f98
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-20 23:47:25 +0530

    feat: added the route for the BlogList and BlogCategoryList

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,6 +6,8 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Dashboard from "./pages/Dashboard";
 import MainLayout from "./components/MainLayout";
 import Enquiries from "./pages/Enquiries";
+import BlogList from "./pages/BlogList";
+import BlogCategoryList from "./pages/BlogCategoryList";
 function App() {
   return (
     <Router>
@@ -16,6 +18,8 @@ function App() {
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="enquiries" element={<Enquiries />} />
+          <Route path="blog-list" element={<BlogList />} />
+          <Route path="blog-category-list" element={<BlogCategoryList />} />
         </Route>
 
         {/* <Route path="*" element={<h1>Not Found</h1>} /> */}
