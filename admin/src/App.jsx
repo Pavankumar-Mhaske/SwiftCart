e451bc4ba719872046e6d1c48784e78cdb35a4ad
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-21 16:53:36 +0530

    feat: added routes for the AddBrand

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -18,6 +18,7 @@ import AddBlog from "./pages/AddBlog";
 import AddBlogCategory from "./pages/AddBlogCategory";
 import AddColor from "./pages/AddColor";
 import AddCategory from "./pages/AddCategory";
+import AddBrand from "./pages/AddBrand";
 
 function App() {
   return (
@@ -35,12 +36,13 @@ function App() {
           <Route path="blog-category-list" element={<BlogCategoryList />} />
           <Route path="orders" element={<Orders />} />
           <Route path="customers" element={<Customers />} />
-          <Route path="color" element={<AddColor />} />
-          <Route path="color-list" element={<ColorList />} />
+          <Route path="product-list" element={<ProductList />} />
+          <Route path="brand" element={<AddBrand />} />
+          <Route path="brand-list" element={<BrandList />} />
           <Route path="category" element={<AddCategory />} />
           <Route path="category-list" element={<CategoryList />} />
-          <Route path="brand-list" element={<BrandList />} />
-          <Route path="product-list" element={<ProductList />} />
+          <Route path="color" element={<AddColor />} />
+          <Route path="color-list" element={<ColorList />} />
         </Route>
 
         {/* <Route path="*" element={<h1>Not Found</h1>} /> */}
