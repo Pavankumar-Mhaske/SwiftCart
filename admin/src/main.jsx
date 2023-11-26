522de51d44752a40234e8b3fc419edddcaf7375f
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-11-26 17:05:38 +0530

    feat: added store for the state manegement with the help of the Provoider

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import './index.css'
+import "./index.css";
+import { store } from "./app/Store";
+import { Provider } from "react-redux";
 
+// use this store inside the render method
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
