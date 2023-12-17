c033d70131f1be548b45398e495c746f8237f92d
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-17 16:58:05 +0530

    feat: added the authReducer

--- admin/src/app/Store.jsx
+++ admin/src/app/Store.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import authReducer from "../features/auth/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { auth: authReducer },
 });
