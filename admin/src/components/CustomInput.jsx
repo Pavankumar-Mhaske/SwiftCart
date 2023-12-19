5ace3cdea5b2deb2d89111ebbeb25a24f8a45e52
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-19 15:35:28 +0530

    feat: added more props/attributes for the input

--- admin/src/components/CustomInput.jsx
+++ admin/src/components/CustomInput.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 const CustomInput = (props) => {
-  const { type, label, i_id, i_class } = props;
+  const { type, label, i_id, i_class, name, value, onChange, onBlur } = props;
   return (
-    <div className="form-floating mb-3">
+    <div className="form-floating mt-3">
       <input
         type={type}
         className={`form-control ${i_class}`}
         id={i_id}
         placeholder={label}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
       />
       <label htmlFor={label}>{label}</label>
     </div>
