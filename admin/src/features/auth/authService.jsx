bb8685f73a8649f10e2b0b654ac35998d3bf2f17
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-19 18:09:27 +0530

    feat: testing with the auth

--- admin/src/features/auth/authService.jsx
+++ admin/src/features/auth/authService.jsx
@@ -4,7 +4,7 @@ import { base_url } from "../../utils/base_url";
 const login = async (userData) => {
   const url = `${base_url}users/admin-login/`;
   const response = await axios.post(url, userData);
-  console.log(response.data);
+  console.log("Response in auth is : ", response.data);
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
   }
