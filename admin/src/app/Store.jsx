8c2cd65c0bbcfcec0e2f59c4ead1cce864397b28
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-19 18:09:09 +0530

    feat: added the customerReducer into the store

--- admin/src/app/Store.jsx
+++ admin/src/app/Store.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import authReducer from "../features/auth/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import customerReducer from "../features/customers/customerSlice";
 
 export const store = configureStore({
-  reducer: { auth: authReducer },
+  reducer: { auth: authReducer, customer: customerReducer },
 });
