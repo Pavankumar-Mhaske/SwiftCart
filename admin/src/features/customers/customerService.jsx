5307e5016a31ed42bd82152cd8176e7f723fc97d
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-24 14:47:38 +0530

    feat: changed the accesstoken

--- admin/src/features/customers/customerService.jsx
+++ admin/src/features/customers/customerService.jsx
@@ -7,7 +7,7 @@ const getUsers = async () => {
   //   const token = Cookies.get("user");
 
   const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTI3ZjBmOWE1Y2IzZGFlZTk5MzU4MGMiLCJlbWFpbCI6Im1oYXNrZXBhdmFua3VtYXJAZ21haWwuY29tIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzAzMjM2OTg0LCJleHAiOjE3MDMzMjMzODR9.zDEWxUIf9WLSyUUhIyFxgAcbXhQazL9lxnEnE7gCnwQ";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTI3ZjBmOWE1Y2IzZGFlZTk5MzU4MGMiLCJlbWFpbCI6Im1oYXNrZXBhdmFua3VtYXJAZ21haWwuY29tIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzAzNDA4ODM0LCJleHAiOjE3MDM0OTUyMzR9.52XbIt2AIgVU07M8W8BBCAAcyFrXCepZmHTBHQ2PQb8";
   console.log("token in customer is : ", token);
   const url = `${base_url}users/all-users/`;
   const response = await axios.get(url, {
