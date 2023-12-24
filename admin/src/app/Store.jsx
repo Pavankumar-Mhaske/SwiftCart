8adc9d3f4f4d6497c3bf63be80cbf94e344ac193
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-24 15:53:55 +0530

    chore: modified the store

--- admin/src/app/Store.jsx
+++ admin/src/app/Store.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import authReducer from "../features/auth/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
 import customerReducer from "../features/customers/customerSlice";
+import productReducer from "../features/product/productSlice";
 
 export const store = configureStore({
-  reducer: { auth: authReducer, customer: customerReducer },
+  reducer: {
+    auth: authReducer,
+    customer: customerReducer,
+    product: productReducer,
+  },
 });
