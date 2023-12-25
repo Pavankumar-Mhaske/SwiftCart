be218747b032914bbbb45ff5912b70621f3643fc
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-25 16:25:18 +0530

    feat: created the brandService

---
+++ admin/src/features/brand/brandService.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { base_url } from "../../utils/base_url";
+import { Token } from "../token";
+
+const getBrands = async () => {
+  const token = Token;
+
+  console.log("token in BrandService is : ", token);
+  const url = `${base_url}brands/`;
+  const response = await axios.get(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  //   const response = await axios.get(url);
+  console.log("Response in BrandService is : ", response);
+
+  return response.data;
+};
+
+const BrandService = {
+  getBrands,
+};
+
+export default BrandService; // export the service
