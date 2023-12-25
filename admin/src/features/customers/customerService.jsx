cf5119e9beed890daa1e40f4c2dda1d6b0a55311
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-25 15:42:33 +0530

    feat: added the token separetely in different file

--- admin/src/features/customers/customerService.jsx
+++ admin/src/features/customers/customerService.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { base_url } from "../../utils/base_url";
+import { Token } from "../token";
 
 const getUsers = async () => {
   // Get the token from cookies
   //   const token = Cookies.get("user");
 
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTI3ZjBmOWE1Y2IzZGFlZTk5MzU4MGMiLCJlbWFpbCI6Im1oYXNrZXBhdmFua3VtYXJAZ21haWwuY29tIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzAzNDA4ODM0LCJleHAiOjE3MDM0OTUyMzR9.52XbIt2AIgVU07M8W8BBCAAcyFrXCepZmHTBHQ2PQb8";
+  const token = Token;
   console.log("token in customer is : ", token);
   const url = `${base_url}users/all-users/`;
   const response = await axios.get(url, {
--- admin/src/features/product/productService.jsx
+++ admin/src/features/product/productService.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { base_url } from "../../utils/base_url";
+import {  Token } from "../token";
 
 const getProducts = async () => {
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTI3ZjBmOWE1Y2IzZGFlZTk5MzU4MGMiLCJlbWFpbCI6Im1oYXNrZXBhdmFua3VtYXJAZ21haWwuY29tIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzAzNDA4ODM0LCJleHAiOjE3MDM0OTUyMzR9.52XbIt2AIgVU07M8W8BBCAAcyFrXCepZmHTBHQ2PQb8";
+  const token = token;
+    
   console.log("token in customer is : ", token);
   const url = `${base_url}products/`;
   const response = await axios.get(url, {
