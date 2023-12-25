3d5527cdfc8d168c513a3ceb8cefc619453246e8
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-25 16:25:01 +0530

    feat: added the brand State in the store

--- admin/src/app/Store.jsx
+++ admin/src/app/Store.jsx
@@ -3,11 +3,13 @@ import authReducer from "../features/auth/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
 import customerReducer from "../features/customers/customerSlice";
 import productReducer from "../features/product/productSlice";
+import brandReducer from "../features/brand/brandSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     customer: customerReducer,
     product: productReducer,
+    brand: brandReducer,
   },
 });
