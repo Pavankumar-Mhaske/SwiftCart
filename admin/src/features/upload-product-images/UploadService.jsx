80569cb142227959bf24413e3c0258c7e9d3cf8f
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-26 19:43:18 +0530

    feat: created the UploadService for Products

---
+++ admin/src/features/upload-product-images/UploadService.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { base_url } from "../../utils/base_url";
+import { Token } from "../Token";
+
+const uploadImages = async (data) => {
+  const token = Token;
+  console.log("token in uploadService is : ", token);
+  const url = `${base_url}products/upload/6560a001bd382c6228e15855/`;
+  const response = await axios.post(url, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  console.log("response in uploadService is : ", response);
+  return response.data;
+};
+
+const uploadService = {
+  uploadImages,
+};
+
+export default uploadService; // export the service
