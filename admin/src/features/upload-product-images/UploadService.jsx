66fb1c46bd5d187f9f77ba242371483511b1e2cd
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-27 17:26:46 +0530

    feat: added  the deleteImage functionality

--- admin/src/features/upload-product-images/UploadService.jsx
+++ admin/src/features/upload-product-images/UploadService.jsx
@@ -5,7 +5,8 @@ import { Token } from "../Token";
 const uploadImages = async (data) => {
   const token = Token;
   console.log("token in uploadService is : ", token);
-  const url = `${base_url}products/upload/6560a001bd382c6228e15855/`;
+  console.log("data in uploadService is : ", data);
+  const url = `${base_url}products/upload/`;
   const response = await axios.post(url, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
@@ -13,20 +14,21 @@ const uploadImages = async (data) => {
   return response.data;
 };
 
-// const deleteImages = async (id) => {
-//   const token = Token;
-//   console.log("token in deleteService is : ", token);
-//   const url = `${base_url}products/delete/${id}/`;
-//   const response = await axios.delete(url, id, {
-//     headers: { Authorization: `Bearer ${token}` },
-//   });
-//   console.log("response in deleteService is : ", response);
-//   return response.data;
-// };
+const deleteImages = async (id) => {
+  const token = Token;
+  console.log("token in deleteService is : ", token);
+  console.log("id in deleteService is : ", id);
+  const url = `${base_url}products/delete/${id}`;
+  const response = await axios.delete(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  console.log("response in deleteService is : ", response);
+  return response.data;
+};
 
 const uploadService = {
   uploadImages,
-  // deleteImages,
+  deleteImages,
 };
 
 export default uploadService; // export the service
