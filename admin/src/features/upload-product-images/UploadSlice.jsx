58004c7e30a222fdcf3436fcb1f69f5aa951198d
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-27 20:48:16 +0530

    chore: commenting out the comment lines

--- admin/src/features/upload-product-images/UploadSlice.jsx
+++ admin/src/features/upload-product-images/UploadSlice.jsx
@@ -5,14 +5,14 @@ export const uploadImages = createAsyncThunk(
   "product/product-images",
   async (data, thunkAPI) => {
     try {
-      console.log("type of data in uploadSlice is : ", typeof data);
-      console.log("thunkAPI in uploadSlice is : ", thunkAPI);
+      // console.log("type of data in uploadSlice is : ", typeof data);
+      // console.log("thunkAPI in uploadSlice is : ", thunkAPI);
       const formData = new FormData();
       for (let i = 0; i < data?.length; i++) {
         formData.append("images", data[i]);
       }
       const response = await uploadService.uploadImages(formData);
-      console.log("response in uploadSlice is : ", response);
+      // console.log("response in uploadSlice is : ", response);
       return response;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -24,10 +24,10 @@ export const deleteImages = createAsyncThunk(
   "product-delete/product-images",
   async (id, thunkAPI) => {
     try {
-      console.log("thunkAPI in deleteUploadSlice is : ", thunkAPI);
-      console.log("id in deleteUploadSlice is : ", id);
+      // console.log("thunkAPI in deleteUploadSlice is : ", thunkAPI);
+      // console.log("id in deleteUploadSlice is : ", id);
       const response = await uploadService.deleteImages(id);
-      console.log("response in deleting uploadSlice is : ", response);
+      // console.log("response in deleting uploadSlice is : ", response);
       return response;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -60,7 +60,7 @@ export const uploadSlice = createSlice({
         state.isSuccess = true;
         state.images = action.payload.data.images;
         // state.productId = action.payload.data._id;
-        console.log("action.payload in uploadSlice is : ", action.payload.data.images);
+        // console.log("action.payload in uploadSlice is : ", action.payload.data.images);
       })
       // ;builder
       .addCase(uploadImages.rejected, (state, action) => {
@@ -82,10 +82,10 @@ export const uploadSlice = createSlice({
         // state.images = action.payload.data == {} ? [] : action.payload.data.subImages;
         state.images = [];
         // state.productId = action.payload.data;
-        console.log(
-          "action.payload in delete deleteUploadSlice is : ",
-          action.payload.data
-        );
+        // console.log(
+        //   "action.payload in delete deleteUploadSlice is : ",
+        //   action.payload.data
+        // );
       })
       // ;builder
       .addCase(deleteImages.rejected, (state, action) => {
