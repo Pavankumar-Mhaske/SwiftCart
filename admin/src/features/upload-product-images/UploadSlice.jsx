2fe60baa2cca55a28488ec985671802dd955a4b5
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-27 13:57:10 +0530

    feat: trying to attempt the deleteImage functionality

--- admin/src/features/upload-product-images/UploadSlice.jsx
+++ admin/src/features/upload-product-images/UploadSlice.jsx
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import uploadService from "./UploadService";
+
 export const uploadImages = createAsyncThunk(
   "product/product-images",
   async (data, thunkAPI) => {
@@ -10,7 +11,6 @@ export const uploadImages = createAsyncThunk(
       for (let i = 0; i < data.length; i++) {
         formData.append("images", data[i]);
       }
-
       const response = await uploadService.uploadImages(formData);
       console.log("response in uploadSlice is : ", response);
       return response;
@@ -19,6 +19,21 @@ export const uploadImages = createAsyncThunk(
     }
   }
 );
+
+// export const deleteImages = createAsyncThunk(
+//   "product-delete/product-images",
+//   async (id, thunkAPI) => {
+//     try {
+//       console.log("thunkAPI in uploadSlice is : ", thunkAPI);
+
+//       const response = await uploadService.deleteImages(id);
+//       console.log("response in deleting uploadSlice is : ", response);
+//       return response;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error);
+//     }
+//   }
+// );
 
 const initialState = {
   images: [],
@@ -56,6 +71,30 @@ export const uploadSlice = createSlice({
         // state.user = null;
         state.message = action.error;
       });
+    /*
+      .addCase(deleteImages.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(deleteImages.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.images = action.payload.data.subImages;
+        console.log(
+          "action.payload in uploadSlice is : ",
+          action.payload.data.subImages
+        );
+      })
+      // ;builder
+      .addCase(deleteImages.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      });
+      */
   },
 });
 
