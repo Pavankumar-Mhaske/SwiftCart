94b42fd51bda74e629d77fb4d611ce82e498cd3a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-28 15:52:24 +0530

    testing

--- admin/src/pages/demo.jsx
+++ admin/src/pages/demo.jsx
@@ -1,28 +1,60 @@
-const colors = [];
-colorState.forEach((color, key) => {
-  colors.push({
-    id: key + 1,
-    color: color.name,
-  });
-});
+{
+  "name": "product-0",
+  "description": "<p>2</p>",
+  "price": 2,
+  "stock": 0,
+  // "category": "travel",
+  "brand": "LENOVO",
+  // "colors": [
+      {
+          "id": 1,
+          "color": "RED"
+      },
+      {
+          "id": 2,
+          "color": "GRAY"
+      }
+  ]
+}
 
-const initialValues = {
-  // some code
-  color: colors.length > 0 ? [colors[0]] : [],
-  // some code
-};
+{
+  "name": "Product 21",
+  "description": "<p>fd</p>",
+//   "description": "Product description",
+  "price": 50,
+  "stock": 120,
+  // "soldItems": 20,
+  "brand": "APPLE",
+  "colors": ["653cedcf7d3bc7fc1be0efd5", "653cee117d3bc7fc1be0efdd","653cee827d3bc7fc1be0efe1"]
+  ,"tags":["POPULAR", "FEATURED", "TOP_RATED"]
+  ,"category":"652b9ce6b8fd6a63e2dcd856"
+  
+}
 
-// console.log("initialValues : ", initialValues);
-const formik = useFormik({
-  initialValues: initialValues,
-  // some code
-});
-console.log("formik.values.color at begining 🌎 : ", formik.values.color);
+{
+  "name": "2",
+  "description": "<p>2</p>",
+  "price": -1,
+  "stock": 2,
+  "category": "652b9ce6b8fd6a63e2dcd856",
+  "brand": "APPLE",
+  "colors": [
+      "653cedcf7d3bc7fc1be0efd5",
+      "653cee117d3bc7fc1be0efdd",
+      "653cee827d3bc7fc1be0efe1"
+  ]
+}
 
-useEffect(() => {
-  formik.values.color = initialValues.color;
-  console.log("formik.values.color ❗💥❗💥: ", formik.values.color);
-}, []);
 
-``` i am confused, that how can i set the initial value of formik.values.color to initialValues.color at the starting only,
-  because when i trying this way .... it is not working, it is not setting the initial value of formik.values.color to initialValues.color at the starting only. moreoveer it's seting to [] empty array.```;
+``` during the changes in the ReactQuill i am able to set the value of the formik.values.description 
+like - "<p>hello this is new product</p>" but i just want the value of it without tag (<p> </p>) that is only "hello this is new product"
+for reference i have provoided the code below
+
+              <ReactQuill
+              theme="snow"
+              name="description"
+              value={formik.values.description}
+              onChange={formik.handleChange("description")}
+            />
+          
+            ```
