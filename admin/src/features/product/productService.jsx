4ea50d4e88e45c4e68418fbda50415a954b804fe
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-28 16:02:31 +0530

    chore: now hiting the request with the actual product

--- admin/src/features/product/productService.jsx
+++ admin/src/features/product/productService.jsx
@@ -22,22 +22,6 @@ const createProduct = async (product) => {
   console.log("token in productService is : ", token);
   console.log("product in productService is : ", product);
   const url = `${base_url}products/`;
-  product = {
-    name: "Product 23",
-    description: "<p>fd</p>",
-    //   "description": "Product description",
-    price: 50,
-    stock: 120,
-    // "soldItems": 20,
-    brand: "APPLE",
-    colors: [
-      "653cedcf7d3bc7fc1be0efd5",
-      "653cee117d3bc7fc1be0efdd",
-      "653cee827d3bc7fc1be0efe1",
-    ],
-    tags: ["POPULAR", "FEATURED", "TOP_RATED"],
-    category: "652b9ce6b8fd6a63e2dcd856",
-  };
   const response = await axios.post(url, product, {
     headers: { Authorization: `Bearer ${token}` },
   });
