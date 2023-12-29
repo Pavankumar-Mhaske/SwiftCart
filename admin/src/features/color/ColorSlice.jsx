05422085fc613cf12590b65a36fadff4b362d409
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-29 06:20:20 +0530

    feat: created color creation Slice

--- admin/src/features/color/ColorSlice.jsx
+++ admin/src/features/color/ColorSlice.jsx
@@ -14,8 +14,23 @@ export const getColors = createAsyncThunk(
   }
 );
 
+export const createColor = createAsyncThunk(
+  "colors/create-color",
+  async (color, thunkAPI) => {
+    try {
+      console.log("thunkAPI in colorSlice is : ", thunkAPI);
+      console.log("color in colorSlice is : ", color);
+      const response = await Colorservice.createColor(color);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 const initialState = {
   colors: [],
+  createdColor: {},
   isError: false,
   isLoading: false,
   isSuccess: false,
@@ -44,6 +59,29 @@ export const colorSlice = createSlice({
       })
       // ;builder
       .addCase(getColors.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      // ;builder
+      .addCase(createColor.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(createColor.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createdColor = action.payload.data;
+        console.log(
+          "action.payload in created colorSlice is : ",
+          action.payload.data
+        );
+      })
+      // ;builder
+      .addCase(createColor.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.isSuccess = false;
