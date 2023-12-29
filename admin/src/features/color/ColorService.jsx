5ec574cde228be6ca867e24c4873ec628a9b5382
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-29 06:20:07 +0530

    feat: created color creation service

--- admin/src/features/color/ColorService.jsx
+++ admin/src/features/color/ColorService.jsx
@@ -16,8 +16,24 @@ const getColors = async () => {
   return response.data;
 };
 
+const createColor = async (color) => {
+  const token = Token;
+  console.log("token in colorService is : ", token);
+  console.log("color in colorService is : ", color);
+  const url = `${base_url}colors/`;
+
+  const response = await axios.post(url, color, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  console.log("Response in colorService is : ", response);
+
+  return response.data;
+};
+
 const Colorservice = {
   getColors,
+  createColor,
 };
 
 export default Colorservice; // export the service
