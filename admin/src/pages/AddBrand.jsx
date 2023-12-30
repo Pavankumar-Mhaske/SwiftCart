936f56bdce5ee1726d098418ba3b96fc4650e2c7
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-31 04:46:21 +0530

    chore: api integration for the updating the brand... - gaand lagi thi bhai 😂😂, raat ko 4:45 baje bug solve juua hein 😅😅

--- admin/src/pages/AddBrand.jsx
+++ admin/src/pages/AddBrand.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import CustomInput from "../components/CustomInput";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import * as yup from "yup";
 import { useFormik } from "formik";
 import {
@@ -10,31 +10,59 @@ import {
   showToastError,
   Toast,
 } from "../utils/HotToastHandler";
-import { createBrand, resetState } from "../features/brand/BrandSlice";
+import {
+  createBrand,
+  getABrand,
+  resetState,
+  updateBrand,
+} from "../features/brand/BrandSlice";
+
 // ❗❗❗❗❗❗❗❗❗❗   yup Validations          ❗❗❗❗❗❗❗❗❗❗
 let schema = yup.object().shape({
   name: yup.string().required("Brand name is required"),
 });
 
 const AddBrand = () => {
+  const location = useLocation();
+  const getBrandId = location.pathname.split("/")[3];
+  console.log("getBrandId in AddBrand is : ", getBrandId);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [loadingToastId, setLoadingToastId] = useState(null);
+  const [loadingCreateToastId, setLoadingCreateToastId] = useState(null);
+  const [loadingUpdateToastId, setLoadingUpdateToastId] = useState(null);
 
   const newBrand = useSelector((state) => state.brand);
-  const { createdBrand, isSuccess, isLoading, isError } = newBrand;
-  // console.log("createdBrand in AddBrand is : ", createdBrand);
+  const { createdBrand, isSuccess, isLoading, isError, brand, updatedBrand } =
+    newBrand;
+  console.log("brand in AddBrand is : ", brand);
+
+  useEffect(() => {
+    if (getBrandId !== undefined) {
+      dispatch(getABrand(getBrandId));
+    } else {
+      dispatch(resetState());
+    }
+  }, [getBrandId]);
+
   useEffect(() => {
     if (formik.isSubmitting) {
-      if (isSuccess && createdBrand) {
-        console.log("toastId : ", loadingToastId);
-        showToastSuccess("Brand Created Successfully", loadingToastId);
-      }
-      if (isError) {
+      console.log(`formik is submitting ❤❤🤍❤💙💙💚💛🧡❤
+      createdBrand: ${createdBrand},isSuccess: ${isSuccess}, isLoading:${isLoading}, isError:${isError}, brand:${brand}, updatedBrand:${updatedBrand}
+      `);
+      if (isSuccess && createdBrand && Object.keys(createdBrand).length > 0) {
+        showToastSuccess("Brand Created Successfully", loadingCreateToastId);
+      } else if (
+        isSuccess &&
+        updatedBrand &&
+        Object.keys(updatedBrand).length > 0
+      ) {
+        showToastSuccess("Brand Updated Successfully", loadingUpdateToastId);
+      } else if (isError) {
         showToastError("Brand Creation Failed");
       }
     }
-  }, [createdBrand, isSuccess, isLoading, isError]);
+  }, [createdBrand, updatedBrand]);
 
   // async functions for dispatching createBrand
   const handleCreateBrand = async (values) => {
@@ -45,33 +73,56 @@ const AddBrand = () => {
       console.log("error : ", error);
     }
   };
+
+  // async functions for dispatching createBrand
+  const handleUpdateBrand = async (values) => {
+    try {
+      const response = await dispatch(updateBrand(values));
+      console.log("response : ", response);
+    } catch (error) {
+      console.log("error : ", error);
+    }
+  };
   const initialValues = {
-    name: "",
+    name: brand?.name || "",
   };
 
   const formik = useFormik({
+    enableReinitialize: true,
     initialValues: initialValues,
     validationSchema: schema,
     onSubmit: async (values) => {
-      const toastId = showToastLoading("Creating Brand");
-      setLoadingToastId(toastId);
+      console.log("getBrandId in AddBrand is given by🚁🚁 : ", getBrandId);
+      if (getBrandId !== undefined) {
+        const toastId = showToastLoading("updating Brand");
+        setLoadingUpdateToastId(toastId);
+        const data = {
+          brandId: getBrandId,
+          name: values.name,
+        };
+        await handleUpdateBrand(data);
+      } else {
+        const toastId = showToastLoading("Creating Brand");
+        setLoadingCreateToastId(toastId);
+        await handleCreateBrand(values);
+      }
       console.log("values : ", values);
       // alert(JSON.stringify(values, null, 2));
       console.log("form is submited 🚚🚚🚚🚚🚚🚚🚚🚚🚚🚚");
-      await handleCreateBrand(values);
       formik.resetForm();
-      // showToastSuccess("Brand Created Successfully", toastId);
-      // setTimeout(() => {
-      //   dispatch(resetState());
-      //   navigate("/admin/brand-list");
-      // }, 3000);
+      dispatch(resetState());
+      setTimeout(() => {
+        navigate("/admin/brand-list");
+      }, 3000);
     },
   });
 
   return (
     <div>
       <Toast />
-      <h3 className="mb-4 title">Add Brand Category</h3>
+      <h3 className="mb-4 title">
+        {getBrandId !== undefined ? `Edit` : `Add`} Brand
+      </h3>
       <div>
         <form action="" onSubmit={formik.handleSubmit}>
           <CustomInput
