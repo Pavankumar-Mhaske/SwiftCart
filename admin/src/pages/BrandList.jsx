7daabb3c917745d32a7b7c1a31bb90efbe7dbdcc
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-12-31 18:55:17 +0530

    feat: added delete functionality with the toast as well

--- admin/src/pages/BrandList.jsx
+++ admin/src/pages/BrandList.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Table, Tag } from "antd";
 import { BiEdit } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
-import { useEffect } from "react";
+
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { getBrands } from "../features/brand/BrandSlice";
+import { deleteBrand, getBrands } from "../features/brand/BrandSlice";
+import CustomModal from "../components/CustomModal";
+import {
+  showToastLoading,
+  showToastSuccess,
+  showToastError,
+  Toast,
+} from "../utils/HotToastHandler";
 
 const columns = [
   {
@@ -40,6 +47,40 @@ const BrandList = () => {
     dispatch(getBrands());
   }, []);
 
+  // deleting functionality
+  const [loadingDeleteToastId, setLoadingDeleteToastId] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [deleteBrandId, setDeleteBrandId] = useState("");
+  const showModal = (brandId) => {
+    setOpen(true);
+    setDeleteBrandId(brandId);
+  };
+  console.log("brandId in brandList is Id: ", deleteBrandId);
+  const hideModal = () => {
+    setOpen(false);
+  };
+  const deleteBrandHelper = (brandId) => {
+    const toastId = showToastLoading("Deleting Brand!");
+    setLoadingDeleteToastId(toastId);
+    console.log("deleteBrand is called");
+    dispatch(deleteBrand(brandId));
+    setOpen(false);
+    // hideModal();
+  };
+
+  const newBrand = useSelector((state) => state.brand);
+  const { isSuccess, isError, deletedBrand } = newBrand;
+  console.log("deletedBrand in BrandList is : ", deletedBrand);
+
+  useEffect(() => {
+    if (isSuccess && deletedBrand && Object.keys(deletedBrand).length > 0) {
+      showToastSuccess("Brand Deleted Successfully", loadingDeleteToastId);
+      dispatch(getBrands());
+    } else if (isError) {
+      showToastError("Brand Deletion Failed");
+    }
+  }, [deletedBrand]);
+
   const brandState = useSelector((state) => state.brand.brands);
   console.log("brandState in brandList is : ", brandState);
 
@@ -54,9 +95,12 @@ const BrandList = () => {
           <Link to={`/admin/brand/${brandState[i]._id}`}>
             <BiEdit className="fs-5 ms-3 me-5 " />
           </Link>
-          <Link to="#">
+          <button
+            onClick={() => showModal(brandState[i]._id)}
+            className="bg-transparent border-0"
+          >
             <MdDelete className="fs-5 ms-3 me-5 text-danger" />
-          </Link>
+          </button>
         </>
       ),
     });
@@ -64,10 +108,19 @@ const BrandList = () => {
 
   return (
     <div>
+      <Toast />
       <h3 className="mb-4 title">BrandList</h3>
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
+      <CustomModal
+        title="Are You Sure to Delete This Brand?"
+        hideModal={hideModal}
+        open={open}
+        performAction={() => {
+          deleteBrandHelper(deleteBrandId);
+        }}
+      />
     </div>
   );
 };
