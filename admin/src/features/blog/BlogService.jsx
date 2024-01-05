265b727901208a175f6f535fb479de7136f4ca8b
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-05 21:40:25 +0530

    chore: added the functionality for the updation  and deletion of the blogs

--- admin/src/features/blog/BlogService.jsx
+++ admin/src/features/blog/BlogService.jsx
@@ -31,9 +31,60 @@ const createBlog = async (blog) => {
   return response.data;
 };
 
+const getABlog = async (blogId) => {
+  const token = Token;
+
+  console.log("token in BlogService is : ", token);
+  const url = `${base_url}blogs/${blogId}`;
+  const response = await axios.get(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  //   const response = await axios.get(url);
+  console.log("Response in BlogService is : ", response);
+
+  return response.data;
+};
+
+const updateBlog = async (data) => {
+  const token = Token;
+
+  console.log("token in update blogService is : ", token);
+  console.log("data in update blogService is : ", data);
+  const { blogId, title, description, category } = data;
+  console.log("blogId and name in update blogService is : ", blogId, name);
+  const url = `${base_url}blogs/${blogId}`;
+  const response = await axios.patch(
+    url,
+    { title: title, description: description, category: category },
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+
+  console.log("Response in blogService is : ", response);
+
+  return response.data;
+};
+
+const deleteBlog = async (blogId) => {
+  const token = Token;
+  console.log("token in BlogService is : ", token);
+  const url = `${base_url}blogs/${blogId}`;
+  const response = await axios.delete(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  //   const response = await axios.get(url);
+  console.log("Response in BlogService is : ", response);
+
+  return response.data;
+};
+
 const Blogservice = {
   getBlogs,
   createBlog,
+  getABlog,
+  updateBlog,
+  deleteBlog,
 };
 
 export default Blogservice; // export the service
