f154c5662da4755d1415d906029647dbab587f2d
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-05 22:28:15 +0530

    chore: added the functionality for the update and delete BlogCategory

--- admin/src/features/blog-category/BlogCategoryService.jsx
+++ admin/src/features/blog-category/BlogCategoryService.jsx
@@ -31,9 +31,64 @@ const createBlogCategory = async (BlogCategory) => {
   return response.data;
 };
 
+const getABlogCategory = async (blogCategoryId) => {
+  const token = Token;
+
+  console.log("token in BlogCategoryService is : ", token);
+  const url = `${base_url}blog-categories/${blogCategoryId}`;
+  const response = await axios.get(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  //   const response = await axios.get(url);
+  console.log("Response in BlogCategoryService is : ", response);
+
+  return response.data;
+};
+
+const updateBlogCategory = async (data) => {
+  const token = Token;
+
+  console.log("token in update blogCategoryService is : ", token);
+  console.log("data in update blogCategoryService is : ", data);
+  const { blogCategoryId, name } = data;
+  console.log(
+    "blogCategoryId and name in update blogCategoryService is : ",
+    blogCategoryId,
+    name
+  );
+  const url = `${base_url}blog-categories/${blogCategoryId}`;
+  const response = await axios.patch(
+    url,
+    { name: name },
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+
+  console.log("Response in blogCategoryService is : ", response);
+
+  return response.data;
+};
+
+const deleteBlogCategory = async (blogCategoryId) => {
+  const token = Token;
+  console.log("token in BlogCategoryService is : ", token);
+  const url = `${base_url}blog-categories/${blogCategoryId}`;
+  const response = await axios.delete(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  //   const response = await axios.get(url);
+  console.log("Response in BlogCategoryService is : ", response);
+
+  return response.data;
+};
+
 const BlogCategoryService = {
   getBlogCategories,
   createBlogCategory,
+  getABlogCategory,
+  updateBlogCategory,
+  deleteBlogCategory,
 };
 
 export default BlogCategoryService; // export the service
