33a90c0bb3961afd4e786350d0a7b3d154c69f8e
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-05 18:42:08 +0530

    feat: created functionality for updation

--- admin/src/pages/AddProductCategory.jsx
+++ admin/src/pages/AddProductCategory.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import CustomInput from "../components/CustomInput";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import * as yup from "yup";
 import { useFormik } from "formik";
 import {
@@ -10,7 +10,12 @@ import {
   showToastError,
   Toast,
 } from "../utils/HotToastHandler";
-import { createProductCategory } from "../features/product-category/ProductCategorySlice";
+import {
+  createProductCategory,
+  getAProductCategory,
+  resetState,
+  updateProductCategory,
+} from "../features/product-category/ProductCategorySlice";
 
 // ❗❗❗❗❗❗❗❗❗❗   yup Validations          ❗❗❗❗❗❗❗❗❗❗
 let schema = yup.object().shape({
@@ -18,28 +23,65 @@ let schema = yup.object().shape({
 });
 
 const AddProductCategory = () => {
+  const location = useLocation();
+  const getProductCategoryId = location.pathname.split("/")[3];
+  console.log(
+    "getProductCategoryId in AddProductCategory is : ",
+    getProductCategoryId
+  );
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [loadingToastId, setLoadingToastId] = useState(null);
+  const [loadingCreateToastId, setLoadingCreateToastId] = useState(null);
+  const [loadingUpdateToastId, setLoadingUpdateToastId] = useState(null);
 
   const newProductCategory = useSelector((state) => state.productCategory);
-  const { createdProductCategory, isSuccess, isLoading, isError } =
-    newProductCategory;
+  const {
+    createdProductCategory,
+    isSuccess,
+    isLoading,
+    isError,
+    productCategory,
+    updatedProductCategory,
+  } = newProductCategory;
+  console.log("productCategory in AddProductCategory is : ", productCategory);
+
+  useEffect(() => {
+    if (getProductCategoryId !== undefined) {
+      dispatch(getAProductCategory(getProductCategoryId));
+    } else {
+      dispatch(resetState());
+    }
+  }, [getProductCategoryId]);
 
   useEffect(() => {
     if (formik.isSubmitting) {
-      if (isSuccess && createdProductCategory) {
-        console.log("loadingToastId 💘💘 : ", loadingToastId);
+      console.log(`formik is submitting ❤❤🤍❤💙💙💚💛🧡❤
+        createdProductCategory: ${createdProductCategory},isSuccess: ${isSuccess}, isLoading:${isLoading}, isError:${isError}, productCategory:${productCategory}, updatedProductCategory:${updatedProductCategory}
+        `);
+      if (
+        isSuccess &&
+        createdProductCategory &&
+        Object.keys(createdProductCategory).length > 0
+      ) {
         showToastSuccess(
           "ProductCategory Created Successfully",
-          loadingToastId
+          loadingCreateToastId
         );
-      }
-      if (isError) {
+      } else if (
+        isSuccess &&
+        updatedProductCategory &&
+        Object.keys(updatedProductCategory).length > 0
+      ) {
+        showToastSuccess(
+          "ProductCategory Updated Successfully",
+          loadingUpdateToastId
+        );
+      } else if (isError) {
         showToastError("ProductCategory Creation Failed");
       }
     }
-  }, [createdProductCategory, isSuccess, isLoading, isError]);
+  }, [createdProductCategory, updatedProductCategory]);
 
   // async functions for dispatching createProductCategory
   const handleCreateProductCategory = async (values) => {
@@ -50,33 +92,60 @@ const AddProductCategory = () => {
       console.log("error : ", error);
     }
   };
+
+  // async functions for dispatching updateProductCategory
+  const handleUpdateProductCategory = async (values) => {
+    try {
+      const response = await dispatch(updateProductCategory(values));
+      console.log("response : ", response);
+    } catch (error) {
+      console.log("error : ", error);
+    }
+  };
   const initialValues = {
-    name: "",
+    name: productCategory?.name || "",
   };
 
   const formik = useFormik({
+    enableReinitialize: true,
     initialValues: initialValues,
     validationSchema: schema,
     onSubmit: async (values) => {
-      const toastId = showToastLoading("Creating ProductCategory");
-      console.log("toastId during formik operation 🚚🚚 : ", toastId);
-      setLoadingToastId(toastId);
+      console.log(
+        "getProductCategoryId in AddProductCategory is given by🚁🚁 : ",
+        getProductCategoryId
+      );
+      if (getProductCategoryId !== undefined) {
+        const toastId = showToastLoading("updating ProductCategory");
+        setLoadingUpdateToastId(toastId);
+        const data = {
+          productCategoryId: getProductCategoryId,
+          name: values.name,
+        };
+        await handleUpdateProductCategory(data);
+      } else {
+        const toastId = showToastLoading("Creating ProductCategory");
+        setLoadingCreateToastId(toastId);
+        await handleCreateProductCategory(values);
+      }
       console.log("values : ", values);
       // alert(JSON.stringify(values, null, 2));
       console.log("form is submited 🚚🚚🚚🚚🚚🚚🚚🚚🚚🚚");
-      await handleCreateProductCategory(values);
       formik.resetForm();
-      // showToastSuccess("ProductCategory Created Successfully", toastId);
-      // setTimeout(() => {
-      //   navigate("/admin/product-category-list");
-      // }, 3000);
+      dispatch(resetState());
+      setTimeout(() => {
+        navigate("/admin/product-category-list");
+      }, 500);
     },
   });
 
   return (
     <div>
       <Toast />
-      <h3 className="mb-4 title">Add Product Category</h3>
+      <h3 className="mb-4 title">
+        {getProductCategoryId !== undefined ? "Update" : "Add"}
+        ProductCategory
+      </h3>
       <div>
         <form action="" onSubmit={formik.handleSubmit}>
           <CustomInput
@@ -95,7 +164,8 @@ const AddProductCategory = () => {
             className="btn btn-success border-0 rounded-3 my-5"
             type="submit"
           >
-            Add Product Category
+            {getProductCategoryId !== undefined ? "Update" : "Add"}
+            ProductCategory
           </button>
         </form>
       </div>
