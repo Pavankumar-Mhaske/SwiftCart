6a00a83d7926f30599ce529a018e56dc6462ea29
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-05 18:40:55 +0530

    feat: created the functionality for the updation and deletion

--- admin/src/features/color/ColorSlice.jsx
+++ admin/src/features/color/ColorSlice.jsx
@@ -1,12 +1,25 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import Colorservice from "./ColorService";
+import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import ColorService from "./ColorService";
 
 export const getColors = createAsyncThunk(
   "colors/get-colors",
   async (thunkAPI) => {
     try {
       console.log("thunkAPI in colorSlice is : ", thunkAPI);
-      const response = await Colorservice.getColors();
+      const response = await ColorService.getColors();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const getAColor = createAsyncThunk(
+  "colors/get-color",
+  async (colorId, thunkAPI) => {
+    try {
+      console.log("thunkAPI in ColorSlice is : ", thunkAPI);
+      const response = await ColorService.getAColor(colorId);
       return response;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -20,7 +33,7 @@ export const createColor = createAsyncThunk(
     try {
       console.log("thunkAPI in colorSlice is : ", thunkAPI);
       console.log("color in colorSlice is : ", color);
-      const response = await Colorservice.createColor(color);
+      const response = await ColorService.createColor(color);
       return response;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -28,9 +41,42 @@ export const createColor = createAsyncThunk(
   }
 );
 
+export const updateColor = createAsyncThunk(
+  "colors/update-color",
+  async (color, thunkAPI) => {
+    try {
+      console.log("thunkAPI in colorSlice is : ", thunkAPI);
+      console.log("color in colorSlice is : ", color);
+      const response = await ColorService.updateColor(color);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const deleteColor = createAsyncThunk(
+  "colors/delete-color",
+  async (colorId, thunkAPI) => {
+    try {
+      console.log("thunkAPI in ColorSlice is : ", thunkAPI);
+      const response = await ColorService.deleteColor(colorId);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+
+export const resetState = createAction("reset_all");
+
 const initialState = {
   colors: [],
+  color: {},
   createdColor: {},
+  updatedColor: {},
+  deletedColor: {},
   isError: false,
   isLoading: false,
   isSuccess: false,
@@ -87,7 +133,77 @@ export const colorSlice = createSlice({
         state.isSuccess = false;
         // state.user = null;
         state.message = action.error;
-      });
+      })
+      // ;builder
+      .addCase(getAColor.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(getAColor.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.color = action.payload.data;
+        console.log(
+          "action.payload in getAColor colorSlice is : ",
+          action.payload.data
+        );
+      })
+      // ;builder
+      .addCase(getAColor.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      // ;builder
+      .addCase(updateColor.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(updateColor.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.updatedColor = action.payload.data;
+        console.log(
+          "action.payload in updated colorSlice is : ",
+          action.payload.data
+        );
+      })
+      // ;builder
+      .addCase(updateColor.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      // ;builder
+      .addCase(deleteColor.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(deleteColor.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.deletedColor = action.payload.data;
+        console.log(
+          "action.payload in deleted colorSlice is : ",
+          action.payload.data
+        );
+      })
+      // ;builder
+      .addCase(deleteColor.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      .addCase(resetState, () => initialState);
   },
 });
 
