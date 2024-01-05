971a9b7dfc6970a75ea24d63a9fcd82489c31035
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-05 21:40:31 +0530

    chore: added the functionality for the updation  and deletion of the blogs

--- admin/src/features/blog/BlogSlice.jsx
+++ admin/src/features/blog/BlogSlice.jsx
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import Blogservice from "./BlogService";
 
 export const getBlogs = createAsyncThunk(
@@ -7,6 +7,19 @@ export const getBlogs = createAsyncThunk(
     try {
       console.log("thunkAPI in blogSlice is : ", thunkAPI);
       const response = await Blogservice.getBlogs();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const getABlog = createAsyncThunk(
+  "blogs/get-blog",
+  async (blogId, thunkAPI) => {
+    try {
+      console.log("thunkAPI in blogSlice is : ", thunkAPI);
+      const response = await Blogservice.getABlog(blogId);
       return response;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -28,9 +41,41 @@ export const createBlog = createAsyncThunk(
   }
 );
 
+export const updateBlog = createAsyncThunk(
+  "blogs/update-blog",
+  async (blog, thunkAPI) => {
+    try {
+      console.log("thunkAPI in blogSlice is : ", thunkAPI);
+      console.log("blog in blogSlice is : ", blog);
+      const response = await Blogservice.updateBlog(blog);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const deleteBlog = createAsyncThunk(
+  "blogs/delete-blog",
+  async (blogId, thunkAPI) => {
+    try {
+      console.log("thunkAPI in blogSlice is : ", thunkAPI);
+      const response = await Blogservice.deleteBlog(blogId);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const resetState = createAction("reset_all");
+
 const initialState = {
   blogs: [],
+  blog: {},
   createdBlog: {},
+  updatedBlog: {},
+  deletedBlog: {},
   isError: false,
   isLoading: false,
   isSuccess: false,
@@ -87,7 +132,77 @@ export const blogSlice = createSlice({
         state.isSuccess = false;
         // state.user = null;
         state.message = action.error;
-      });
+      })
+      // ;builder
+      .addCase(getABlog.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(getABlog.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.blog = action.payload.data;
+        console.log(
+          "action.payload in getABlog blogSlice is : ",
+          action.payload.data
+        );
+      })
+      // ;builder
+      .addCase(getABlog.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      // ;builder
+      .addCase(updateBlog.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(updateBlog.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.updatedBlog = action.payload.data;
+        console.log(
+          "action.payload in updateBlog blogSlice is : ",
+          action.payload.data
+        );
+      })
+      // ;builder
+      .addCase(updateBlog.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      // ;builder
+      .addCase(deleteBlog.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(deleteBlog.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.deletedBlog = action.payload.data;
+        console.log(
+          "action.payload in deleteBlog blogSlice is : ",
+          action.payload.data
+        );
+      })
+      // ;builder
+      .addCase(deleteBlog.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      .addCase(resetState, () => initialState);
   },
 });
 
