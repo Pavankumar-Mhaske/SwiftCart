93db05b0a9f2ff47fe1b1b9a601b0a3ef53d8cdf
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-05 18:41:16 +0530

    feat: created the functionality for the updation and deletion

--- admin/src/features/product-category/ProductCategoyService.jsx
+++ admin/src/features/product-category/ProductCategoyService.jsx
@@ -19,9 +19,12 @@ const getProductCategories = async () => {
 const createProductCategory = async (productCategory) => {
   const token = Token;
   console.log("token in productCategoryService is : ", token);
-  console.log("productCategory in productCategoryService is : ", productCategory);
+  console.log(
+    "productCategory in productCategoryService is : ",
+    productCategory
+  );
   const url = `${base_url}product-categories/`;
-  
+
   const response = await axios.post(url, productCategory, {
     headers: { Authorization: `Bearer ${token}` },
   });
@@ -31,10 +34,64 @@ const createProductCategory = async (productCategory) => {
   return response.data;
 };
 
+const getAProductCategory = async (productCategoryId) => {
+  const token = Token;
+
+  console.log("token in ProductCategoryService is : ", token);
+  const url = `${base_url}product-categories/${productCategoryId}`;
+  const response = await axios.get(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  //   const response = await axios.get(url);
+  console.log("Response in ProductCategoryService is : ", response);
+
+  return response.data;
+};
+
+const updateProductCategory = async (data) => {
+  const token = Token;
+
+  console.log("token in update productCategoryService is : ", token);
+  console.log("data in update productCategoryService is : ", data);
+  const { productCategoryId, name } = data;
+  console.log(
+    "productCategoryId and name in update productCategoryService is : ",
+    productCategoryId,
+    name
+  );
+  const url = `${base_url}product-categories/${productCategoryId}`;
+  const response = await axios.patch(
+    url,
+    { name: name },
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+
+  console.log("Response in ProductCategoryService is : ", response);
+
+  return response.data;
+};
+
+const deleteProductCategory = async (productCategoryId) => {
+  const token = Token;
+  console.log("token in ProductCategoryService is : ", token);
+  const url = `${base_url}product-categories/${productCategoryId}`;
+  const response = await axios.delete(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  //   const response = await axios.get(url);
+  console.log("Response in ProductCategoryService is : ", response);
+
+  return response.data;
+};
 
 const ProductCategoryService = {
-    getProductCategories,
-    createProductCategory,
+  getProductCategories,
+  createProductCategory,
+  getAProductCategory,
+  updateProductCategory,
+  deleteProductCategory,
 };
 
 export default ProductCategoryService; // export the service
