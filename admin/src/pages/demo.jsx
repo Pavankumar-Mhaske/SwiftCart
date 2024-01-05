31e91c5a90613f04f35785d3df28328651934364
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-05 18:43:16 +0530

    testing...

---
+++ admin/src/pages/demo.jsx
@@ -0,0 +1,16 @@
+{
+    "data": {
+        "statusCode": 200,
+        "data": {
+            "_id": "6591bda462b3c9e20054270f",
+            "name": "vvvvvvvv",
+            "owner": "6527f0f9a5cb3daee993580c",
+            "createdAt": "2023-12-31T19:14:44.049Z",
+            "updatedAt": "2023-12-31T20:28:59.940Z",
+            "__v": 0
+        },
+        "message": "ProductCategory fetched successfully",
+        "success": true
+    },
+    
+}
