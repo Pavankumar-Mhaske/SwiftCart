485c06d43d9fa926e39e934d008068a4970cdbfa
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-05 21:41:29 +0530

    chore: added route for the updation of the Blog

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -34,6 +34,7 @@ function App() {
           <Route index element={<Dashboard />} />
           <Route path="enquiries" element={<Enquiries />} />
           <Route path="blog" element={<AddBlog />} />
+          <Route path="blog/:id" element={<AddBlog />} />
           <Route path="blog-list" element={<BlogList />} />
           <Route path="coupon" element={<AddCoupon />} />
           <Route path="coupon-list" element={<CouponList />} />
