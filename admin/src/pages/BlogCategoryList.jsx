bfef38a9a8ba811d5d5a75fd180a7f53569c0c84
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-05 22:28:52 +0530

    chore: added functionality for the deleting blogCategory

--- admin/src/pages/BlogCategoryList.jsx
+++ admin/src/pages/BlogCategoryList.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Table, Tag } from "antd";
 import { BiEdit } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { getBlogCategories } from "../features/blog-category/BlogCategorySlice";
+import {
+  deleteBlogCategory,
+  getBlogCategories,
+} from "../features/blog-category/BlogCategorySlice";
+import CustomModal from "../components/CustomModal";
+import {
+  showToastLoading,
+  showToastSuccess,
+  showToastError,
+  Toast,
+} from "../utils/HotToastHandler";
 
 const columns = [
   {
@@ -30,16 +39,6 @@ const columns = [
   {
     title: "Action",
     dataIndex: "action",
-    render: () => (
-      <>
-        <Link to="#">
-          <BiEdit className="fs-5 me-2 " />
-        </Link>
-        <Link to="#">
-          <MdDelete className="fs-5 text-danger" />
-        </Link>
-      </>
-    ),
   },
 ];
 
@@ -48,6 +47,53 @@ const BlogCategoryList = () => {
   useEffect(() => {
     dispatch(getBlogCategories());
   }, []);
+
+  // deleting functionality
+  const [loadingDeleteToastId, setLoadingDeleteToastId] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [deleteBlogCategoryId, setDeleteBlogCategoryId] = useState("");
+  const showModal = (blogCategoryId) => {
+    setOpen(true);
+    setDeleteBlogCategoryId(blogCategoryId);
+  };
+  console.log(
+    "blogCategoryId in blogCategoryList is Id: ",
+    deleteBlogCategoryId
+  );
+  const hideModal = () => {
+    setOpen(false);
+  };
+  const deleteBlogCategoryHelper = (blogCategoryId) => {
+    const toastId = showToastLoading("Deleting BlogCategory!");
+    setLoadingDeleteToastId(toastId);
+    console.log("deleteBlogCategory is called");
+    dispatch(deleteBlogCategory(blogCategoryId));
+    setOpen(false);
+    // hideModal();
+  };
+
+  const newBlogCategory = useSelector((state) => state.blogCategory);
+  const { isSuccess, isError, deletedBlogCategory } = newBlogCategory;
+  console.log(
+    "deletedBlogCategory in BlogCategoryList is : ",
+    deletedBlogCategory
+  );
+
+  useEffect(() => {
+    if (
+      isSuccess &&
+      deletedBlogCategory &&
+      Object.keys(deletedBlogCategory).length > 0
+    ) {
+      showToastSuccess(
+        "BlogCategory Deleted Successfully",
+        loadingDeleteToastId
+      );
+      dispatch(getBlogCategories());
+    } else if (isError) {
+      showToastError("BlogCategory Deletion Failed");
+    }
+  }, [deletedBlogCategory]);
 
   const blogCategoryState = useSelector(
     (state) => state.blogCategory.blogCategories
@@ -60,16 +106,37 @@ const BlogCategoryList = () => {
       key: i + 1,
       blogCategory: blogCategoryState[i].name,
       owner: blogCategoryState[i].owner,
-      action: "action",
+      action: (
+        <>
+          <Link to={`/admin/blog-category/${blogCategoryState[i]._id}`}>
+            <BiEdit className="fs-5 me-2 " />
+          </Link>
+          <button
+            onClick={() => showModal(blogCategoryState[i]._id)}
+            className="bg-transparent border-0"
+          >
+            <MdDelete className="fs-5  text-danger" />
+          </button>
+        </>
+      ),
     });
   }
 
   return (
     <div>
+      <Toast />
       <h3 className="mb-4 title">Blog Category List</h3>
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
+      <CustomModal
+        title="Are You Sure to Delete This BlogCategory?"
+        hideModal={hideModal}
+        open={open}
+        performAction={() => {
+          deleteBlogCategoryHelper(deleteBlogCategoryId);
+        }}
+      />
     </div>
   );
 };
