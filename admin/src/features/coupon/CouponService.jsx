6d0bf9faf517ff5af0dd191c848fb3fc0708b6d7
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-06 19:08:06 +0530

    feat: added services for updation and deletion

--- admin/src/features/coupon/CouponService.jsx
+++ admin/src/features/coupon/CouponService.jsx
@@ -31,9 +31,88 @@ const createCoupon = async (coupon) => {
   return response.data;
 };
 
+const getACoupon = async (couponId) => {
+  const token = Token;
+
+  console.log("token in CouponService is : ", token);
+  const url = `${base_url}coupons/${couponId}`;
+  const response = await axios.get(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  //   const response = await axios.get(url);
+  console.log("Response in CouponService is : ", response);
+
+  return response.data;
+};
+
+const updateCoupon = async (data) => {
+  const token = Token;
+
+  console.log("token in update couponService is : ", token);
+  console.log("data in update couponService is : ", data);
+  const {
+    couponId,
+    name,
+    couponCode,
+    type,
+    discountValue,
+    isActive,
+    minimumCartValue,
+    startDate,
+    expiryDate,
+  } = data;
+
+  console.log(
+    "couponId and name in update couponService is : ",
+    couponId,
+    name,
+    couponCode,
+    type,
+    discountValue,
+    isActive,
+    minimumCartValue,
+    startDate,
+    expiryDate
+  );
+  const couponToUpdate = {
+    name: name,
+    couponCode: couponCode,
+    type: type,
+    discountValue: discountValue,
+    isActive: isActive,
+    minimumCartValue: minimumCartValue,
+    startDate: startDate,
+    expiryDate: expiryDate,
+  };
+  const url = `${base_url}coupons/${couponId}`;
+  const response = await axios.patch(url, couponToUpdate, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  console.log("Response in couponService is : ", response);
+
+  return response.data;
+};
+
+const deleteCoupon = async (couponId) => {
+  const token = Token;
+  console.log("token in CouponService is : ", token);
+  const url = `${base_url}coupons/${couponId}`;
+  const response = await axios.delete(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  //   const response = await axios.get(url);
+  console.log("Response in CouponService is : ", response);
+
+  return response.data;
+};
+
 const CouponService = {
   getCoupons,
   createCoupon,
+  getACoupon,
+  updateCoupon,
+  deleteCoupon,
 };
 
 export default CouponService; // export the service
