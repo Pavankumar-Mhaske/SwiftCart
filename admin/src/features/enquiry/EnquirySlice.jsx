5c439a49e10053351caa8ab969f5eaeebadee059
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-07 14:26:02 +0530

    feat: added deleteEnquiry feature

--- admin/src/features/enquiry/EnquirySlice.jsx
+++ admin/src/features/enquiry/EnquirySlice.jsx
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import EnquiryService from "./EnquiryService";
 
 export const getEnquiries = createAsyncThunk(
@@ -14,8 +14,23 @@ export const getEnquiries = createAsyncThunk(
   }
 );
 
+export const deleteEnquiry = createAsyncThunk(
+  "enquiries/delete-enquiry",
+  async (enquiryId, thunkAPI) => {
+    try {
+      console.log("thunkAPI in EnquirySlice is : ", thunkAPI);
+      const response = await EnquiryService.deleteEnquiry(enquiryId);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const resetState = createAction("reset_all");
 const initialState = {
   enquiries: [],
+  deletedEnquiry: {},
   isError: false,
   isLoading: false,
   isSuccess: false,
@@ -49,7 +64,32 @@ export const enquirySlice = createSlice({
         state.isSuccess = false;
         // state.user = null;
         state.message = action.error;
-      });
+      })
+      // ;builder
+      .addCase(deleteEnquiry.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(deleteEnquiry.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.deletedEnquiry = action.payload.data.deletedEnquiry;
+        console.log(
+          "action.payload in enquirySlice is :🧡🧡 ",
+          action.payload.data.deletedEnquiry
+        );
+      })
+      // ;builder
+      .addCase(deleteEnquiry.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      // ;builder
+      .addCase(resetState, () => initialState);
   },
 });
 
