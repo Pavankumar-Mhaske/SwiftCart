04582507773defa43b246e634200f5f235a97406
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-07 19:55:29 +0530

    chore: added ViewEnquiry route

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -22,6 +22,7 @@ import AddBrand from "./pages/AddBrand";
 import AddProduct from "./pages/AddProduct";
 import AddCoupon from "./pages/AddCoupon";
 import CouponList from "./pages/CouponList";
+import ViewEnquiry from "./pages/ViewEnquiry";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="enquiries" element={<Enquiries />} />
+          <Route path="enquiries/:id" element={<ViewEnquiry />} />
           <Route path="blog" element={<AddBlog />} />
           <Route path="blog/:id" element={<AddBlog />} />
           <Route path="blog-list" element={<BlogList />} />
