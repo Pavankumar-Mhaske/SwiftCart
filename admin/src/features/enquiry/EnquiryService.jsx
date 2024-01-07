a3b8d0089eb3fbab6bd8b83d940a02a86bd92222
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-07 14:25:52 +0530

    feat: added deleteEnquiry feature

--- admin/src/features/enquiry/EnquiryService.jsx
+++ admin/src/features/enquiry/EnquiryService.jsx
@@ -16,8 +16,22 @@ const getEnquiries = async () => {
   return response.data;
 };
 
+const deleteEnquiry = async (enquiryId) => {
+  const token = Token;
+  console.log("token in EnquiryService is : ", token);
+  const url = `${base_url}enquiries/${enquiryId}`;
+  const response = await axios.delete(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  //   const response = await axios.get(url);
+  console.log("Response in EnquiryService is : ", response);
+
+  return response.data;
+};
+
 const EnquiryService = {
   getEnquiries,
+  deleteEnquiry,
 };
 
 export default EnquiryService; // export the service
