6f17faaaa5dec903c4d7dcc807db179346d1771c
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-07 19:56:12 +0530

    chore: added features for updation and creation of the enquiry

--- admin/src/features/enquiry/EnquirySlice.jsx
+++ admin/src/features/enquiry/EnquirySlice.jsx
@@ -14,6 +14,47 @@ export const getEnquiries = createAsyncThunk(
   }
 );
 
+export const getAEnquiry = createAsyncThunk(
+  "enquiries/get-enquiry",
+  async (enquiryId, thunkAPI) => {
+    try {
+      console.log("thunkAPI in EnquirySlice is : ", thunkAPI);
+      const response = await EnquiryService.getAEnquiry(enquiryId);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const createEnquiry = createAsyncThunk(
+  "enquiries/create-enquiry",
+  async (enquiry, thunkAPI) => {
+    try {
+      console.log("thunkAPI in enquirySlice is : ", thunkAPI);
+      console.log("enquiry in enquirySlice is : ", enquiry);
+      const response = await EnquiryService.createEnquiry(enquiry);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const updateEnquiry = createAsyncThunk(
+  "enquiries/update-enquiry",
+  async (enquiry, thunkAPI) => {
+    try {
+      console.log("thunkAPI in enquirySlice is : ", thunkAPI);
+      console.log("enquiry in enquirySlice is : ", enquiry);
+      const response = await EnquiryService.updateEnquiry(enquiry);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 export const deleteEnquiry = createAsyncThunk(
   "enquiries/delete-enquiry",
   async (enquiryId, thunkAPI) => {
@@ -30,6 +71,9 @@ export const deleteEnquiry = createAsyncThunk(
 export const resetState = createAction("reset_all");
 const initialState = {
   enquiries: [],
+  enquiry: {},
+  createdEnquiry: {},
+  updatedEnquiry: {},
   deletedEnquiry: {},
   isError: false,
   isLoading: false,
@@ -59,6 +103,68 @@ export const enquirySlice = createSlice({
       })
       // ;builder
       .addCase(getEnquiries.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      // ;builder
+      .addCase(getAEnquiry.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAEnquiry.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.enquiry = action.payload.data;
+        console.log(
+          "action.payload in enquirySlice is : ",
+          action.payload.data
+        );
+      })
+      .addCase(getAEnquiry.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      // ;builder for create and update enquiry
+      .addCase(createEnquiry.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createEnquiry.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createdEnquiry = action.payload.data;
+        console.log(
+          "action.payload in enquirySlice is : ",
+          action.payload.data
+        );
+      })
+      .addCase(createEnquiry.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      .addCase(updateEnquiry.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateEnquiry.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.updatedEnquiry = action.payload.data;
+        console.log(
+          "action.payload in enquirySlice is : 💚💚💚💚 ",
+          action.payload.data
+        );
+      })
+      .addCase(updateEnquiry.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.isSuccess = false;
