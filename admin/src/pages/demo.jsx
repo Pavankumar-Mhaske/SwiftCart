24cb560f4baefa399b702dcc2fc32413a3dd9c18
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-07 19:57:01 +0530

    testing: all okay

---
+++ admin/src/pages/demo.jsx
@@ -0,0 +1,37 @@
+// _id(pin):"659a4189a974fe10deb6140d"
+// name(pin):"rani mhaske"
+// email(pin):"mhaskepavankumar@gmail.com"
+// mobile(pin):"8530470680"
+// comment(pin):"I want to know how muny day's it will take for exchange the product?"
+// owner(pin):"6527f0f9a5cb3daee993580c"
+// status(pin):"IN_PROGRESS"
+// createdAt(pin):"2024-01-07T06:15:37.433Z"
+// updatedAt(pin):"2024-01-07T09:23:13.523Z"
+// __v(pin):0
+
+// {
+//     "_id": "659a4189a974fe10deb6140d",
+//     "name": "rani mhaske",
+//     "email": "mhaskepavankumar@gmail.com",
+//     "mobile": "8530470680",
+//     "comment": "I want to know how muny day's it will take for exchange the product?",
+//     "owner": "6527f0f9a5cb3daee993580c",
+//     "status": "IN_PROGRESS",
+//     "createdAt": "2024-01-07T06:15:37.433Z",
+//     "updatedAt": "2024-01-07T09:23:13.523Z",
+//     "__v": 0
+// }
+
+`
+SUBMITTED,
+PENDING,
+AWAITING_RESPONSE,
+IN_PROGRESS,
+ON_HOLD,
+COMPLETED,
+CANCELED,
+CLOSED,
+ARCHIVED,
+
+`
+
