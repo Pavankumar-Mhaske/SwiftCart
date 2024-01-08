489f5de37a0d5f9bef174e66c98f04eb9557ef45
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-09 02:09:03 +0530

    chore: api integration with viewOrder page done

--- admin/src/pages/ViewOrder.jsx
+++ admin/src/pages/ViewOrder.jsx
@@ -5,8 +5,9 @@ import { BiEdit } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { getOrders } from "../features/order/OrderSlice";
+import { Link, useLocation } from "react-router-dom";
+import { getAOrder } from "../features/order/OrderSlice";
+import { getAColor } from "../features/color/ColorSlice";
 
 const columns = [
   {
@@ -15,95 +16,66 @@ const columns = [
   },
   // PaymentId
   {
-    title: "PaymentId",
-    dataIndex: "paymentId",
-  },
-  // IsPaymentDone
-  {
-    title: "P. Status",
-    dataIndex: "isPaymentDone",
-    render: (isPaymentDone) => {
-      let color = "";
-      let paymentDone = "";
-      switch (isPaymentDone) {
-        case true:
-          color = "green";
-          paymentDone = "Paid";
-          break;
-
-        case false:
-          color = "red";
-          paymentDone = "Unpaid";
-          break;
-
-        default:
-          color = "default";
-      }
-
-      return <Tag color={color}>{paymentDone}</Tag>;
-    },
-  },
-  // PaymentProvider
-  {
-    title: "P. Provider",
-    dataIndex: "paymentProvider",
-  },
-  // Status
-  {
-    title: "Status",
-    dataIndex: "status",
-    render: (status) => {
-      let color = "";
-
-      switch (status) {
-        case "PENDING":
-          color = "blue";
-          break;
-
-        case "CANCELLED":
-          color = "red";
-          break;
-
-        case "DELIVERED":
-          color = "green";
-          break;
-
-        default:
-          color = "default";
-      }
-
-      return <Tag color={color}>{status}</Tag>;
-    },
-  },
-  // OrderPrice
-  {
-    title: "OP",
-    dataIndex: "orderPrice",
-  },
-  // discountedOrderPrice
-  {
-    title: "DOP",
-    dataIndex: "discountedOrderPrice",
-  },
-  // coupon
-  {
-    title: "Coupon",
-    dataIndex: "coupon",
-  },
-  // // customer
-  // {
-  //   title: "Customer",
-  //   dataIndex: "customer",
-  // },
-  // product
-  {
     title: "Product",
     dataIndex: "product",
-    render: (orderId) => {
-      // return <Tag color="blue">{product}</Tag>;
-      // return link
-      return <Link to={`/admin/orders/${orderId}`}>View Order</Link>;
-    },
+  },
+  //   Quantity
+  {
+    title: "Quantity",
+    dataIndex: "quantity",
+  },
+  //   Brand
+  {
+    title: "Brand",
+    dataIndex: "brand",
+  },
+  //   Color
+  {
+    title: "Color",
+    dataIndex: "color",
+    // render an array of colors in tags, each on a new line
+    render: (colors) => (
+      <>
+        {colors?.map((Id, index) => (
+          <div key={index}>
+            <Tag color="green">{Id}</Tag>
+          </div>
+        ))}
+      </>
+    ),
+  },
+  //   Category
+  {
+    title: "Category",
+    dataIndex: "category",
+    // render a category inside tag...
+    render: (category) => (
+      <>
+        <div>
+          <Tag color="green">{category}</Tag>
+        </div>
+      </>
+    ),
+  },
+  //   Tags
+  {
+    title: "Tags",
+    dataIndex: "tags",
+    // render an array of tags in tags, each on a new line
+    render: (tags) => (
+      <>
+        {tags.map((tag, index) => (
+          <div key={index}>
+            <Tag color="green">{tag}</Tag>
+          </div>
+        ))}
+      </>
+    ),
+  },
+  //   Price
+  {
+    title: "Price",
+    dataIndex: "price",
   },
   // Action
   {
@@ -123,34 +95,71 @@ const columns = [
 ];
 
 const ViewOrder = () => {
+  const location = useLocation();
+  const getOrderId = location.pathname.split("/")[3];
+  console.log("getOrderId in ViewOrder is : ", getOrderId);
+
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getOrders());
+    dispatch(getAOrder(getOrderId));
   }, []);
 
-  const orderState = useSelector((state) => state.order.orders);
-  console.log("orderState in Orders is : ", orderState);
+  const orderState = useSelector((state) => state.order.order.items);
+  console.log("orderState in ViewOrder is :  ", orderState);
+  //   console.log(
+  //     "orderState.length in ViewOrder is 🌹🌹🌹 :  ",
+  //     ordervState.items
+  //   );
+  //   const orderState = ordervState.items;
+
+  //   function to getAColor from colorId in orderState
+
+  //   const colorState = useSelector((state) => state.color.color);
+  //   console.log("colorState in ViewOrder is 🌳🌳🌳 : ", colorState);
+
+  //   const getColor = async (colorId) => {
+  //     try {
+  //       const response = await dispatch(getAColor(colorId));
+  //     } catch (error) {
+  //       console.log("error in getColor is : ", error);
+  //     }
+  //   };
+
+  //   const getColorsHelper = async (colorsArray) => {
+  //     console.log("colorsArray in ViewOrder is : ", colorsArray);
+  //     const colors = [];
+  //     for (let j = 0; j < colorsArray?.length; j++) {
+  //       const colorName = await getColor(colorsArray[j]);
+  //       colors.push(colorName);
+  //     }
+  //     console.log("colors in ViewOrder is 🔴🟢🟡 : ", colors);
+  //     return colors;
+  //   };
 
   const data1 = [];
-  for (let i = 0; i < orderState.length; i++) {
+  for (let i = 0; i < orderState?.length; i++) {
+    // console.log(
+    //   "orderState[i].product.color in ViewOrder is : ",
+    //   orderState[i].product.colors
+    // );
+    // const colors = getColorsHelper(orderState[i].product.colors);
+    // console.log("colors in ViewOrder is : ", colors);
     data1.push({
       key: i + 1,
-      paymentId: orderState[i].paymentId,
-      isPaymentDone: orderState[i].isPaymentDone,
-      paymentProvider: orderState[i].paymentProvider,
-      status: orderState[i].status,
-      orderPrice: orderState[i].orderPrice,
-      discountedOrderPrice: orderState[i].discountedOrderPrice,
-      coupon: orderState[i].coupon?.couponCode,
-      // customer: orderState[i].customer?._id,
-      product: orderState[i]?._id,
+      product: orderState[i].product.name,
+      quantity: orderState[i].quantity,
+      brand: orderState[i].product.brand,
+      color: orderState[i].product.colors,
+      category: orderState[i].product.category,
+      tags: orderState[i].product.tags,
+      price: orderState[i].product.price,
       action: "action",
     });
   }
 
   return (
     <div>
-      <h3 className="mb-4 title">View Orders</h3>
+      <h3 className="mb-4 title">View Order</h3>
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
