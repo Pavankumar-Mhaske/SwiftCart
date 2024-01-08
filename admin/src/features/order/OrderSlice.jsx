b6a2df9c8bb89f2a24a80eec48b6ac5fed129975
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-09 02:08:14 +0530

    chore: added getAOrder facility

--- admin/src/features/order/OrderSlice.jsx
+++ admin/src/features/order/OrderSlice.jsx
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import OrderService from "./OrderService";
 
 export const getOrders = createAsyncThunk(
@@ -14,8 +14,23 @@ export const getOrders = createAsyncThunk(
   }
 );
 
+export const getAOrder = createAsyncThunk(
+  "orders/get-order",
+  async (orderId, thunkAPI) => {
+    try {
+      console.log("thunkAPI in OrderSlice is : ", thunkAPI);
+      const response = await OrderService.getAOrder(orderId);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const resetState = createAction("reset_all");
 const initialState = {
   orders: [],
+  order: {},
   isError: false,
   isLoading: false,
   isSuccess: false,
@@ -49,7 +64,31 @@ export const orderSlice = createSlice({
         state.isSuccess = false;
         // state.user = null;
         state.message = action.error;
-      });
+      })
+      // ;builder
+      .addCase(getAOrder.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(getAOrder.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.order = action.payload.data.order;
+        console.log(
+          "action.payload in orderSlice is :💚💚💚 ",
+          action.payload.data.order
+        );
+      })
+      // ;builder
+      .addCase(getAOrder.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      .addCase(resetState, () => initialState);
   },
 });
 
