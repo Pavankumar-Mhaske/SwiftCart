7c82468a954217d05f9356f448bec4434e6ea9ae
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-08 23:30:38 +0530

    testing...

---
+++ admin/src/pages/demo.jsx
@@ -0,0 +1,110 @@
+[
+    {
+        "_id": "653c9b8657eea8db8787a988",
+        "orderPrice": 2450,
+        "discountedOrderPrice": 2250,
+        "coupon": {
+            "_id": "65901ce83b469629aadfa546",
+            "name": "dashera Coupon",
+            "couponCode": "DASHERA"
+        },
+        "customer": {
+            "_id": "6527f0f9a5cb3daee993580c",
+            "firstname": "mani",
+            "email": "mhaskepavankumar@gmail.com"
+        },
+        "status": "PENDING",
+        "paymentProvider": "COD",
+        "isPaymentDone": true,
+        "createdAt": "2023-10-28T05:26:30.863Z",
+        "updatedAt": "2023-10-28T06:56:14.925Z",
+        "__v": 0,
+        "paymentId": "ABC123DEF456",
+        "address": {
+            "_id": "653b8e7dc77b9ffbcbd6ede7",
+            "addressLine1": "123 Main Street",
+            "addressLine2": "Apt 4B",
+            "city": "California",
+            "country": "USA",
+            "owner": "6527f0f9a5cb3daee993580c",
+            "pincode": "411005",
+            "state": "California",
+            "createdAt": "2023-10-27T10:18:37.720Z",
+            "updatedAt": "2023-10-27T10:18:37.720Z",
+            "__v": 0
+        },
+        "totalOrderItems": 3
+    },
+    {
+        "_id": "653cb049bf816af1592b0135",
+        "orderPrice": 4500,
+        "discountedOrderPrice": 4300,
+        "coupon": {
+            "_id": "65901ce83b469629aadfa546",
+            "name": "dashera Coupon",
+            "couponCode": "DASHERA"
+        },
+        "customer": {
+            "_id": "6527f0f9a5cb3daee993580c",
+            "firstname": "mani",
+            "email": "mhaskepavankumar@gmail.com"
+        },
+        "status": "DELIVERED",
+        "paymentProvider": "COD",
+        "isPaymentDone": false,
+        "createdAt": "2023-10-28T06:55:05.950Z",
+        "updatedAt": "2023-10-28T06:56:14.628Z",
+        "__v": 0,
+        "paymentId": "ABC123DEF123",
+        "address": {
+            "_id": "653b8e7dc77b9ffbcbd6ede7",
+            "addressLine1": "123 Main Street",
+            "addressLine2": "Apt 4B",
+            "city": "California",
+            "country": "USA",
+            "owner": "6527f0f9a5cb3daee993580c",
+            "pincode": "411005",
+            "state": "California",
+            "createdAt": "2023-10-27T10:18:37.720Z",
+            "updatedAt": "2023-10-27T10:18:37.720Z",
+            "__v": 0
+        },
+        "totalOrderItems": 3
+    },
+    {
+        "_id": "653cd400710f5ff57cbdc0a0",
+        "orderPrice": 4500,
+        "discountedOrderPrice": 4300,
+        "coupon": {
+            "_id": "65901ce83b469629aadfa546",
+            "name": "dashera Coupon",
+            "couponCode": "DASHERA"
+        },
+        "customer": {
+            "_id": "6527f0f9a5cb3daee993580c",
+            "firstname": "mani",
+            "email": "mhaskepavankumar@gmail.com"
+        },
+        "status": "DELIVERED",
+        "paymentProvider": "COD",
+        "isPaymentDone": true,
+        "createdAt": "2023-10-28T09:27:28.576Z",
+        "updatedAt": "2023-10-28T09:40:52.916Z",
+        "__v": 0,
+        "paymentId": "ABC123DEF789",
+        "address": {
+            "_id": "653b8e7dc77b9ffbcbd6ede7",
+            "addressLine1": "123 Main Street",
+            "addressLine2": "Apt 4B",
+            "city": "California",
+            "country": "USA",
+            "owner": "6527f0f9a5cb3daee993580c",
+            "pincode": "411005",
+            "state": "California",
+            "createdAt": "2023-10-27T10:18:37.720Z",
+            "updatedAt": "2023-10-27T10:18:37.720Z",
+            "__v": 0
+        },
+        "totalOrderItems": 3
+    }
+]
