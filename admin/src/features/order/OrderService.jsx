a3140d3d31e4ad916108f20bd84f22a3b647a890
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-09 02:08:06 +0530

    chore: added getAOrder facility

--- admin/src/features/order/OrderService.jsx
+++ admin/src/features/order/OrderService.jsx
@@ -16,8 +16,24 @@ const getOrders = async () => {
   return response.data;
 };
 
+// getAOrder 
+const getAOrder = async (orderId) => {
+  const token = Token;
+
+  console.log("token in OrderService is : ", token);
+  const url = `${base_url}orders/${orderId}`;
+  const response = await axios.get(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  //   const response = await axios.get(url);
+  console.log("Response in OrderService is : 🤍🤍", response);
+
+  return response.data;
+};
+
 const OrderService = {
   getOrders,
+  getAOrder,
 };
 
 export default OrderService; // export the service
