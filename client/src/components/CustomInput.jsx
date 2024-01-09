c911f3c060012396559bdef63ff393655a856bbd
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-09 17:40:37 +0530

    chore: still label is not added

--- client/src/components/CustomInput.jsx
+++ client/src/components/CustomInput.jsx
@@ -4,10 +4,9 @@ const CustomInput = (props) => {
   const { type, label, i_id, i_class, name, value, onChange, onBlur } = props;
   return (
     <div className="form-group mt-1">
-      {/* <label htmlFor="email">Email</label> */}
       <input
         type={type}
-        className={` form-control ${i_class}`}
+        className={`form-control ${i_class}`}
         id={i_id}
         placeholder={label}
         name={name}
