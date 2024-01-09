c0505e9b5f4313eb2608edda0fb4d3e86d3551d8
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-09 14:01:48 +0530

    chore: created new State in store

--- client/src/app/Store.jsx
+++ client/src/app/Store.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { configureStore } from "@reduxjs/toolkit";
-
+import authReducer from "../features/auth/AuthSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 });
