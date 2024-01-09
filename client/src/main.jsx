9cb871d6fcb9425f3444481509a6bef5d2fa3644
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-09 13:30:26 +0530

    chore: made available the store Globally

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./app/Store";
 // import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
   // {/* <App /> */}
 );
