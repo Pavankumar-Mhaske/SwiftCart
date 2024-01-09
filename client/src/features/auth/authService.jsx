c1fc93ba89cfa5223fe9ddc3f75ff74af00055c2
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-09 14:00:42 +0530

    feat: created the auth service functionality

---
+++ client/src/features/auth/authService.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { base_url } from "../../utils/base_url";
+
+const register = async (userData) => {
+  const url = `${base_url}users/register/`;
+  const response = await axios.post(url, userData);
+  console.log("Response in auth is : ", response);
+  if (response.data.data.token) {
+    localStorage.setItem("user", JSON.stringify(response.data.data.user));
+
+    // localStorage.setItem(key, JSON.stringify(value))
+  }
+  const token = JSON.parse(localStorage.getItem("user"));
+  console.log("token in auth is : ", token);
+  return response.data;
+};
+
+const AuthService = {
+  register,
+};
+
+export default AuthService; // export the service
