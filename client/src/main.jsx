de5b5b2c545aedf31fff7ee085455672d1374739
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-09 15:34:04 +0530

    css file imported

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./app/Store";
-// import "./main.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
