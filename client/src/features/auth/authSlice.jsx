b247caa998b8a13d485e43f5a6884fa02a300475
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-09 14:01:01 +0530

    feat: created the auth slice  functionality

---
+++ client/src/features/auth/authSlice.jsx
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import AuthService from "./AuthService";
+
+const initialState = {
+  user: null,
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      console.log("thunkAPI in authSlice is : ", thunkAPI);
+      const response = await AuthService.register(user);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        state.user = action.payload;
+      })
+      // ;builder
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.user = null;
+        state.message = action.payload.message;
+      });
+  },
+});
+
+export default authSlice.reducer;
