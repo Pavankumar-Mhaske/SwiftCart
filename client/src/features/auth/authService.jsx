034a313dfbcc125c9d424a6184673950e691abf2
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-09 18:29:29 +0530

    chore: added login service

--- client/src/features/auth/authService.jsx
+++ client/src/features/auth/authService.jsx
@@ -4,7 +4,21 @@ import { base_url } from "../../utils/base_url";
 const register = async (userData) => {
   const url = `${base_url}users/register/`;
   const response = await axios.post(url, userData);
-  console.log("Response in auth is : ", response);
+  console.log("Response in register auth is : ", response);
+  // if (response.data.data.token) {
+  //   localStorage.setItem("user", JSON.stringify(response.data.data.user));
+
+  //   // localStorage.setItem(key, JSON.stringify(value))
+  // }
+  // const token = JSON.parse(localStorage.getItem("user"));
+  // console.log("token in auth is : ", token);
+  return response.data;
+};
+
+const login = async (userData) => {
+  const url = `${base_url}users/login/`;
+  const response = await axios.post(url, userData);
+  console.log("Response in login auth is : ", response);
   // if (response.data.data.token) {
   //   localStorage.setItem("user", JSON.stringify(response.data.data.user));
 
@@ -17,6 +31,7 @@ const register = async (userData) => {
 
 const AuthService = {
   register,
+  login,
 };
 
 export default AuthService; // export the service
