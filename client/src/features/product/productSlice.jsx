4368bccf61c451e8d7a1423555f53ad748410a41
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-10 12:57:01 +0530

    feat: added getProducts slice

---
+++ client/src/features/product/productSlice.jsx
@@ -0,0 +1,59 @@
+import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import ProductService from "./ProductService";
+
+export const getProducts = createAsyncThunk(
+  "products/get-products",
+  async (thunkAPI) => {
+    try {
+      console.log("thunkAPI in productSlice is : ", thunkAPI);
+      const response = await ProductService.getProducts();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const resetState = createAction("reset_all");
+const initialState = {
+  products: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+// variable which changes the value of the variable which is from another file by it's setter function of useState
+
+export const ProductSlice = createSlice({
+  name: "product",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.products = action.payload.data.products;
+        console.log(
+          "action.payload in productSlice is : ",
+          action.payload.data.products
+        );
+      })
+      // ;builder
+      .addCase(getProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        // state.user = null;
+        state.message = action.error;
+      })
+      .addCase(resetState, () => initialState);
+  },
+});
+
+export default ProductSlice.reducer;
