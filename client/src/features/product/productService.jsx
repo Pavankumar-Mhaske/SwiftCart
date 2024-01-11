d404540f438db7da4ac95071139a8cf0b860ff60
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-11 16:46:30 +0530

    chore: now dependecies shift on the config

--- client/src/features/product/productService.jsx
+++ client/src/features/product/productService.jsx
@@ -1,22 +1,13 @@
 import axios from "axios";
 import { base_url } from "../../utils/base_url";
 import { configureStore } from "@reduxjs/toolkit";
-// import { Token } from "../Token";
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTlkNGYxZGQ1MWIwOTM4OTQ1ZWMxMTEiLCJlbWFpbCI6InBhdmFuQGV4YW1wbGUuY29tIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE3MDQ5Njk0MjEsImV4cCI6MTcwNTA1NTgyMX0.7jOr-4bP4_gVpLEVLwCEc-i9jEkP6zQk-vhIfhxOnKc";
 import { config } from "../../utils/AxiosConfig";
 
 const getProducts = async () => {
   //   const token = Token;
   //   console.log("token in productService is : ", token);
   const url = `${base_url}products/`;
-  const response = await axios.get(
-    url
-    //   , {
-    //   headers: { Authorization: `Bearer ${token}` },
-    // }
-  );
-  //   const response = await axios.get(url);
+  const response = await axios.get(url);
   console.log("Response in productService is : ", response);
 
   return response.data;
@@ -25,9 +16,7 @@ const getProducts = async () => {
 const addRemoveProductInWishList = async (productId) => {
   const url = `${base_url}products/wishlist/${productId}`;
   console.log("config", config);
-  const response = await axios.post(url, null, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  const response = await axios.post(url, null, config);
   console.log("Response in productService is 😀 : ", response);
 
   return response.data;
