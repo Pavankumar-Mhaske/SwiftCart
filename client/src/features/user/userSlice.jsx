48951365032ed9ce722450560eeb6c53c173ec3e
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-12 14:10:28 +0530

    feat: creaed getUserWishList slice

---
+++ client/src/features/user/userSlice.jsx
@@ -0,0 +1,66 @@
+import { createSlice, createAsyncThunk, createAction } from "@reduxjs/toolkit";
+import UserService from "./userService";
+
+export const getUserWishList = createAsyncThunk(
+  "users/get-wishlist",
+  async (thunkAPI) => {
+    try {
+      console.log("thunkAPI in userSlice is : ", thunkAPI);
+      const response = await UserService.getUserWishList();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+const getUserFromLocalStorage = localStorage.getItem("accessToken")
+  ? JSON.parse(localStorage.getItem("accessToken"))
+  : null;
+
+export const resetState = createAction("reset_all");
+const initialState = {
+  user: getUserFromLocalStorage,
+  wishlist: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUserWishList.pending, (state) => {
+        state.isLoading = true;
+      })
+      // ;builder
+      .addCase(getUserWishList.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        state.wishlist = action.payload.data.wishlist;
+        console.log(
+          "action.payload in userSlice is 💘💘 : ",
+          action.payload.data.wishlist
+        );
+      })
+      .addCase(getUserWishList.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = true;
+        state.message = action.payload.message;
+      })
+      .addCase(resetState, (state) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = false;
+        state.message = "";
+      });
+  },
+});
+
+export default userSlice.reducer;
