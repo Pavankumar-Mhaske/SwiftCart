dd8ba09601a7d4f1aeb42742fba61cdbb381504c
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-12 17:37:22 +0530

    chore: values will be fetched dynamically

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-function BlogCard() {
+const BlogCard = (props) => {
+  const { id, title, description, image, date } = props;
+
+  // function convertDate(inputDate) {
+  //   // Create a new Date object from the input date string
+  //   const inputDateTime = new Date(inputDate);
+
+  //   // Get the day, month, and year from the Date object
+  //   const day = inputDateTime.getUTCDate();
+  //   const month = inputDateTime
+  //     .toLocaleString("default", { month: "short" })
+  //     .toUpperCase();
+  //   const year = inputDateTime.getUTCFullYear();
+
+  //   // Format the date in the desired output format
+  //   const outputDate = `${day}-${month}-${year}`;
+
+  //   return outputDate;
+  // }
+
+  // const formattedDate = convertDate(date);
+
   return (
     <div className="blog-card">
       <div className="card-image">
-        <img className="img-fluid  w-100" src="/images/blog-1.jpg" alt="Blog" />
+        <img className="img-fluid  w-100" src={image} alt="Blog" />
       </div>
       <div className="blog-content">
-        <p className="date">1 DEC 2023</p>
-        <h5 className="title">A Beautiful Sunday Morning Renaissance</h5>
-        <p className="description">
-          You're Only As Good As Your Last Collection. Which is An Enomous
-          Pressure. I Think there is Something About...
-        </p>
-        <Link to="/blog/:id" className="button">
+        <p className="date">{date}</p>
+        <h5 className="title">{title}</h5>
+        <p className="description mb-3">{description}</p>
+
+        <Link to={`/blog/:${id}`} className="button">
           READ MORE
         </Link>
       </div>
     </div>
   );
-}
+};
 
 export default BlogCard;
