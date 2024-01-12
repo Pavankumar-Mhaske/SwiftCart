22a1f8bad452a196d79a159111e3f1a78a906315
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-12 14:27:32 +0530

    chore: added new state

--- client/src/app/Store.jsx
+++ client/src/app/Store.jsx
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/AuthSlice";
 import productReducer from "../features/product/ProductSlice";
 import userReducer from "../features/user/UserSlice";
+import blogReducer from "../features/blog/BlogSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     product: productReducer,
     user: userReducer,
+    blog: blogReducer,
   },
 });
