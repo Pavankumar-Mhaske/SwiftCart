78e6278831e2755ec38d23a3438b3676f7f1daba
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-16 17:20:00 +0530

    chore: values fetched dynamically

--- client/src/pages/SingleBlog.jsx
+++ client/src/pages/SingleBlog.jsx
@@ -1,62 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import { Link } from "react-router-dom";
 import { HiOutlineArrowLeft } from "react-icons/hi";
 import Container from "../components/Container";
+import { useDispatch, useSelector } from "react-redux";
+import { getABlog } from "../features/blog/BlogSlice";
+import { useLocation, useNavigate } from "react-router-dom";
+
 const SingleBlog = () => {
+  const location = useLocation();
+  const getBlogId = location.pathname.split("/")[2];
+  console.log("getBlogId in SingleBlog is : ", getBlogId);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getABlog(getBlogId));
+  }, []);
+
+  const blogState = useSelector((state) => state?.blog);
+  const { blog } = blogState;
+  console.log("blogState in Single blog is 🔥🔥 : ", blog);
+
   return (
-    
     <>
-      <Meta title={"Dynamic Blog Name"} />
-      <BreadCrumb title="Dynamic Blog Name" />
+      <Meta title={blog?.title} />
+      <BreadCrumb title={blog?.title} />
 
       <Container class1="blog-wrapper home-wrapper-2 py-5  ">
-          <div className="row">
-            {/* 🍓🍒🍓 Filter-Card 🍓🍒🍓 */}
-            <div className="col-3">
-              {/* 🍓🍒🍓 Filter-Card -1 🍓🍒🍓 */}
-              <div className="filter-card mb-3">
-                <h3 className="filter-title">Find by Categories</h3>
-                <div>
-                  <ul className="ps-0">
-                    <li>Watch</li>
-                    <li>Tv</li>
-                    <li>Camera</li>
-                    <li>Laptop</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            {/* 📄📃📃 Blog Card 📄📃📃  */}
-            <div className="col-9">
-              <div className="single-blog-card">
-                <Link to="/blogs" className="d-flex align-items-center gap-10">
-                  <HiOutlineArrowLeft className="fs-4" /> Go Back to Blogs
-                </Link>
-                <h3 className="title">
-                  A Beautiful Sunday Morning Renaissance
-                </h3>
-                <img
-                  className="img-fluid w-100 my-4"
-                  src="/images/blog-1.jpg"
-                  alt="Blog"
-                />
-
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non
-                  animi officiis a quos, deleniti praesentium reprehenderit.
-                  Veritatis libero enim, excepturi ratione eveniet voluptatibus
-                  velit quo unde asperiores similique animi impedit? Soluta
-                  accusamus in cupiditate eveniet totam quaerat nostrum sequi
-                  autem animi laudantium, quia tempore similique ullam aut
-                  voluptatum dicta? Et voluptate minima cumque magnam harum est
-                  necessitatibus autem earum nostrum! Placeat at eaque fugit
-                </p>
+        <div className="row">
+          {/* 🍓🍒🍓 Filter-Card 🍓🍒🍓 */}
+          <div className="col-3">
+            {/* 🍓🍒🍓 Filter-Card -1 🍓🍒🍓 */}
+            <div className="filter-card mb-3">
+              <h3 className="filter-title">Find by Categories</h3>
+              <div>
+                <ul className="ps-0">
+                  <li>Watch</li>
+                  <li>Tv</li>
+                  <li>Camera</li>
+                  <li>Laptop</li>
+                </ul>
               </div>
             </div>
           </div>
-        </Container>
+
+          {/* 📄📃📃 Blog Card 📄📃📃  */}
+          <div className="col-9">
+            <div className="single-blog-card">
+              <Link to="/blogs" className="d-flex align-items-center gap-10">
+                <HiOutlineArrowLeft className="fs-4" /> Go Back to Blogs
+              </Link>
+              <h3 className="title">{blog?.title}</h3>
+              <img
+                className="img-fluid w-100 my-4"
+                src={blog?.images ? blog?.images[0]?.url : "/images/blog-1.jpg"}
+                alt="Blog"
+              />
+
+              <p>{blog?.description}</p>
+            </div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 };
