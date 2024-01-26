fc1ab0a0d43a7f0910ffe8f65be4247caaa29507
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-26 15:05:20 +0530

    chore: created feature for getAProductById

--- client/src/features/product/productService.jsx
+++ client/src/features/product/productService.jsx
@@ -12,6 +12,15 @@ const getProducts = async () => {
   return response.data;
 };
 
+const getAProduct = async (productId) => {
+  console.log("productId in productService is  ", productId);
+  const url = `${base_url}products/${productId}`;
+  const response = await axios.get(url, config);
+  console.log("Response in productService is : ", response);
+
+  return response.data;
+};
+
 const addRemoveProductInWishList = async (productId) => {
   const url = `${base_url}products/wishlist/${productId}`;
   console.log("config", config);
@@ -23,6 +32,7 @@ const addRemoveProductInWishList = async (productId) => {
 
 const ProductService = {
   getProducts,
+  getAProduct,
   addRemoveProductInWishList,
 };
 
