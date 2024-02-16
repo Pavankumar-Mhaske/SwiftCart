2aeb88a7bda958fc693a46ca391f7e2ed57fa4fb
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-16 17:21:28 +0530

    chore: bit of modification

--- client/src/features/user/userService.jsx
+++ client/src/features/user/userService.jsx
@@ -10,7 +10,8 @@ const getUserWishList = async () => {
   return response.data;
 };
 
-const addItemOrUpdateItemQuantity = async (productId, quantity) => {
+const addItemOrUpdateItemQuantity = async (cartData) => {
+  const { productId, quantity } = cartData;
   const url = `${base_url}cart/item/${productId}/`;
   const response = await axios.post(url, quantity, config);
   console.log("Response in userService is : ", response);
