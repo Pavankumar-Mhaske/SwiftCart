d03c868fa6d2b99435913d73320aa01be38f3985
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-25 00:22:08 +0530

    chore: done with orders

--- server/controller/ecommerce/dummy.js
+++ server/controller/ecommerce/dummy.js
@@ -13,15 +13,158 @@
     "orderPrice": 2222,
     "discountedOrderPrice": 2222,
     "coupon": null,
-    "customer": "659d4f1dd51b0938945ec111",
+    "customer": {
+        "_id": "659d4f1dd51b0938945ec111",
+        "avatar": {
+            "url": "https://via.placeholder.com/200x200.png",
+            "localPath": "",
+            "_id": "659d4f1dd51b0938945ec110"
+        },
+        "firstname": "pavan",
+        "lastname": "mhaske",
+        "email": "mhaskepavankumar@gmail.com",
+        "role": "USER",
+        "password": "$2b$10$sjCIypIHCq09AadH1ya6teiPo5lpHojkKOlijsvsK8dxZ.3xu/HMm",
+        "loginType": "EMAIL_PASSWORD",
+        "mobile": "1111111111",
+        "isBlocked": false,
+        "cart": "659d4f1ed51b0938945ec117",
+        "wishlist": [
+            "659f9f29d14c2e31ef3057e1",
+            "659e8853f54f8670d5aa6819",
+            "659e884df54f8670d5aa680f",
+            "65b3bb577580337d2fd7de0e"
+        ],
+        "isEmailVerified": false,
+        "isVerified": false,
+        "createdAt": "2024-01-09T13:50:21.548Z",
+        "updatedAt": "2024-02-24T16:33:58.675Z",
+        "__v": 62,
+        "emailVerificationExpiry": "2024-01-09T14:10:23.790Z",
+        "emailVerificationToken": "d61da773960ffdd6f433cedc2cc213ec675156b1a08741f0f942d0cd56b05647",
+        "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTlkNGYxZGQ1MWIwOTM4OTQ1ZWMxMTEiLCJlbWFpbCI6Im1oYXNrZXBhdmFua3VtYXJAZ21haWwuY29tIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE3MDg3OTI0MzgsImV4cCI6MTcwODg3ODgzOH0.iC0mk7C8PB27R6R2EtLnNI_-hCvfEgWydagt2wre5HM",
+        "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTlkNGYxZGQ1MWIwOTM4OTQ1ZWMxMTEiLCJpYXQiOjE3MDg3OTI0MzgsImV4cCI6MTcwOTY1NjQzOH0.LX3efu328u17Abb_rQLy1phPc_WPUhRDoo3A-W481Zk",
+        "address": [
+            "65d8e01355e70f9cc96d3f7b",
+            "65d8e39edaf6887d33661f18",
+            "65d8e3addaf6887d33661f1f",
+            "65d8ec6e93796f26a55c8685",
+            "65d8ece093796f26a55c8728",
+            "65d8edfc93796f26a55c8779",
+            "65d8ef0493796f26a55c881c",
+            "65d8ef9d93796f26a55c8865",
+            "65d8f01d93796f26a55c88a1",
+            "65d8f0ab93796f26a55c88d4",
+            "65d8f17c93796f26a55c8923",
+            "65d8f1f193796f26a55c89a3",
+            "65d8f22d93796f26a55c89d6",
+            "65d8f34b5c4096b2520b8474",
+            "65d8f47d5c4096b2520b84c3",
+            "65d8f62c5c4096b2520b84f6",
+            "65d8f700eaa2fb964a2a9132",
+            "65d8f74beaa2fb964a2a913b",
+            "65d8f75beaa2fb964a2a9175",
+            "65d8f762eaa2fb964a2a917e",
+            "65d8f7bdeaa2fb964a2a91b8",
+            "65d8fa5eeaa2fb964a2a91eb",
+            "65d8faa5eaa2fb964a2a9217",
+            "65d8fb01eaa2fb964a2a9251",
+            "65d8fbc3eaa2fb964a2a92dc",
+            "65d8fce4eaa2fb964a2a938e",
+            "65d8fdd2eaa2fb964a2a942f",
+            "65d8ffa2eaa2fb964a2a9552",
+            "65d901b5eaa2fb964a2a9666",
+            "65d901e9eaa2fb964a2a96f9"
+        ]
+    },
     "items": [
         {
-            "productId": "65b3bb577580337d2fd7de0e",
+            "productId": {
+                "_id": "65b3bb577580337d2fd7de0e",
+                "name": "watch7",
+                "slug": "watch7",
+                "description": "this is the product just for entertainment....",
+                "price": 2222,
+                "stock": 19968,
+                "soldItems": 32,
+                "category": "652b9cf2b8fd6a63e2dcd85e",
+                "brand": "SONY",
+                "mainImages": [
+                    {
+                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277700/qnmul1fpa3wspzwlfmav.jpg",
+                        "asset_id": "5906941ac3c5223d68eeae31bc210ab8",
+                        "public_id": "qnmul1fpa3wspzwlfmav",
+                        "_id": "65b3bb577580337d2fd7de0f"
+                    },
+                    {
+                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277702/ff2depmib2okqfgj2zp0.jpg",
+                        "asset_id": "ecf40431897ee528cd46941cf7ebbde6",
+                        "public_id": "ff2depmib2okqfgj2zp0",
+                        "_id": "65b3bb577580337d2fd7de10"
+                    }
+                ],
+                "subImages": [
+                    {
+                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277703/js1qnfk2yifrel552aw1.jpg",
+                        "asset_id": "8f2f1966436e9882d94412a99ccd92bc",
+                        "public_id": "js1qnfk2yifrel552aw1",
+                        "_id": "65b3bb577580337d2fd7de11"
+                    },
+                    {
+                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277705/u6h4hvcbhpa6ij4gu5tj.jpg",
+                        "asset_id": "02c31c9618428b78767a3249a1d4b9cd",
+                        "public_id": "u6h4hvcbhpa6ij4gu5tj",
+                        "_id": "65b3bb577580337d2fd7de12"
+                    },
+                    {
+                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277707/bvkovdw9wcnyryubg1ji.jpg",
+                        "asset_id": "c09a28f59ac0f07f1c3bd4897743faa4",
+                        "public_id": "bvkovdw9wcnyryubg1ji",
+                        "_id": "65b3bb577580337d2fd7de13"
+                    },
+                    {
+                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277708/ybhtmkc38ts6j8gqq3se.jpg",
+                        "asset_id": "8dd86533c4129206a34e652a1b9528b4",
+                        "public_id": "ybhtmkc38ts6j8gqq3se",
+                        "_id": "65b3bb577580337d2fd7de14"
+                    }
+                ],
+                "colors": [
+                    "653cee827d3bc7fc1be0efe1",
+                    "653cedcf7d3bc7fc1be0efd5",
+                    "6590170e3b469629aadfa527"
+                ],
+                "tags": [
+                    "FEATURED",
+                    "POPULAR",
+                    "SPECIAL"
+                ],
+                "rating": 5,
+                "owner": "6527f0f9a5cb3daee993580c",
+                "reviews": [],
+                "createdAt": "2024-01-26T14:01:59.124Z",
+                "updatedAt": "2024-02-23T20:37:17.582Z",
+                "__v": 0
+            },
             "quantity": 1,
             "_id": "65d8d74adab24643438b117d"
         }
     ],
-    "address": "65d8ef0493796f26a55c881c",
+    "address": {
+        "_id": "65d8ef0493796f26a55c881c",
+        "country": "India",
+        "addressLine1": "Students hostel COEP, revenue colony, Pune, 411005,",
+        "addressLine2": "revenue colony, Pune, 411005,",
+        "addressLine3": "",
+        "city": "Pune",
+        "state": "Maharashtra",
+        "pincode": "411005",
+        "deliveryinfo": "",
+        "owner": "659d4f1dd51b0938945ec111",
+        "createdAt": "2024-02-23T19:16:20.561Z",
+        "updatedAt": "2024-02-23T19:16:20.561Z",
+        "__v": 0
+    },
     "status": "PENDING",
     "paymentProvider": "RAZORPAY",
     "paymentId": "order_NeQwbLK7ixdrXx",
@@ -29,11 +172,4 @@
     "createdAt": "2024-02-23T19:18:04.044Z",
     "updatedAt": "2024-02-23T19:18:04.044Z",
     "__v": 0
-},
-
-this is what i get when i try to get the order details
-by command :    const userOrders = await EcomOrder.find({ customer: _id });
-"but i want to populate the product insite the items array which has id as 
-productId and the address insite the address field which has id as address"
-
-
+}
