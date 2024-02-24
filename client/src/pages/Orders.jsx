98f2ae828947e2a965d3384e53ad7ef2e00c79a6
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-24 23:29:34 +0530

    feat: created the tablular component for the showing orders

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import Container from "../components/Container";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
 
 const Orders = () => {
   return (
-    <div>Orders</div>
-  )
-}
+    <>
+      <Meta title={"Orders"} />
+      <BreadCrumb title="Cart" />
+      <Container class1="cart-wrapper home-wrapper-2 py-5">
+        <div className="col-12">
+          <div className="row">
+            <div className="col-3">
+              <h5>Order Id</h5>
+            </div>
+            <div className="col-3">
+              <h5>Total Amount</h5>
+            </div>
+            <div className="col-3">
+              <h5>Total amount after discount</h5>
+            </div>
+            <div className="col-3">
+              <h5>Status</h5>
+            </div>
+          </div>
+          <div className="col-12 mt-3">
+            <div className="row">
+              <div className="col-3">
+                <p>Order Id</p>
+              </div>
+              <div className="col-3">
+                <p>Total Amount</p>
+              </div>
+              <div className="col-3">
+                <p>Total amount after discount</p>
+              </div>
+              <div className="col-3">
+                <p>Status</p>
+              </div>
+              <div className="col-12">
+                <div className="row bg-secondary p-3">
+                  <div className="col-3">
+                    <p>Order Id</p>
+                  </div>
+                  <div className="col-3">
+                    <p>Total Amount</p>
+                  </div>
+                  <div className="col-3">
+                    <p>Total amount after discount</p>
+                  </div>
+                  <div className="col-3">
+                    <p>Status</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
 
-export default Orders
+export default Orders;
