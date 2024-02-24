ea1114a737f1b4dd0c91eb7bddd4e3870c82a006
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-24 23:13:57 +0530

    chore: added link for the my orders

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -201,6 +201,7 @@ const Header = () => {
                   <div className="d-flex align-items-center gap-15">
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/store">Our Store</NavLink>
+                    <NavLink to="/orders">My Orders</NavLink>
                     <NavLink to="/blogs">Blogs</NavLink>
                     <NavLink to="/contact">contact</NavLink>
                   </div>
