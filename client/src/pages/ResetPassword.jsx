3d32fdee7a72d2b97f83ccf6811747c057a2d454
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-25 17:54:49 +0530

    chore: modified it for validation

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 import Meta from "../components/Meta";
 import BreadCrumb from "../components/BreadCrumb";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
+
+let schema = yup.object().shape({
+  password: yup.string().required("Password is required"),
+  confirmPassword: yup.string().required("Confirm Password is required"),
+});
 
 const ResetPassword = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const initialValues = {
+    password: "",
+    confirmPassword: "",
+  };
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: initialValues,
+    validationSchema: schema,
+    onSubmit: async (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <>
       <Meta title={"Reset Password"} />
@@ -17,24 +42,45 @@ const ResetPassword = () => {
             <div className="auth-card">
               <h3 className="title text-center mb-3">Reset Password</h3>
               {/* 📜📜📜 Form 📜📜📜 */}
-              <form action="" className="d-flex flex-column gap-15">
+              <form
+                action=""
+                onSubmit={formik.handleSubmit}
+                className="d-flex flex-column gap-15"
+              >
                 {/* 📧📧📧 Email 📧📧📧 */}
 
                 {/* 🔐🔐🔐 Password 🔐🔐🔐 */}
                 <CustomInput
+                  id="password"
                   type="password"
                   name="password"
-                  placeholder="Enter your password"
-                  className="form-control "
+                  label="Enter your password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
+                <div className="error">
+                  {formik.touched.password && formik.errors.password ? (
+                    <div className="error">{formik.errors.password}</div>
+                  ) : null}
+                </div>
 
                 {/* 🔐🔐🔐 Confirm Password 🔐🔐🔐 */}
                 <CustomInput
+                  id="password"
                   type="password"
-                  name="password"
-                  placeholder="Confirm your password"
-                  className="form-control "
+                  name="confirmPassword"
+                  label="Confirm your password"
+                  value={formik.values.confirmPassword}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
+                <div className="error">
+                  {formik.touched.confirmPassword &&
+                  formik.errors.confirmPassword ? (
+                    <div className="error">{formik.errors.confirmPassword}</div>
+                  ) : null}
+                </div>
                 {/* 🔗🔗🔗 Links 🔗🔗🔗 */}
                 <div className="form-group">
                   <div className=" mt-3 d-flex justify-content-center align-items-center gap-15 ">
