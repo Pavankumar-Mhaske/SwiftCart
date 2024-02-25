34d61cd44061ae957bccf8a1a483d8155d9deea7
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-25 17:38:17 +0530

    chore: this place has been made dynamic

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import Meta from "../components/Meta";
 import BreadCrumb from "../components/BreadCrumb";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
+
+let schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Invalid email address")
+    .required("Email is required"),
+});
 
 const ForgotPassword = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: schema,
+    onSubmit: async (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <>
       <Meta title={"Forgot Password"} />
@@ -20,15 +43,27 @@ const ForgotPassword = () => {
                 We will send you an email to reset your password
               </p>
               {/* 📜📜📜 Reset Password Form 📜📜📜 */}
-              <form action="" className="d-flex flex-column gap-15">
+              <form
+                action=""
+                onSubmit={formik.handleSubmit}
+                className="d-flex flex-column gap-15"
+              >
                 {/* 📧📧📧 Email 📧📧📧 */}
                 <CustomInput
+                  id="email"
+                  className="form-control"
                   type="email"
                   name="email"
-                  placeholder="Enter your email"
-                  className="form-control"
+                  label="Enter your email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
-
+                <div className="error text-center">
+                  {formik.touched.email && formik.errors.email ? (
+                    <div className="error">{formik.errors.email}</div>
+                  ) : null}
+                </div>
                 {/* 📝📝📝 Forgot Password 📝📝📝 */}
                 <div className="form-group">
                   <div className=" mt-3 d-flex flex-column justify-content-center align-items-center gap-15 ">
