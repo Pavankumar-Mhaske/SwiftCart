8417e6a9e42b5c1ea023dec3e67ff006754eda8a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-26 06:33:48 +0530

    feat: added multiFormatDateString

---
+++ client/src/utils/DateFormate.js
@@ -0,0 +1,26 @@
+export const multiFormatDateString = (timestamp = "") => {
+  const timestampNum = Math.round(new Date(timestamp).getTime() / 1000);
+  const date = new Date(timestampNum * 1000);
+  const now = new Date();
+
+  const diff = now.getTime() - date.getTime();
+  const diffInSeconds = diff / 1000;
+  const diffInMinutes = diffInSeconds / 60;
+  const diffInHours = diffInMinutes / 60;
+  const diffInDays = diffInHours / 24;
+
+  switch (true) {
+    case Math.floor(diffInDays) >= 30:
+      return formatDateString(timestamp);
+    case Math.floor(diffInDays) === 1:
+      return `${Math.floor(diffInDays)} day ago`;
+    case Math.floor(diffInDays) > 1 && diffInDays < 30:
+      return `${Math.floor(diffInDays)} days ago`;
+    case Math.floor(diffInHours) >= 1:
+      return `${Math.floor(diffInHours)} hours ago`;
+    case Math.floor(diffInMinutes) >= 1:
+      return `${Math.floor(diffInMinutes)} minutes ago`;
+    default:
+      return "Just now";
+  }
+};
