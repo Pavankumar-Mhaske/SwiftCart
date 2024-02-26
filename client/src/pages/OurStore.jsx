747a2854495e027b8e5527a8463c97f2b3d51801
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-27 02:12:58 +0530

    chore: made all the filtering states dynamic

--- client/src/pages/OurStore.jsx
+++ client/src/pages/OurStore.jsx
@@ -16,8 +16,69 @@ const OurStore = () => {
     dispatch(getProducts());
   }, []);
 
-  const productState = useSelector((state) => state.product.products);
-  console.log("productState in OurStore is : ", productState);
+  const productState = useSelector((state) => state.product);
+  const { products } = productState;
+  console.log("products in OurStore is : ", products);
+
+  const [brands, setBrands] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [colors, setColors] = useState([]);
+
+  // Filter States
+  const [brand, setBrand] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [tag, setTag] = useState([]);
+  const [color, setColor] = useState([]);
+  const [minPrice, setMinPrice] = useState(null);
+  const [maxPrice, setMaxPrice] = useState(null);
+
+  useEffect(() => {
+    let brands = [];
+    let categories = [];
+    let tags = [];
+    let colors = [];
+    if (products && products.length > 0) {
+      for (let index = 0; index < products?.length; index++) {
+        const product = products[index];
+        brands.push(product?.brand);
+
+        if (product.category && product.category.length > 0) {
+          for (let i = 0; i < product.category.length; i++) {
+            categories.push(product.category[i]?.name);
+          }
+        }
+
+        if (product.tags && product.tags.length > 0) {
+          for (let i = 0; i < product.tags.length; i++) {
+            tags.push(product.tags[i]);
+          }
+        }
+
+        if (product.colors && product.colors.length > 0) {
+          for (let i = 0; i < product.colors.length; i++) {
+            colors.push(product.colors[i]?.name);
+          }
+        }
+      }
+    }
+    setBrands(brands);
+    setCategories(categories);
+    setTags(tags);
+    setColors(colors);
+  }, [products]);
+
+  console.log("brands in OurStore is : ", [...new Set(brands)]);
+  console.log("categories in OurStore is : ", [...new Set(categories)]);
+  console.log("tags in OurStore is : ", [...new Set(tags)]);
+  console.log("colors in OurStore is : ", [...new Set(colors)]);
+  console.log("brand in OurStore is : ", brand);
+  console.log("category in OurStore is : ", category);
+  console.log("tag in OurStore is : ", tag);
+  console.log("color in OurStore is : ", color);
+  console.log("minPrice in OurStore is : ", minPrice);
+  console.log("maxPrice in OurStore is : ", maxPrice);
+
   return (
     <>
       <Meta title={"Our Store"} />
@@ -26,25 +87,55 @@ const OurStore = () => {
       <Container class1="store-wrapper home-wrapper-2 py-5">
         <div className="row">
           <div className="col-3">
-            {/* 🍓🍒🍓 Filter-Card -1 🍓🍒🍓 */}
+            {/* 🍓🍒🍓 Filter-Card -1 (Categories) 🍓🍒🍓 */}
             <div className="filter-card mb-3">
               <h3 className="filter-title">Shop by Categories</h3>
               <div>
                 <ul className="ps-0">
-                  <li>Watch</li>
+                  {categories &&
+                    [...new Set(categories)].map((category, index) => {
+                      return (
+                        <li key={index} onClick={() => setCategory(category)}>
+                          {category}
+                        </li>
+                      );
+                    })}
+                  {/* <li>Watch</li>
                   <li>Tv</li>
                   <li>Camera</li>
-                  <li>Laptop</li>
+                  <li>Laptop</li> */}
                 </ul>
               </div>
             </div>
 
-            {/* 🍓🍒🍓 Filter-Card -2 🍓🍒🍓 */}
+            {/* 🍓🍒🍓 Filter-Card -2 (Brands)🍓🍒🍓 */}
+            <div className="filter-card mb-3">
+              <h3 className="filter-title">Product Brands</h3>
+              <div>
+                <div className="product-tags d-flex flex-wrap align-items-center gap-10 ">
+                  {brands &&
+                    [...new Set(brands)].map((brand, index) => {
+                      return (
+                        <span
+                          className="badge bg-light text-secondary  rounded-3 py-2 px-3"
+                          style={{ cursor: "pointer" }}
+                          key={index}
+                          onClick={() => setBrand(brand)}
+                        >
+                          {brand}
+                        </span>
+                      );
+                    })}
+                </div>
+              </div>
+            </div>
+
+            {/* 🍓🍒🍓 Filter-Card -3 (Price, Color)🍓🍒🍓 */}
             <div className="filter-card mb-3">
               <h3 className="filter-title">Filter By </h3>
               <div>
                 {/* Availability 🥑🥑🥑🥑🥑🥑🥑🥑🥑🥑 */}
-                <h5 className="sub-title ">Availability</h5>
+                {/* <h5 className="sub-title ">Availability</h5>
                 <div>
                   <div className="form-check">
                     <input
@@ -68,7 +159,7 @@ const OurStore = () => {
                       Out of Stock (0)
                     </label>
                   </div>
-                </div>
+                </div> */}
 
                 {/* Price 💲💲💲💲💲💲💲💲 💸💸💸💸💸💸💸 */}
                 <h5 className="sub-title ">Price</h5>
@@ -76,83 +167,75 @@ const OurStore = () => {
                   {/* 🥧🥧 First Input 🥧🥧 */}
                   <div className="form-floating">
                     <input
-                      type="email"
+                      type="number"
                       className="form-control"
                       id="floatingInput"
                       placeholder="From"
+                      onChange={(e) => setMinPrice(e.target.value)}
                     />
                     <label htmlFor="floatingInput1">From</label>
                   </div>
                   {/* 🥧🥧 Second Input 🥧🥧 */}
                   <div className="form-floating">
                     <input
-                      type="email"
+                      type="number"
                       className="form-control"
                       id="floatingInput1"
                       placeholder="To"
+                      onChange={(e) => setMaxPrice(e.target.value)}
                     />
                     <label htmlFor="floatingInput1">To</label>
                   </div>
                 </div>
 
                 {/* Colors 🔴🟥 🟡🟨 🟠🟧 ⚪⬜ 🔵🟦 🟢🟩  */}
-                <h5 className="sub-title ">Colors</h5>
-                <div>
-                  <Color />
-                </div>
-
-                {/* Size 🐁 -> 🐘 */}
-                <h5 className="sub-title ">Size</h5>
-                <div>
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value=""
-                      id="color-1"
-                    />
-                    <label className="form-check-label" htmlFor="color-1">
-                      S (2)
-                    </label>
+                <div style={{ marginTop: "40px" }}>
+                  <h3 className="sub-title">Color :</h3>
+                  <div>
+                    <ul className="colors ps-0 ">
+                      {colors &&
+                        [...new Set(colors)].map((color, index) => {
+                          return (
+                            <li
+                              key={index}
+                              style={{
+                                backgroundColor: color,
+                                cursor: "pointer",
+                              }}
+                              onClick={() => setColor(color)}
+                            ></li>
+                          );
+                        })}
+                    </ul>
                   </div>
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value=""
-                      id="color-2"
-                    />
-                    <label className="form-check-label" htmlFor="color-2">
-                      M (2)
-                    </label>
+                </div>
+              </div>
+
+              {/* 🍓🍒🍓 Filter-Card -4 (Tags) 🍓🍒🍓 */}
+              <div style={{ marginTop: "50px" }}>
+                <h3 className="sub-title">Product Tags</h3>
+                <div>
+                  <div className="product-tags d-flex flex-wrap align-items-center gap-10 ">
+                    {brands &&
+                      [...new Set(tags)].map((tag, index) => {
+                        return (
+                          <span
+                            className="badge bg-light text-secondary  rounded-3 py-2 px-3"
+                            style={{ cursor: "pointer" }}
+                            key={index}
+                            onClick={() => setTag(tag)}
+                          >
+                            {tag}
+                          </span>
+                        );
+                      })}
                   </div>
                 </div>
               </div>
             </div>
 
-            {/* 🍓🍒🍓 Filter-Card -3 🍓🍒🍓 */}
-            <div className="filter-card mb-3">
-              <h3 className="filter-title">Product Tags</h3>
-              <div>
-                <div className="product-tags d-flex flex-wrap align-items-center gap-10 ">
-                  <span className="badge bg-light text-secondary  rounded-3 py-2 px-3 ">
-                    Headphone
-                  </span>
-                  <span className="badge bg-light text-secondary  rounded-3 py-2 px-3 ">
-                    Laptop
-                  </span>
-                  <span className="badge bg-light text-secondary  rounded-3 py-2 px-3 ">
-                    Mobile
-                  </span>
-                  <span className="badge bg-light text-secondary  rounded-3 py-2 px-3 ">
-                    Wire
-                  </span>
-                </div>
-              </div>
-            </div>
-
-            {/* 🍓🍒🍓 Filter-Card -4 🍓🍒🍓 */}
-            <div className="filter-card mb-3">
+            {/* 🍓🍒🍓 Filter-Card -5 🍓🍒🍓 */}
+            {/* <div className="filter-card mb-3">
               <h3 className="filter-title">Random Product</h3>
               <div>
                 <div className="random-products mb-3 d-flex">
@@ -202,8 +285,9 @@ const OurStore = () => {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
+          {/* 🍓🍒🍓 Products sections 🍓🍒🍓 */}
           <div className="col-9">
             <div className="filter-sort-grid mb-4">
               <div className="d-flex justify-content-between align-items-center">
@@ -267,8 +351,8 @@ const OurStore = () => {
             </div>
             <div className="products-list pb-5">
               <div className="d-flex gap-10 flex-wrap">
-                <ProductCard data={productState} grid={grid} />
-                </div>
+                <ProductCard data={products} grid={grid} />
+              </div>
             </div>
           </div>
         </div>
