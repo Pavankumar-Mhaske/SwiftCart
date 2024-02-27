28a955905b3376ec89de332568002e098c9e166c
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-27 21:02:06 +0530

    chore: making the arrangement for the minPrice but not getting done yet

--- client/src/features/product/productService.jsx
+++ client/src/features/product/productService.jsx
@@ -2,10 +2,28 @@ import axios from "axios";
 import { base_url } from "../../utils/base_url";
 import { config } from "../../utils/AxiosConfig";
 
-const getProducts = async () => {
+const getProducts = async (data) => {
+  console.log("data in productService is 😍😍: ", data);
   //   const token = Token;
   //   console.log("token in productService is : ", token);
-  const url = `${base_url}products/`;
+  // console.log("brand==='' ? ", data?.brand === "" ? "true" : "false");
+  console.log("data: ", data);
+
+  // const url = `${base_url}products?${
+  //   data?.brand ? `brand=${data?.brand}` : ""
+  // }`;
+  const url = `${base_url}products?${
+    data?.brand && data.brand.length > 0 ? `brand=${data.brand}&&` : ""
+  }${data?.tag && data.tag.length > 0 ? `tags=${data.tag}&&` : ""}${
+    data?.category && data.category.length > 0
+      ? `category=${data.category}&&`
+      : ""
+  }
+  ${data?.color && data.color.length > 0 ? `colors=${data.color}&&` : ""}
+  ${data?.minPrice ? `price[gte]=${data.minPrice}&&` : ""}
+  `;
+  // `price[gte]=${Number(data.minPrice)}&`
+  console.log("url in productService is 😍😍: ", url);
   const response = await axios.get(url);
   console.log("Response in productService is : ", response);
 
