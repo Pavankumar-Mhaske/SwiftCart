46766c29476c8a65adb4b48633b4466ec91fcff4
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-29 03:09:12 +0530

    chore: removed the dependency over the hardcoded token and shifted to thesession storage (localstorage)

--- admin/src/features/customers/customerService.jsx
+++ admin/src/features/customers/customerService.jsx
@@ -1,17 +1,14 @@
 import axios from "axios";
 import { base_url } from "../../utils/base_url";
-import { Token } from "../Token";
+import { config } from "../../utils/AxiosConfig";
 
 const getUsers = async () => {
   // Get the token from cookies
   //   const token = Cookies.get("user");
 
-  const token = Token;
-  console.log("token in customerService is : ", token);
+ ;
   const url = `${base_url}users/all-users/`;
-  const response = await axios.get(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  const response = await axios.get(url, config);
   //   const response = await axios.get(url);
   console.log("Response in customer is : ", response);
 
