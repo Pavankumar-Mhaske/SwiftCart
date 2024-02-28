4323bb4991dc2ad1a5792476397872018b97c252
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-28 19:01:15 +0530

    chore: all set with it

--- server/controller/ecommerce/dummy.js
+++ server/controller/ecommerce/dummy.js
@@ -1,16 +1,81 @@
-onst url = `${base_url}products?${
-    data?.brand && data.brand.length > 0 ? `brand=${data.brand}&&` : ""
-  }${data?.tag && data.tag.length > 0 ? `tags=${data.tag}&&` : ""}${
-    data?.category && data.category.length > 0
-      ? `category=${data.category}&&`
-      : ""
-  }
-  ${data?.color && data.color.length > 0 ?`colors=${data.color}&&` : ""}
-  ${data?.minPrice ? `price=${data.minPrice}&&` : ""}
-  `;
-$ when i pass the category i am getting queryObj as 
-queryObj = { '  colors': 'GREEN' }
-but when i pass the color then the queryObj is
-queryObj = { '  colors': 'GREEN' }
-can you find out what is the problem in passing this if required modified the code for url
-$
+{
+  "statusCode": 200,
+  "data": {
+      "data": [
+          {
+              "_id": {
+                  // "month": 2
+              },
+              "sales": 4444
+          },
+          {
+              "_id": {
+                  // "month": 5
+              },
+              "sales": 2222
+          },
+          {
+              "_id": {
+                  // "month": 9
+              },
+              "sales": 2222
+          },
+          {
+              "_id": {
+                  // "month": 10
+              },
+              "sales": 2222
+          },
+          {
+              "_id": {
+                  // "month": 1
+              },
+              "sales": 2222
+          },
+          {
+              "_id": {
+                  // "month": 4
+              },
+              "sales": 2222
+          },
+          {
+              "_id": {
+                  // "month": 6
+              },
+              "sales": 2222
+          },
+          {
+              "_id": {
+                  // "month": 11
+              },
+              "sales": 2222
+          },
+          {
+              "_id": {
+                  // "month": 3
+              },
+              "sales": 2222
+          },
+          {
+              "_id": {
+                  // "month": 7
+              },
+              "sales": 2222
+          },
+          {
+              "_id": {
+                  // "month": 12
+              },
+              "sales": 2222
+          },
+          {
+              "_id": {
+                  // "month": 8
+              },
+              "sales": 2222
+          }
+      ]
+  },
+  "message": "Monthwise order income fetched Successfully!",
+  "success": true
+}
