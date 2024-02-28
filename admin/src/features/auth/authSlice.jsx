d7e1abfa1e3b2767e1a0bd4627c2f31adc5f7148
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-29 02:42:25 +0530

    chore: added slice for the getMonthwiseOrdersIncome

--- admin/src/features/auth/authSlice.jsx
+++ admin/src/features/auth/authSlice.jsx
@@ -1,27 +1,5 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createAction } from "@reduxjs/toolkit";
 import authService from "./AuthService";
-
-// const userDefaultState = {
-//   _id: null,
-//   firstName: null,
-//   lastName: null,
-//   email: null,
-//   mobile: null,
-//   token: null,
-// };
-
-const getUserFromLocalStorage = localStorage.getItem("user")
-  ? JSON.parse(localStorage.getItem("user"))
-  : null;
-
-const initialState = {
-  // user: userDefaultState,
-  user: getUserFromLocalStorage,
-  isError: false,
-  isLoading: false,
-  isSuccess: false,
-  message: "",
-};
 
 export const login = createAsyncThunk(
   "auth/admin-login",
@@ -36,6 +14,34 @@ export const login = createAsyncThunk(
   }
 );
 
+// getMonthwiseOrdersInfo
+export const getMonthwiseOrdersInfo = createAsyncThunk(
+  "auth/get-monthwise-orders-info",
+  async (_, thunkAPI) => {
+    try {
+      console.log("thunkAPI in authSlice is : ", thunkAPI);
+      const response = await authService.getMonthwiseOrdersIncome();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+const getUserFromLocalStorage = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : null;
+
+export const resetState = createAction("reset_all");
+const initialState = {
+  user: getUserFromLocalStorage,
+  ordersInfo: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+
 export const authSlice = createSlice({
   name: "auth",
   initialState: initialState,
@@ -45,21 +51,39 @@ export const authSlice = createSlice({
       .addCase(login.pending, (state) => {
         state.isLoading = true;
       })
-      // ;builder
       .addCase(login.fulfilled, (state, action) => {
         state.isLoading = false;
         state.isSuccess = true;
         state.isError = false;
         state.user = action.payload;
       })
-      // ;builder
       .addCase(login.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.isSuccess = false;
         state.user = null;
         state.message = action.payload.message;
-      });
+      })
+      .addCase(getMonthwiseOrdersInfo.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getMonthwiseOrdersInfo.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        state.ordersInfo = action.payload.data.data;
+        console.log(
+          "action.payload in userSlice is 🥔🥔 : ",
+          action.payload.data.data
+        );
+      })
+      .addCase(getMonthwiseOrdersInfo.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = true;
+        state.message = action.payload.message;
+      })
+      .addCase(resetState, () => initialState);
   },
 });
 
