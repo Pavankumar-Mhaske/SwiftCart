d66b6dd79353d14c9b5cd49360b986b602542ecd
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-29 03:09:22 +0530

    chore: removed the dependency over the hardcoded token and shifted to thesession storage (localstorage)

--- admin/src/features/order/OrderService.jsx
+++ admin/src/features/order/OrderService.jsx
@@ -1,30 +1,21 @@
 import axios from "axios";
 import { base_url } from "../../utils/base_url";
-import { Token } from "../Token";
+import { config } from "../../utils/AxiosConfig";
 
 const getOrders = async () => {
-  const token = Token;
-
   console.log("token in OrderService is : ", token);
   const url = `${base_url}orders/list/admin`;
-  const response = await axios.get(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  const response = await axios.get(url, config);
   //   const response = await axios.get(url);
   console.log("Response in OrderService is : ", response);
 
   return response.data;
 };
 
-// getAOrder 
+// getAOrder
 const getAOrder = async (orderId) => {
-  const token = Token;
-
-  console.log("token in OrderService is : ", token);
   const url = `${base_url}orders/${orderId}`;
-  const response = await axios.get(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  const response = await axios.get(url, config);
   //   const response = await axios.get(url);
   console.log("Response in OrderService is : 🤍🤍", response);
 
