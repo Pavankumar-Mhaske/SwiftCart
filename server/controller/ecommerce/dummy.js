d741884bfde6bae3cb44745cd12073ec895e1d4b
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-29 02:44:40 +0530

    chore: all good

--- server/controller/ecommerce/dummy.js
+++ server/controller/ecommerce/dummy.js
@@ -1,81 +1,36 @@
 {
-  "statusCode": 200,
-  "data": {
-      "data": [
-          {
-              "_id": {
-                  " ": 1
-              },
-              "sales": 22220
-          },
-          {
-              "_id": {
-                  " ": 4
-              },
-              "sales": 2222
-          },
-          {
-              "_id": {
-                  " ": 3
-              },
-              "sales": 2222
-          },
-          {
-              "_id": {
-                  " ": 5
-              },
-              "sales": 2222
-          },
-          {
-              "_id": {
-                  " ": 8
-              },
-              "sales": 2222
-          },
-          {
-              "_id": {
-                  " ": 9
-              },
-              "sales": 2222
-          },
-          {
-              "_id": {
-                  " ": 7
-              },
-              "sales": 2222
-          },
-          {
-              "_id": {
-                  " ": 12
-              },
-              "sales": 2222
-          },
-          {
-              "_id": {
-                  " ": 10
-              },
-              "sales": 2222
-          },
-          {
-              "_id": {
-                  " ": 2
-              },
-              "sales": 222
-          },
-          {
-              "_id": {
-                  " ": 6
-              },
-              "sales": 2222
-          },
-          {
-              "_id": {
-                  " ": 11
-              },
-              "sales": 2222
-          }
-      ]
-  },
-  "message": "Monthwise order income fetched Successfully!",
-  "success": true
+    "statusCode": 200,
+    "data": {
+        "user": {
+            "_id": "6527f0f9a5cb3daee993580c",
+            "avatar": {
+                "url": "https://via.placeholder.com/200x200.png",
+                "localPath": "",
+                "_id": "6527f0f9a5cb3daee993580b"
+            },
+            "firstname": "mani",
+            "lastname": "rani",
+            "email": "mhaskepavankumar470684@gmail.com",
+            "role": "ADMIN",
+            "mobile": "1234567890",
+            "isEmailVerified": false,
+            "isVerified": false,
+            "createdAt": "2023-10-12T13:13:29.419Z",
+            "updatedAt": "2024-02-28T20:55:47.563Z",
+            "__v": 9,
+            "isBlocked": false,
+            "wishlist": [
+                "652a0f339698649ffbddfb28"
+            ],
+            "address": [
+                "653b8e7dc77b9ffbcbd6ede7",
+                "653b8eb4c77b9ffbcbd6edee"
+            ],
+            "loginType": "EMAIL_PASSWORD",
+            "cart": "653b9ce4aa245763d1960382",
+            "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTI3ZjBmOWE1Y2IzZGFlZTk5MzU4MGMiLCJlbWFpbCI6Im1oYXNrZXBhdmFua3VtYXI0NzA2ODRAZ21haWwuY29tIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzA5MTUzNzQ3LCJleHAiOjE3MDkyNDAxNDd9.sDtsmCUSuVCTFiCXI6dMgxYTGfYIRwOHSztTPIWGHj8"
+        }
+    },
+    "message": "User logged in successfully",
+    "success": true
 }
