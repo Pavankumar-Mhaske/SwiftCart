9877dd8db398b48a75f835a1698146158085c8d9
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-29 02:43:16 +0530

    chore: created the AxiosConfig in separate utils file

---
+++ admin/src/utils/AxiosConfig.jsx
@@ -0,0 +1,13 @@
+const getTokenFromLocalStorage = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : null;
+
+export const config = {
+  headers: {
+    Authorization: `Bearer ${
+      getTokenFromLocalStorage !== null
+        ? getTokenFromLocalStorage.accessToken
+        : ""
+    }`,
+  },
+};
