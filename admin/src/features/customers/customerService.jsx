8ac467804b7c68a092d6fc4d377cc67de3ba2f9a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-29 03:25:14 +0530

    chore: removed cookies comment... no need of cookies it's done via localStorage

--- admin/src/features/customers/customerService.jsx
+++ admin/src/features/customers/customerService.jsx
@@ -3,10 +3,7 @@ import { base_url } from "../../utils/base_url";
 import { config } from "../../utils/AxiosConfig";
 
 const getUsers = async () => {
-  // Get the token from cookies
-  //   const token = Cookies.get("user");
 
- ;
   const url = `${base_url}users/all-users/`;
   const response = await axios.get(url, config);
   //   const response = await axios.get(url);
