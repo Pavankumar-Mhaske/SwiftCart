7af8013896c20818f1534fdb21586e9a4cf65484
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-29 03:10:07 +0530

    chore: removed the dependency over the hardcoded token and shifted to thesession storage (localstorage)

--- client/src/features/contact/ContactService.jsx
+++ client/src/features/contact/ContactService.jsx
@@ -3,8 +3,7 @@ import { base_url } from "../../utils/base_url";
 import { config } from "../../utils/AxiosConfig";
 
 const createEnquiry = async (enquiry) => {
-  //   const token = Token;
-  //   console.log("token in productService is : ", token);
+
   const url = `${base_url}enquiries/`;
   const response = await axios.post(url, enquiry, config);
   console.log("Response in contactService is : ", response);
