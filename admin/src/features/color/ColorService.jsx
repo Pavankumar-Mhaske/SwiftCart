82043e7b45d17163742e62113e9932a1f590e730
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-29 03:09:01 +0530

    chore: removed the dependency over the hardcoded token and shifted to thesession storage (localstorage)

--- admin/src/features/color/ColorService.jsx
+++ admin/src/features/color/ColorService.jsx
@@ -1,15 +1,10 @@
 import axios from "axios";
 import { base_url } from "../../utils/base_url";
-import { Token } from "../Token";
+import { config } from "../../utils/AxiosConfig";
 
 const getColors = async () => {
-  const token = Token;
-
-  console.log("token in Colorservice is : ", token);
   const url = `${base_url}colors/`;
-  const response = await axios.get(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  const response = await axios.get(url, config);
   //   const response = await axios.get(url);
   console.log("Response in Colorservice is : ", response);
 
@@ -17,14 +12,10 @@ const getColors = async () => {
 };
 
 const createColor = async (color) => {
-  const token = Token;
-  console.log("token in colorService is : ", token);
   console.log("color in colorService is : ", color);
   const url = `${base_url}colors/`;
 
-  const response = await axios.post(url, color, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  const response = await axios.post(url, color, config);
 
   console.log("Response in colorService is : ", response);
 
@@ -32,13 +23,8 @@ const createColor = async (color) => {
 };
 
 const getAColor = async (colorId) => {
-  const token = Token;
-
-  console.log("token in ColorService is : ", token);
   const url = `${base_url}colors/${colorId}`;
-  const response = await axios.get(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  const response = await axios.get(url, config);
   //   const response = await axios.get(url);
   console.log("Response in ColorService is : ", response);
 
@@ -46,9 +32,6 @@ const getAColor = async (colorId) => {
 };
 
 const updateColor = async (data) => {
-  const token = Token;
-
-  console.log("token in update colorService is : ", token);
   console.log("data in update colorService is : ", data);
   const { colorId, name } = data;
   console.log("colorId and name in update colorService is : ", colorId, name);
@@ -67,12 +50,8 @@ const updateColor = async (data) => {
 };
 
 const deleteColor = async (colorId) => {
-  const token = Token;
-  console.log("token in ColorService is : ", token);
   const url = `${base_url}colors/${colorId}`;
-  const response = await axios.delete(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  const response = await axios.delete(url, config);
   //   const response = await axios.get(url);
   console.log("Response in ColorService is : ", response);
 
