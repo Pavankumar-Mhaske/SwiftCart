0bb281d91818dbafa5ef9424225779ccc46f5370
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-03-04 15:38:05 +0530

    chore: added the new showing status for the order

--- server/controller/ecommerce/dummy.js
+++ server/controller/ecommerce/dummy.js
@@ -1,176 +1,47 @@
 {
-    "_id": "65d8ef6c93796f26a55c882f",
-    "orderPrice": 222220,
-    "discountedOrderPrice": 222220,
-    "coupon": null,
-    "customer": {
-        "_id": "659d4f1dd51b0938945ec111",
-        "firstname": "pavan",
-        "lastname": "mhaske",
-        "email": "mhaskepavankumar@gmail.com"
+    title: "Status",
+    dataIndex: "status",
+    render: (status) => {
+      let color = "";
+
+      switch (status) {
+        case "PENDING":
+          color = "orange";
+          break;
+        case "CANCELED":
+          color = "red";
+          break;
+        case "DELIVERED":
+            color = "purple";
+            break;
+        case "HOLD":
+          color = "blue";
+          break;
+        case "COMPLETED":
+          color = "green";
+          break;
+        case "PROCESSING":
+          color = "cyan";
+          break;
+        case "SHIPPED":
+          color = "geekblue";
+          break;
+        
+        case "REFUNDED":
+          color = "magenta";
+          break;
+        case "ON_HOLD":
+          color = "gold";
+          break;
+        case "PARTIALLY_SHIPPED":
+          color = "volcano";
+          break;
+        // Add more cases as needed...
+
+        default:
+          color = "default";
+      }
+
+      return <Tag color={color}>{status}</Tag>;
     },
-    "items": [
-        {
-            "_id": "65d8d74adab24643438b117d",
-            "quantity": 1,
-            "product": {
-                "_id": "65b3bb577580337d2fd7de0e",
-                "name": "watch7",
-                "slug": "watch7",
-                "description": "this is the product just for entertainment....",
-                "price": 2222,
-                "stock": 19967,
-                "soldItems": 33,
-                "category": [
-                    {
-                        "_id": "652b9cf2b8fd6a63e2dcd85e",
-                        "name": "food",
-                        "owner": "6527f0f9a5cb3daee993580c",
-                        "createdAt": "2023-10-15T08:04:02.605Z",
-                        "updatedAt": "2023-10-15T08:04:02.605Z",
-                        "__v": 0
-                    }
-                ],
-                "brand": "SONY",
-                "mainImages": [
-                    {
-                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277700/qnmul1fpa3wspzwlfmav.jpg",
-                        "asset_id": "5906941ac3c5223d68eeae31bc210ab8",
-                        "public_id": "qnmul1fpa3wspzwlfmav",
-                        "_id": "65b3bb577580337d2fd7de0f"
-                    },
-                    {
-                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277702/ff2depmib2okqfgj2zp0.jpg",
-                        "asset_id": "ecf40431897ee528cd46941cf7ebbde6",
-                        "public_id": "ff2depmib2okqfgj2zp0",
-                        "_id": "65b3bb577580337d2fd7de10"
-                    }
-                ],
-                "subImages": [
-                    {
-                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277703/js1qnfk2yifrel552aw1.jpg",
-                        "asset_id": "8f2f1966436e9882d94412a99ccd92bc",
-                        "public_id": "js1qnfk2yifrel552aw1",
-                        "_id": "65b3bb577580337d2fd7de11"
-                    },
-                    {
-                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277705/u6h4hvcbhpa6ij4gu5tj.jpg",
-                        "asset_id": "02c31c9618428b78767a3249a1d4b9cd",
-                        "public_id": "u6h4hvcbhpa6ij4gu5tj",
-                        "_id": "65b3bb577580337d2fd7de12"
-                    },
-                    {
-                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277707/bvkovdw9wcnyryubg1ji.jpg",
-                        "asset_id": "c09a28f59ac0f07f1c3bd4897743faa4",
-                        "public_id": "bvkovdw9wcnyryubg1ji",
-                        "_id": "65b3bb577580337d2fd7de13"
-                    },
-                    {
-                        "url": "https://res.cloudinary.com/dgtchtfd2/image/upload/v1706277708/ybhtmkc38ts6j8gqq3se.jpg",
-                        "asset_id": "8dd86533c4129206a34e652a1b9528b4",
-                        "public_id": "ybhtmkc38ts6j8gqq3se",
-                        "_id": "65b3bb577580337d2fd7de14"
-                    }
-                ],
-                "colors": [
-                    {
-                        "_id": "653cedcf7d3bc7fc1be0efd5",
-                        "name": "GREEN",
-                        "owner": "6527f0f9a5cb3daee993580c",
-                        "createdAt": "2023-10-28T11:17:35.885Z",
-                        "updatedAt": "2024-01-05T11:36:13.685Z",
-                        "__v": 0
-                    },
-                    {
-                        "_id": "653cee827d3bc7fc1be0efe1",
-                        "name": "YELLOW",
-                        "owner": "6527f0f9a5cb3daee993580c",
-                        "createdAt": "2023-10-28T11:20:34.735Z",
-                        "updatedAt": "2024-01-05T11:36:03.916Z",
-                        "__v": 0
-                    },
-                    {
-                        "_id": "6590170e3b469629aadfa527",
-                        "name": "PINK",
-                        "owner": "6527f0f9a5cb3daee993580c",
-                        "createdAt": "2023-12-30T13:11:42.944Z",
-                        "updatedAt": "2024-01-05T11:35:47.720Z",
-                        "__v": 0
-                    }
-                ],
-                "tags": [
-                    "FEATURED",
-                    "POPULAR",
-                    "SPECIAL"
-                ],
-                "rating": 5,
-                "owner": "6527f0f9a5cb3daee993580c",
-                "createdAt": "2024-01-26T14:01:59.124Z",
-                "updatedAt": "2024-02-26T01:15:26.229Z",
-                "__v": 2,
-                "reviews": [
-                    {
-                        "user": "659d4f1dd51b0938945ec111",
-                        "rating": 5,
-                        "comment": "This was crazy.... i loved it soo much😀😀😀😀",
-                        "_id": "65dbdead26417091a48ba7be",
-                        "createdAt": "2024-02-26T00:58:58.236Z",
-                        "updatedAt": "2024-02-26T01:15:26.228Z"
-                    }
-                ]
-            }
-        }
-    ],
-    "status": "PENDING",
-    "paymentProvider": "RAZORPAY",
-    "paymentId": "order_NeQwbLK7ixdrXx",
-    "isPaymentDone": false,
-    "createdAt": "2024-01-23T19:18:04.044Z",
-    "updatedAt": "2024-01-23T19:18:04.044Z",
-    "__v": 0,
-    "address": {
-        "_id": "65d8ef0493796f26a55c881c",
-        "country": "India",
-        "addressLine1": "Students hostel COEP, revenue colony, Pune, 411005,",
-        "addressLine2": "revenue colony, Pune, 411005,",
-        "addressLine3": "",
-        "city": "Pune",
-        "state": "Maharashtra",
-        "pincode": "411005",
-        "deliveryinfo": "",
-        "owner": "659d4f1dd51b0938945ec111",
-        "createdAt": "2024-02-23T19:16:20.561Z",
-        "updatedAt": "2024-02-23T19:16:20.561Z",
-        "__v": 0
-    }
-}
-
-
-orderState &&
-      items &&
-      items.length > 0 &&
-      items.map((item, index) => {
-        data1.push({
-          key: index + 1,
-          product: item.product.name,
-          quantity: item.quantity,
-          brand: item.product.brand,
-          color: item.product.colors,
-          category: item.product.category,
-          tags: item.product.tags,
-          price: item.product.price,
-          action: "action",
-        });
-      });
-
-
-      PENDING: "orange",
-      HOLD: "blue",
-      CANCELED: "red",
-      COMPLETED: "green",
-      PROCESSING: "cyan",
-      SHIPPED: "geekblue",
-      DELIVERED: "purple",
-      REFUNDED: "magenta",
-      ON_HOLD: "gold",
-      PARTIALLY_SHIPPED: "volcano",
+  },
