95008bf78b47ba7e004d8da520c0a3330a4da2b9
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-03-04 18:16:49 +0530

    chore: removed the forgot-password and reset-password

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import ResetPassword from "./pages/ResetPassword";
-import ForgotPassword from "./pages/ForgotPassword";
+// import ResetPassword from "./pages/ResetPassword";
+// import ForgotPassword from "./pages/ForgotPassword";
 import Dashboard from "./pages/Dashboard";
 import MainLayout from "./components/MainLayout";
 import Enquiries from "./pages/Enquiries";
@@ -30,8 +30,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        {/* <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} /> */}
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="enquiries" element={<Enquiries />} />
