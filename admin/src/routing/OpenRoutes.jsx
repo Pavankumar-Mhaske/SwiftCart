048b1cff68603fab0315bdbdcb519235fec031bb
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-03-04 19:42:37 +0530

    chore: fixing the path issues

--- admin/src/routing/OpenRoutes.jsx
+++ admin/src/routing/OpenRoutes.jsx
@@ -14,6 +14,6 @@ export const OpenRoutes = ({ children }) => {
     getUserFromLocalStorage?.accessToken === undefined ? (
     children
   ) : (
-    <Navigate to="/" replace={true} />
+    <Navigate to="/admin" replace={true} />
   );
 };
