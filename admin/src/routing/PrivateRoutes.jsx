09862f7c130d64f9bcc966400ba82f3668b3e3cc
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-03-04 18:17:42 +0530

    chore: added PrivateRoutes

---
+++ admin/src/routing/PrivateRoutes.jsx
@@ -0,0 +1,19 @@
+import { Navigate } from "react-router-dom";
+
+export const PrivateRoutes = ({ children }) => {
+  const getUserFromLocalStorage = localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : null;
+
+  console.log(
+    "getUserFromLocalStorage in PrivateRoutes is : ",
+    getUserFromLocalStorage
+  );
+
+  return getUserFromLocalStorage &&
+    getUserFromLocalStorage?.accessToken !== undefined ? (
+    children
+  ) : (
+    <Navigate to="/login" replace={true} />
+  );
+};
