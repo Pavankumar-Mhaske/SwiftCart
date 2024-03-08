ed1475da9a915b6de7fc106d064f55918ff09743
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-03-08 15:29:34 +0530

    chore: changed the localStorage setting for the admin side

--- admin/src/routing/PrivateRoutes.jsx
+++ admin/src/routing/PrivateRoutes.jsx
@@ -1,8 +1,8 @@
 import { Navigate } from "react-router-dom";
 
 export const PrivateRoutes = ({ children }) => {
-  const getUserFromLocalStorage = localStorage.getItem("user")
-    ? JSON.parse(localStorage.getItem("user"))
+  const getUserFromLocalStorage = localStorage.getItem("adminUser")
+    ? JSON.parse(localStorage.getItem("adminUser"))
     : null;
 
   console.log(
