e21f48892d7c65bd40a60f0d205537f452c8e18d
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-03-16 17:14:25 +0530

    chore: handling the jwt expiration scenario

--- admin/src/features/blog/BlogService.jsx
+++ admin/src/features/blog/BlogService.jsx
@@ -3,64 +3,124 @@ import { base_url } from "../../utils/base_url";
 import { config } from "../../utils/AxiosConfig";
 
 const getBlogs = async () => {
- 
   const url = `${base_url}blogs/`;
-  const response = await axios.get(url, config);
-  //   const response = await axios.get(url);
-  console.log("Response in Blogservice is : ", response);
-
-  return response.data;
+  try {
+    const response = await axios.get(url, config);
+    //   const response = await axios.get(url);
+    console.log("Response in Blogservice is : ", response);
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+    }
+  }
 };
 
 const createBlog = async (blog) => {
- 
   console.log("blog in blogService is : ", blog);
-  const url = `${base_url}blogs/`;
-  const response = await axios.post(url, blog, config);
-
-  console.log("Response in blogService is : ", response);
-
-  return response.data;
+  try {
+    const url = `${base_url}blogs/`;
+    const response = await axios.post(url, blog, config);
+    console.log("Response in blogService is : ", response);
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const getABlog = async (blogId) => {
-   const url = `${base_url}blogs/${blogId}`;
-  const response = await axios.get(url, config);
-  //   const response = await axios.get(url);
-  console.log("Response in BlogService is : ", response);
-
-  return response.data;
+  const url = `${base_url}blogs/${blogId}`;
+  try {
+    const response = await axios.get(url, config);
+    //   const response = await axios.get(url);
+    console.log("Response in BlogService is : ", response);
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const updateBlog = async (data) => {
-   console.log("data in update blogService is : ", data);
+  console.log("data in update blogService is : ", data);
   const { blogId, title, category, description } = data;
   console.log("blogId and name in update blogService is : ", blogId, name);
-  const url = `${base_url}blogs/${blogId}`;
-  const blogToUpdate = {
-    title: title,
-    category: category,
-    description: description,
-  };
-
-  const response = await axios.patch(
-    url,
-    { ...blogToUpdate },
-    config
-  );
-
-  console.log("Response in blogService is : ", response);
-
-  return response.data;
+  try {
+    const url = `${base_url}blogs/${blogId}`;
+    const blogToUpdate = {
+      title: title,
+      category: category,
+      description: description,
+    };
+    const response = await axios.patch(url, { ...blogToUpdate }, config);
+    console.log("Response in blogService is : ", response);
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const deleteBlog = async (blogId) => {
-   const url = `${base_url}blogs/${blogId}`;
-  const response = await axios.delete(url, config);
-  //   const response = await axios.get(url);
-  console.log("Response in BlogService is : ", response);
-
-  return response.data;
+  const url = `${base_url}blogs/${blogId}`;
+  try {
+    const response = await axios.delete(url, config);
+    //   const response = await axios.get(url);
+    console.log("Response in BlogService is : ", response);
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const Blogservice = {
