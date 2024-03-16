990642bd99a9f697fcb10c187355fc6cc1d96fde
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-03-16 17:15:15 +0530

    chore: handling the jwt expiration scenario

--- admin/src/features/order/OrderService.jsx
+++ admin/src/features/order/OrderService.jsx
@@ -4,31 +4,76 @@ import { config } from "../../utils/AxiosConfig";
 
 const getOrders = async () => {
   const url = `${base_url}orders/list/admin`;
-  const response = await axios.get(url, config);
-  //   const response = await axios.get(url);
-  console.log("Response in OrderService is : ", response);
+  try {
+    const response = await axios.get(url, config);
+    //   const response = await axios.get(url);
+    console.log("Response in OrderService is : ", response);
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 // getAOrder
 const getAOrder = async (orderId) => {
   const url = `${base_url}orders/${orderId}`;
-  const response = await axios.get(url, config);
-  //   const response = await axios.get(url);
-  console.log("Response in OrderService is : 🌳🌳🌳", response);
+  try {
+    const response = await axios.get(url, config);
+    //   const response = await axios.get(url);
+    console.log("Response in OrderService is : 🌳🌳🌳", response);
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 // updateOrderStatus
 const updateOrderStatus = async (orderData) => {
   const { orderId, status } = orderData;
   const url = `${base_url}orders/status/${orderId}`;
-  const response = await axios.patch(url, { status: status }, config);
-  console.log("Response in OrderService is : ", response);
+  try {
+    const response = await axios.patch(url, { status: status }, config);
+    console.log("Response in OrderService is : ", response);
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const OrderService = {
