b4987596f2596dcc18dd1c6a2ba26f6e785a8ba2
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-03-16 17:15:08 +0530

    chore: handling the jwt expiration scenario

--- admin/src/features/enquiry/EnquiryService.jsx
+++ admin/src/features/enquiry/EnquiryService.jsx
@@ -4,30 +4,75 @@ import { config } from "../../utils/AxiosConfig";
 
 const getEnquiries = async () => {
   const url = `${base_url}enquiries/`;
-  const response = await axios.get(url, config);
-  //   const response = await axios.get(url);
-  console.log("Response in EnquiryService is : ", response);
+  try {
+    const response = await axios.get(url, config);
+    //   const response = await axios.get(url);
+    console.log("Response in EnquiryService is : ", response);
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const createEnquiry = async (enquiry) => {
   console.log("enquiry in enquiryService is : ", enquiry);
   const url = `${base_url}enquiries/`;
-  const response = await axios.post(url, enquiry, config);
+  try {
+    const response = await axios.post(url, enquiry, config);
 
-  console.log("Response in enquiryService is : ", response);
+    console.log("Response in enquiryService is : ", response);
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const getAEnquiry = async (enquiryId) => {
   const url = `${base_url}enquiries/${enquiryId}`;
-  const response = await axios.get(url, config);
-  //   const response = await axios.get(url);
-  console.log("Response in EnquiryService is : ", response);
+  try {
+    const response = await axios.get(url, config);
+    //   const response = await axios.get(url);
+    console.log("Response in EnquiryService is : ", response);
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const updateEnquiry = async (data) => {
@@ -39,24 +84,50 @@ const updateEnquiry = async (data) => {
     name
   );
   const url = `${base_url}enquiries/${enquiryId}`;
-  const response = await axios.patch(
-    url,
-    { status: status },
-    config
-  );
+  try {
+    const response = await axios.patch(url, { status: status }, config);
 
-  console.log("Response in enquiryService is : ", response);
+    console.log("Response in enquiryService is : ", response);
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const deleteEnquiry = async (enquiryId) => {
   const url = `${base_url}enquiries/${enquiryId}`;
-  const response = await axios.delete(url, config);
-  //   const response = await axios.get(url);
-  console.log("Response in EnquiryService is : ", response);
+  try {
+    const response = await axios.delete(url, config);
+    //   const response = await axios.get(url);
+    console.log("Response in EnquiryService is : ", response);
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const EnquiryService = {
