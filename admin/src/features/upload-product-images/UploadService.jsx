f7615b5f5724f5cb5711a24bcfba0c83c400e99c
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-03-16 17:15:36 +0530

    chore: handling the jwt expiration scenario

--- admin/src/features/upload-product-images/UploadService.jsx
+++ admin/src/features/upload-product-images/UploadService.jsx
@@ -4,16 +4,46 @@ import { config } from "../../utils/AxiosConfig";
 
 const uploadImages = async (data) => {
   const url = `${base_url}products/upload/`;
-  const response = await axios.post(url, data, config);
-  // console.log("response in uploadService is : ", response);
-  return response.data;
+  try {
+    const response = await axios.post(url, data, config);
+    console.log("response in uploadService is : ", response);
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const deleteImages = async (id) => {
   const url = `${base_url}products/delete/${id}`;
-  const response = await axios.delete(url, config);
-  // console.log("response in deleteService is : ", response);
-  return response.data;
+  try {
+    const response = await axios.delete(url, config);
+    console.log("response in deleteService is : ", response);
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const uploadService = {
