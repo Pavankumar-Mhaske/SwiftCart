6d831258202f491bc339a8e7af919bcdeac7de92
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-03-16 17:14:59 +0530

    chore: handling the jwt expiration scenario

--- admin/src/features/customers/customerService.jsx
+++ admin/src/features/customers/customerService.jsx
@@ -3,13 +3,27 @@ import { base_url } from "../../utils/base_url";
 import { config } from "../../utils/AxiosConfig";
 
 const getUsers = async () => {
-
   const url = `${base_url}users/all-users/`;
-  const response = await axios.get(url, config);
-  //   const response = await axios.get(url);
-  console.log("Response in customer is : ", response);
+  try {
+    const response = await axios.get(url, config);
+    //   const response = await axios.get(url);
+    console.log("Response in customer is : ", response);
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const customerService = {
