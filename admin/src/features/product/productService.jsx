cdc4a1b6b3363c2706dbb80eae9947e2bb6c9436
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-03-16 17:15:20 +0530

    chore: handling the jwt expiration scenario

--- admin/src/features/product/productService.jsx
+++ admin/src/features/product/productService.jsx
@@ -4,21 +4,53 @@ import { config } from "../../utils/AxiosConfig";
 
 const getProducts = async () => {
   const url = `${base_url}products/`;
-  const response = await axios.get(url, config);
-  //   const response = await axios.get(url);
-  console.log("Response in productService is : ", response);
 
-  return response.data;
+  try {
+    const response = await axios.get(url, config);
+    //   const response = await axios.get(url);
+    console.log("Response in productService is : ", response);
+
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const createProduct = async (product) => {
   console.log("product in productService is : ", product);
   const url = `${base_url}products/`;
-  const response = await axios.post(url, product, config);
 
-  console.log("Response in productService is : ", response);
+  try {
+    const response = await axios.post(url, product, config);
 
-  return response.data;
+    console.log("Response in productService is : ", response);
+
+    return response.data;
+  } catch (error) {
+    // console.log("error in userService is 💖💖💖💖💖💖💖💖💖💖: ", error);
+    // console.log(
+    //   "statusCode in userService is ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐: ",
+    //   error?.response?.data?.statusCode
+    // );
+    const statusCode = error?.response?.data?.statusCode;
+    if ([401, 403].includes(statusCode)) {
+      alert(`JWT Expired, Please login again!`);
+      localStorage.clear(); // Clear local storage on authentication issues
+      window.location.href = "/admin-login"; // Redirect to login page
+      // window.location.reload();
+    }
+  }
 };
 
 const productService = {
